ed1f127f6ff9f4fbe369d984eb2f33ace6081f05
Russell TAFF <rtaff2012@RTs-MacBook-Pro.local>
2013-09-23 18:46:15 -0400

    HW D2 & guessmy#

--- w01/d02/Russell/guess_the_number.rb
+++ w01/d02/Russell/guess_the_number.rb
@@ -1,21 +1,21 @@
 puts "I am thinking of an integer between 0 and 10... what number am I thinking of? "
 
-answer = get_answer_from_user
+answer = gets.chomp.to_i
 
 secret = rand(10)
 
 while secret != answer
+
 if answer > secret
       puts "Guess lower!"
       answer = gets.chomp.to_i
-
+end
  elsif answer < secret
  	puts "Guess higher!"
  	answer = gets.chomp.to_i
 
- end
-
  if answer == secret
  	puts "How did you know?"
+
  end
 
