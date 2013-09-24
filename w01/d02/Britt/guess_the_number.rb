00c8021e5f21c779bf6948a270732abbe9509f6e
brittlewis12 <brittlewis12@gmail.com>
2013-09-24 15:24:07 -0400

    fixed a string spacing anomaly in line 2

--- w01/d02/Britt/guess_the_number.rb
+++ w01/d02/Britt/guess_the_number.rb
@@ -1,5 +1,5 @@
 # Display a prompt for the user
-puts "I am thinking of an integer between 0 and 10... what number am I thinking of? "
+puts "I am thinking of an integer between 0 and 10... what number am I thinking of?"
 
 # Obtain the user's input and interpret it as an integer.
 answer = gets.chomp.to_i
