070554649bedddebe61b19c08a00b7c14cf6ff26
brittlewis12 <brittlewis12@gmail.com>
2013-09-24 18:40:48 -0400

    calculator.rb working in basic and advanced modes. just placeholders for factorial and trig.

--- w01/d02/Britt/calculator.rb
+++ w01/d02/Britt/calculator.rb
@@ -1,29 +1,117 @@
+
 # A user should be given a menu of operations
 # A user should be able to choose from the menu
 def menu
+	puts "What type of operation you would like to perform?"
+	puts "(b)asic arithmetic: addition, subtraction, multiplication, division"
+	puts "(a)dvanced: exponents and square roots"
+	puts "(t)rigonometry: sin(), cos(), tan()"
+	puts "(f)actorial: product of each integer below itself"
+	puts "(q)uit the calculator"
+	puts "Type your selection and press return:"
+	$choice = gets.chomp.downcase
+end
 
+def get_two_numbers
+	puts "What is the first number you'd like to use?"
+  $num1 = gets.chomp.to_i
+
+  puts "What is the second number youd like to use?"
+  $num2 = gets.chomp.to_i
+end
+
+
+#methods for basic_calc
+def addition(a, b)
+	puts a + b
+end
+
+def subtraction(a, b)
+	a - b
+end
+
+def multiplication(a, b)
+	a * b
+end
+
+def division(a, b)
+	a / b
 end
 
 
 # A user should be able to enter numbers to perform the operation on
 # A user should be shown the result
 def basic_calc
-  print "(a)dd, (s)ubtract, (m)ultiply, (d)ivide: "
+  puts "(a)dd, (s)ubtract, (m)ultiply, (d)ivide: "
+  basic = gets.chomp.downcase
+
+  get_two_numbers
+
+  case arithmetic
+  when "a"
+  	puts addition($num1, $num2)
+  when "s"
+   	puts subtraction($num1, $num2)
+  when "m"
+   	puts multiplication($num1, $num2)
+  when "d"
+   	puts division($num1, $num2)
+  end
 
 end
+
+
+
+
+# placeholder methods for advanced_calc
+def power
+	get_two_numbers
+	puts ($num1 ** $num2)
+end
+
+def square_root
+	puts "What number would you like to use?"
+	$num1 = gets.chomp.to_f
+	puts Math.sqrt($num1)
+end
+
 
 # Only tackle these once you get all of the basic_calc operations working
 # A user should be able to enter numbers to perform the operation on
 # A user should be shown the result
 def advanced_calc
   print "(p)ower, (s)qrt: "
+  advanced = gets.chomp.downcase
+
+  case advanced
+  when "p"
+  	power
+  when "s"
+  	square_root
+  end
 
 end
 
 
-response = menu
+def trigonometric_calc; end
+
+def factorial; end
+
+menu
 # This process should continue until the user selects a quit option from the menu
-while response != 'q'
+while $choice != 'q'
 
-  response = menu
+	case $choice
+	when "b"
+		basic_calc
+	when "a"
+		advanced_calc
+	when "t"
+		trigonometric_calc
+	when "f"
+		factorial
+	end
+
+  menu
 end
+
