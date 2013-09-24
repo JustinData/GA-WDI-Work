36fdbf08fb01c49386bb09fd789a8bc2bf8f96d0
brittlewis12 <brittlewis12@gmail.com>
2013-09-24 15:05:55 -0400

    updated guess_the_number logic for user input greater than 10

--- w01/d02/Britt/guess_the_number.rb
+++ w01/d02/Britt/guess_the_number.rb
@@ -9,8 +9,16 @@ secret = rand(10)
 
 # Use a while loop to let the user guess again until he/she gets the answer right.
 while answer != secret
-	puts "Guess again!"
+	# Checking to shame user if guess is above 10
+	if answer <= 10
+		puts "Guess again!"
+	else
+		puts "Why would you guess a number larger than 10, dummy?"
+		puts "Whatever, guess again!"
+	end
+
 	answer = gets.chomp.to_i
+
 end
 
 # Display a congratulatory response
