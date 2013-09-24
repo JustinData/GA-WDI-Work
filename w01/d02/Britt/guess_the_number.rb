6082c50e6a1078165686e649d4821b7bf78a290e
brittlewis12 <brittlewis12@gmail.com>
2013-09-24 15:23:12 -0400

    now tells user to guess higher or lower

--- w01/d02/Britt/guess_the_number.rb
+++ w01/d02/Britt/guess_the_number.rb
@@ -10,10 +10,15 @@ secret = rand(9) + 1
 # Use a while loop to let the user guess again until he/she gets the answer right.
 while answer != secret
 	# Checking to shame user if guess is above 10
-	if answer <= 10
-		puts "Guess again!"
+	if answer < 10
+		if secret > answer
+			puts "Good guess, but you undershot it! Guess higher!"
+		else
+			puts "Nice try, but you guessed too high! Guess lower!"
+		end
+		puts "Try again:"
 	else
-		puts "Why would you guess a number larger than 10, dummy?"
+		puts "Why would you guess a number larger than I told you to, dummy?"
 		puts "Whatever, guess again!"
 	end
 
