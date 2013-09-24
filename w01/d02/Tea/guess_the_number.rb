95a7f95281c7c1f7af405d78ef4ca4e78a0614ef
teabait <hello.teaho@gmail.com>
2013-09-24 16:03:42 -0400

    Drier version of Guess the Number

--- w01/d02/Tea/guess_the_number.rb
+++ w01/d02/Tea/guess_the_number.rb
@@ -1,21 +1,19 @@
 # Display a prompt for the user
-puts "I am thinking of an integer between 0 and 10... what number am I thinking of? "
+def get_answer_from_user
+  puts "I am thinking of an integer between 0 and 10... what number am I thinking of? "
+  $answer = gets.chomp.to_i
+end
 
-# Obtain the user's input and interpret it as an integer.
-answer = gets.chomp.to_i
-
-# Generate a random number between 0 and 10
 secret = rand(1..9)
 
 # Use a while loop to let the user guess again until he/she gets the answer right.
-while answer != secret
-  if answer < secret
+while $answer != secret
+  if $answer < secret
     puts "Higher...Guess again!"
-    answer = gets.chomp.to_i
   else
     puts "Lower...Guess again!"
-    answer = gets.chomp.to_i
   end
+  $answer = get_answer_from_user
 end
 
 puts "Congrats! You read my mind! You're amazing!"
