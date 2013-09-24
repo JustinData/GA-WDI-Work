57a7d97094b287a86852ac4be9a10e54ee55c774
Nichol Alexander <nichol.alexander@gmail.com>
2013-09-24 15:19:42 -0400

    added guidence for higher or lower

--- w01/d02/Nichol/guess_the_number.rb
+++ w01/d02/Nichol/guess_the_number.rb
@@ -5,12 +5,19 @@ puts "I am thinking of an integer between 0 and 10... what number am I thinking 
 answer = gets.chomp.to_i
 
 # Generate a random number between 0 and 10
-secret = rand(1..10)
+secret = rand(1..9)
 
 # Use a while loop to let the user guess again until he/she gets the answer right.
 while answer != secret
-  puts "thats not it - put in another guess"
+  puts "thats not it!"
+
+  if answer > secret
+  	puts "guess lower"
+  elsif answer < secret
+  	puts "guess higher"
+  end
   answer = gets.chomp.to_i
+
 end
 
 # Display a congratulatory response
