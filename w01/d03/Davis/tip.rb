94094b20c034a9dffe6616c75f4cbbe6df9cae91
daviskoh <koh.davis.0@gmail.com>
2013-09-25 09:57:39 -0400

    allow for percentage sign in input

--- w01/d03/Davis/tip.rb
+++ w01/d03/Davis/tip.rb
@@ -2,9 +2,9 @@ puts "What is the price of the meal?"
 price = gets.chomp.to_f
 
 puts "What is the tax as a percentage terms?"
-tax = gets.chomp.to_f * (0.001)
+tax = gets.chomp[/\d+/].to_f / 100.0
 
 puts "How much would you like to tip in percentage terms?"
-tip = gets.chomp.to_f * (0.001)
+tip = gets.chomp[/\d+/].to_f / 100.0
 
 puts "You are paying $#{price + (price * tax) + (price * tip)}"
