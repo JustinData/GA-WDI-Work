8858e5052ad7ac7257ed8c592b630eeac9afb329
DrRobotmck <mckenneth.scott@icloud.com>
2013-09-25 10:13:49 -0400

    Started all temp conversion...needs work

--- w01/d03/McKenneth/temperature.rb
+++ w01/d03/McKenneth/temperature.rb
@@ -11,7 +11,7 @@ def kelvin_to_celsius(temp)
 end
 
 def kelvin_to_fahrenheit(temp)
-  celsius_to_fahrenheit(temp - 273)
+  celsius_to_fahrenheit(kelvin_to_celsius(temp))
 end
 
 def fahrenheit_to_celsius(temp)
@@ -20,6 +20,7 @@ end
 
 def fahrenheit_to_kelvin(temp)
   celsius_to_kelvin(fahrenheit_to_celsius(temp))
+end
 
 puts "Give me a temperature in Celsius"
 print ">"
@@ -30,10 +31,48 @@ print ">"
 selection = gets.chomp.downcase
 
 case selection
-when "fahrenheit"
-  puts "#{convert_to_fahrenheit(celsius)} ºF"
-when "kelvin"
-  puts "#{convert_to_kelvin(celsius)} K"
-else
+  when "fahrenheit"
+  puts "#{celsius_to_fahrenheit(celsius)} ºF"
+  when "kelvin"
+  puts "#{celsius_to_kelvin(celsius)} K"
+  else
   puts "I don't know what that is..."
+  end
+
+puts "Now! new hotness...I can convert anything!"
+puts "Give me a temperature"
+temperature = gets.chomp.to_i
+
+puts "Now tell me what scale it is"
+scale = gets.chomp
+
+def converter
+puts "Now tell me what you want to convert it to!"
+$convert_to = gets.chomp
 end
+
+if scale == "kelvin"
+    converter
+    if $convert_to == "fahrenheit"
+      puts kelvin_to_fahrenheit(temperature)
+    else
+      puts kelvin_to_celsius(temperature)
+    end
+  elsif scale == "fahrenheit"
+    converter
+    if $convert_to == "kelvin"
+     puts fahrenheit_to_kelvin(temperature)
+    else
+      puts fahrenheit_to_celsius(temperature)
+   end
+  elsif scale == "celsius"
+    if $convert_to == "fahrenheit"
+      puts celsius_to_fahrenheit(temperature)
+    else
+      puts celsius_to_kelvin(temperature)
+    end
+  else
+    puts "you broke me!"
+end
+
+
