58a2556389ee4bc0ef0458c5c83de29af796db8e
My Name <myemail@example.com>
2013-09-24 20:24:04 -0400

    refactored

--- w01/d02/Paris/calculator.rb
+++ w01/d02/Paris/calculator.rb
@@ -1,73 +1,93 @@
-# A user should be given a menu of operations
-# A user should be able to choose from the menu
+
 def menu
-puts "(b)asic, (a)dvanced, or (q)uit:"
-$request = gets.chomp.downcase
-# A user should be able to enter numbers to perform the operation on
-# A user should be shown the result
-    if $request == "b"
-      def basic_calc
-      print "(a)dd, (s)ubtract, (m)ultiply, (d)ivide: "
-      end
-    basic_calc
-    $request2 = gets.chomp.downcase
-      if $request2 == "a"
-        puts "give me a number"
-        number1 = gets.chomp.to_i
-        puts "give me another number"
-        number2 = gets.chomp.to_i
-          puts "your answer is #{number1 + number2}"
-        end
-       if $request2 == "s"
-          puts "give me a number"
-          number3 = gets.chomp.to_i
-          puts "give me another number"
-          number4 = gets.chomp.to_i
-          puts "your answer is #{number3 - number4}"
-    end
-           if $request2 == "m"
-          puts "give me a number"
-          number5 = gets.chomp.to_i
-          puts "give me another number"
-          number6 = gets.chomp.to_i
-          puts "your answer is #{number5 * number6}"
-    end
-           if $request2 == "d"
-          puts "give me a number"
-          number7 = gets.chomp.to_i
-          puts "give me another number"
-          number8 = gets.chomp.to_i
-          puts "your answer is #{number7 / number8}"
-        end
-  end
-# Only tackle these once you get all of the basic_calc operations working
-# A user should be able to enter numbers to perform the operation on
-# A user should be shown the result
-     if $request == "a"
-        def advanced_calc
-        print "(p)ower, (s)qrt: "
-      end
-    advanced_calc
-      request3 = gets.chomp.downcase
-    if request3 == "p"
-    puts "give me a number"
-        number9 = gets.chomp.to_i
-        puts "give me another number"
-        number10 = gets.chomp.to_i
-        puts "your answer is #{number9**number10}"
-    end
-        if request3 == "s"
-        puts "give me a number"
-        number11 = gets.chomp.to_i
-        puts "your answer is #{Math.sqrt(number11)}"
-      end
-  end
+  puts "(b)asic, (a)dvanced, or (q)uit:"
+  $top_menu_choice = gets.chomp.downcase
 end
 
+def basic_calc
+  print "(a)dd, (s)ubtract, (m)ultiply, (d)ivide: "
+  $basic_operator_choice = gets.chomp.downcase
+  ask_for_number
+end
+
+def advanced_calc
+  print "(p)ower, (s)qrt: "
+  $advanced_operator_choice = gets.chomp.downcase
+  ask_for_number
+end
+
+# def ask_for_one_number
+#     puts "Give me a number."
+#     $users_first_number = gets.chomp.to_i
+# end
+
+def ask_for_number
+  puts "Give me a number."
+  $users_first_number = gets.chomp.to_i
+  puts "Give me another number."
+  $users_second_number = gets.chomp.to_i
+  operator
+end
+
+def add 
+  $users_first_number + $users_second_number
+end 
+
+def sub
+  $users_first_number - $users_second_number
+end
+
+def mult
+  $users_first_number * $users_second_number
+end
+
+def divide
+  $users_first_number / $users_second_number
+end  
+
+def power
+  $users_first_number ** $users_second_number
+end
+
+def sqrt
+  Math.sqrt($users_first_number )
+end
+
+# def $operator_one
+#   when $advanced_operator_choice == "s"
+#     puts "your answer is #{sqrt}"
+#   end 
+# end 
+
+def operator
+  if $basic_operator_choice == "a"
+    puts "your answer is #{add}"
+  end
+  if $basic_operator_choice == "s"
+    puts "your answer is #{sub}"
+  end
+  if $basic_operator_choice == "m"
+    puts "your answer is #{mult}"
+  end
+  if $basic_operator_choice == "d"
+    puts "your answer is #{divide}"
+  end
+  if $advanced_operator_choice == "p"
+    puts "your answer is #{power}"
+  end
+  # here I wasn't able to figure out how to ask for just one number and not two for square root.
+  if $advanced_operator_choice == "s"
+    puts "your answer is #{sqrt}"
+  end
+end 
 
 response = menu
 # This process should continue until the user selects a quit option from the menu
-while response != 'q'
-
-        response = menu
+  while response != 'q'
+  if response == "b"
+    basic_calc
+  end
+  if response == "a"
+    advanced_calc
+  end
 end
