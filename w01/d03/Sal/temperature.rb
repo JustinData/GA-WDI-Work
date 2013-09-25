337b78330e722f1fa5d270c1cfe4dcea3ae18a0a
slardizzone <slardizzone@gmail.com>
2013-09-25 09:40:59 -0400

    morning exercises

---
+++ w01/d03/Sal/temperature.rb
@@ -0,0 +1,12 @@
+def convert_to_fahrenheit(celsius)
+	farenheit = (celsius * 9) / 5 + 32
+	farenheit
+end
+
+def convert_to_kelvin(celsius)
+	kelvin = celsius + 273.15
+	kelvin
+end
+
+puts "Please enter a temperature in Celsius"
+
