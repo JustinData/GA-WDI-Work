4cf1c3d6f383c611c20a2b131b135979f8f09180
Arun Umapathy <arunumap@gmail.com>
2013-09-25 09:40:58 -0400

    morning ex

---
+++ w01/d03/Arun/temperature.rb
@@ -0,0 +1,16 @@
+
+	puts "Please enter the current temperature in Celcius"
+	celcius = gets.chomp.to_i
+	puts "Would you like to convert to (f)arenheit or (k)elvin?:"
+	temp = gets.chomp.to_i
+def convert_to_farenheit
+	celcius = gets.chomp.to_i
+	farenheit = 32 + (9/5) * celcius
+
+
+end
+
+def convert_to_kelvin
+
+	
+end
