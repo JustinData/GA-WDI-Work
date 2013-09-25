9425cfbfd39493fd4869b15acc2493bd02ef7371
Raleigh D. Froeber <RDFroeber@gmail.com>
2013-09-25 16:08:42 -0400

    Added lastest version of calc_teach.rb for ec

--- w01/d02/Raleigh/calc_teach.rb
+++ w01/d02/Raleigh/calc_teach.rb
@@ -1,4 +1,12 @@
 # Definitions
+
+# correct possible input for main menu
+good_input = ["b", "a", "q"]
+
+#will work also
+ok_input = {"basic"=>"b", "advanced"=>"a", "quit"=>"q"}
+
+# get_two_num asks for two numbers and then returns them
 def get_two_num 
   puts "give me a number"
   number1 = gets.chomp.to_i
@@ -7,21 +15,29 @@ def get_two_num
   both_num_output = [number1, number2] #RETURN as a list(array)
 end
 
+# basic_calc prints add, subtract, multiply... and gets input
 def basic_calc
   print "(a)dd, (s)ubtract, (m)ultiply, (d)ivide: "
   user_input = gets.chomp.downcase
   # "return user_input" is implied (Ruby methods ALWAYS returns the last expression)
 end
 
+# advanced_calc prints...
 def advanced_calc
   print "(p)ower, (s)qrt: "
 end
 
-def menu
+def menu(ok_input, map_input)
   puts "(b)asic, (a)dvanced, or (q)uit:"
-  request1 = gets.chomp.downcase
-  
-  if request1 == "b"
+  $request1 = gets.chomp.downcase
+
+  mapped_request = map_input[$request1]
+
+    if mapped_request != nil
+      $request1 = mapped_request
+    end
+
+  if $request1 == "b"
     # prints add, subtract, multiply, divide & gets user_input
     request2 = basic_calc
 
@@ -38,10 +54,10 @@ def menu
     when "d"
       puts "your answer is #{both_num[0] / both_num[1]}"
     end
-    
+
   end
 
-    if request1 == "a"
+    if $request1 == "a"
       # prints power, sqrt
       advanced_calc
       request3 = gets.chomp.downcase
@@ -64,7 +80,7 @@ end
 
 # Program Begins
  
-response = menu
+response = menu(good_input, ok_input)
 
 # This process should continue until the user selects a quit option from the menu
 while response != 'q'
