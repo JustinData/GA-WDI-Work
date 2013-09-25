b720fd91a4321a94d2c41dff6c35ee571e94e0f0
Ernie Casilla <ecasilla@icloud.com>
2013-09-25 09:41:08 -0400

    Morning Excerise

---
+++ w01/d03/Ernie/tip.rb
@@ -0,0 +1,13 @@
+puts "Please enter the price of your meal?"
+
+puts "Please enter your desrired tip amount?"
+
+puts "This is your new total including all charges!"
+
+def meal_price(num)
+
+end
+
+def tax_amount()
+  
+end
