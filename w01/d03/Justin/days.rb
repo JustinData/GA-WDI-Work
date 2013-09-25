fdf4209ec7cb886ef8f7fa452449813bb2e22ab8
JustinGA <justinkemmerer@gmail.com>
2013-09-25 09:41:38 -0400

    moring ex, not quite done yet

---
+++ w01/d03/Justin/days.rb
@@ -0,0 +1,19 @@
+puts "What day of the week is it?"
+day = gets.chomp.downcase
+
+case day
+when "sunday"
+	puts "Football!!"
+when "monday"
+	puts "Get busy."
+when "tuesday"
+	puts "Something, something..."
+when "wednesday"
+	puts "Ride a camel today."
+when "thursday"
+	puts "Almost there."
+when "friday"
+	puts "TGIF!"
+when "saturday"
+	puts "Sleep in!"
+end
