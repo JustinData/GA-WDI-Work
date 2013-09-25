3407c2a1a0123c03cf597381c3ae2d66fa2cc280
millzpaugh <ann.millspaugh@gmail.com>
2013-09-25 14:38:37 -0400

    new changes to folder D3

--- w01/d03/Ann/temperature.rb
+++ w01/d03/Ann/temperature.rb
@@ -1,24 +1,26 @@
 puts "What is the temperature in Celsius?"
-temp_celsius = gets.chomp.to_i 
+temp_celsius = gets.chomp.to_f 
 
 puts "Would you like to convert this to (f)ahrenheit or (k)elvin?"
 conversion = gets.chomp.downcase 
 
-case conversion 
-when "f"
-  convert_to_fahrenheit
-when "k"
-  convert_to_kelvin
-end 
-
-def convert_to_fahrenheit (temp_celsius)
-  temp_celsius * (9/5) + 32 
-  puts "#{temp_celsius} degrees Celsius is equivalent to #{temp_celsius * (9/5) + 32} degrees Celsius."
+def convert_to_fahrenheit (c)
+   return puts (c * 9/5) + 32 
+  #puts "#{temp_celsius} degrees Celsius is equivalent to #{temp_celsius * (9/5) + 32} degrees Celsius."
 end
 
 def convert_to_kelvin 
   puts "#{temp_celsius} degrees Celsius is equivalent to #{temp_celsius + 273.15} kelvin."
 end 
+
+case conversion 
+when "f"
+  #puts convert_to_fahrenheit(temp_celsius)
+  puts "The #{temp_celsius} is equivalent to #{convert_to_fahrenheit (temp_celsius)} degrees F."
+when "k"
+  puts convert_to_kelvin
+end 
+
 
 
 
