73b280ced708170ae3c7d3348729a7de6b8378ee
Peter Lai <peter.yd.lai@gmail.com>
2013-09-24 23:11:36 -0400

    w1d3 folders

--- w01/d03/Peter/README.md
+++ w01/d03/Peter/README.md
@@ -1,2 +1,1 @@
-Name: Peter Lai
-Github: peterylai
+Peter Lai
--- w01/d03/Peter/name_my_pet.rb
+++
@@ -1,9 +0,0 @@
-puts "Please enter an animal type"
-animal = gets.chomp
-puts "Please enter your age"
-age = gets.chomp.to_i
-puts "Please enter your name"
-name = gets.chomp
-puts "#{name} the #{animal} is #{age} years old"
-
-puts name + " the " + animal + " is " + age + " years old"
