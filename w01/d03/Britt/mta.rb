caf539ddf0163ea69e3323ccc38491b8aab40791
brittlewis12 <brittlewis12@gmail.com>
2013-09-25 17:10:17 -0400

    pseudo code for phase 1 complete

---
+++ w01/d03/Britt/mta.rb
@@ -0,0 +1,21 @@
+=begin
+	
+N Line Stops Array: TS, 34th, 28th-N, 23rd-N, US, 8th-N
+
+Ask user what stop they're getting on at?
+puts N line array to display options
+
+Set variable start equal to user input
+
+Ask user what stop they're getting off at
+puts N line array to display options
+
+Get variable end equal to user input
+
+Get indeces of start and stop using Array#index("stop_name")
+
+Subtract indeces from one another and return the absolute value
+
+Puts distance between the stops
+	
+=end
