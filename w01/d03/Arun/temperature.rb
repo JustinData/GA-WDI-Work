a52dc6962bb39d2dc30a7b9c639218721d294456
Arun Umapathy <arunumap@gmail.com>
2013-09-26 09:09:19 -0400

    Finalized temp app

--- w01/d03/Arun/temperature.rb
+++ w01/d03/Arun/temperature.rb
@@ -2,15 +2,18 @@
 	puts "Please enter the current temperature in Celcius"
 	celcius = gets.chomp.to_i
 	puts "Would you like to convert to (f)arenheit or (k)elvin?:"
-	temp = gets.chomp.to_i
-def convert_to_farenheit
-	celcius = gets.chomp.to_i
-	farenheit = 32 + (9/5) * celcius
-
-
+	temp = gets.chomp.downcase
+def convert_to_farenheit (c)
+	(c * 9/5) + 32
 end
 
-def convert_to_kelvin
-
+def convert_to_kelvin (c)
+	c + 273
 	
+end
+case temp 
+when "f"
+	puts " #{convert_to_farenheit(celcius)} in farenheit"
+when "k"
+	puts "#{convert_to_kelvin(celcius)} in kelvin"
 end
