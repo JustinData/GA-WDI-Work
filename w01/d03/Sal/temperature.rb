d155f4136f8511ff0fc2dae3090c47dcf298ee26
slardizzone <slardizzone@gmail.com>
2013-09-26 09:00:26 -0400

    Complete HW W01 D03

--- w01/d03/Sal/temperature.rb
+++ w01/d03/Sal/temperature.rb
@@ -1,12 +1,24 @@
-def convert_to_fahrenheit(celsius)
-	farenheit = (celsius * 9) / 5 + 32
-	farenheit
+def convert_to_fahrenheit(c)
+	(c * 9/5) + 32
 end
 
-def convert_to_kelvin(celsius)
-	kelvin = celsius + 273.15
-	kelvin
+def convert_to_kelvin(c)
+	c + 273 
 end
 
-puts "Please enter a temperature in Celsius"
+puts "Enter a temperature in Celsius"
+temperature_celsius = gets.chomp.to_f
+
+puts "Do you want to convert to (f)arenheit or (k)elvin?"
+temperature_type = gets.chomp.downcase
+
+
+case temperature_type 
+	when "f"
+		puts convert_to_fahrenheit(temperature_celsius)
+	when "k"
+		puts convert_to_kelvin(temperature_celsius)
+	else 
+		puts "please choose f or k"
+end
 
