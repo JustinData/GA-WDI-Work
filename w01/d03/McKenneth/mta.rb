2f30069416533da7f9f3914bf7c5a06eb237c9b5
DrRobotmck <mckenneth.scott@icloud.com>
2013-09-25 22:05:14 -0400

    Added comments, scared to re-factor, one-train transfer started but returns 'nil' value

--- w01/d03/McKenneth/mta.rb
+++ w01/d03/McKenneth/mta.rb
@@ -1,9 +1,14 @@
+
+#######DATA USED########
 n_train = ["times square", "34th", "28th-n", "23rd-n", "union square"]
 l_train = ["8th", "6th", "union square", "3rd", "1st"]
 s_train = ["grand central", "33rd", "28th-s", "23rd-s", "union square"]
-mta= {n: n_train, l: l_train, s: s_train}
-trains = ["(N) train", "(L) train","(S)ix train"]
+o_train = ["59th", "50th", "times square", "34th", "28th"]
+mta= {n: n_train, l: l_train, s: s_train, o: o_train}
+trains = ["(N) train", "(L) train","(S)ix train", "(O)ne train"]
 
+
+########Start of  SUBWAY SYSTEM#########
 puts "Welcome to the MTA Subway System! The best darn train system this side of the Hudson!"
 puts "These are the trains you can take:"
 
@@ -11,12 +16,14 @@ puts trains
 
 puts "What train do you want to start your trip on??"
 print ">"
+
 train_choice = gets.chomp.downcase.to_sym
 
 puts "What stop do you want to start at??"
 puts mta[train_choice][0..5]
 print ">"
 
+#gets the start point for the user
 start_trip = gets.chomp.downcase
   case start_trip
     when mta[train_choice][0]
@@ -33,28 +40,31 @@ start_trip = gets.chomp.downcase
        puts "That's not a stop!"
     end
 
+#determines what to do for transfers
 puts "Do you want to [stay] on this train or [transfer]"
 transfer = gets.chomp
   if transfer == "transfer"
     
-    point_of_transfer = "union square"
-      case point_of_transfer
-      when mta[train_choice][4]
-        trip_1 = (4 -choice_1)
-      when mta[train_choice][2]
-        trip_1 = (2-choice_1).abs
-      end
-    
     puts "Which train do you want to transfer to?"
     puts trains
     print ">"
-    transfer_to = gets.chomp.to_sym
+    transfer_to = gets.chomp.downcase.to_sym
+
+#compares arrays to find the transfer station and tallies stops for the first leg
+    point_of_transfer = mta[train_choice] & mta[transfer_to]
+      case point_of_transfer[0]
+        when mta[train_choice][4]
+            trip_1 = (4 -choice_1)
+        when mta[train_choice][2]
+            trip_1 = (2-choice_1).abs
+      end
     
     puts "Where do you want to go??"
     puts mta[transfer_to][0..5]
     print ">"
-    last_stop= gets.chomp.downcase
 
+#gets the final destination from the user
+    last_stop= gets.chomp.downcase
     case last_stop
           when mta[transfer_to][0]
             choice_2 = 0
@@ -69,9 +79,10 @@ transfer = gets.chomp
           else
             puts "That's not a stop!"
           end
-          
-      post_transfer = "union square"
-      case post_transfer
+
+#determines the trip length for the second leg         
+      post_transfer = mta[train_choice] & mta[transfer_to]
+      case post_transfer[0]
       when mta[transfer_to][4]
         trip_2 = 4 - choice_2
       when mta[transfer_to][2]
@@ -80,26 +91,26 @@ transfer = gets.chomp
       puts "That's a trip of #{trip_1+trip_2} stops"
 
   else
+
+#handles single-line trips (no transfers)    
     puts "Where do you want to go??"
     puts mta[train_choice][0..5]
     print ">"
     end_trip = gets.chomp.downcase
           case end_trip
           when mta[train_choice][0]
-            choice_2 = 0
+             choice_2 = 0
           when mta[train_choice][1]
-            choice_2 = 1
+              choice_2 = 1
           when mta[train_choice][2]
-            choice_2 = 2
+              choice_2 = 2
           when mta[train_choice][3]
-            choice_2 = 3
+              choice_2 = 3
           when mta[train_choice][4]
-            choice_2 = 4
+              choice_2 = 4
           else
-            puts "That's not a stop!"
+              puts "That's not a stop!"
           end
-          puts "That's a trip of #{trip_length = (choice_1 - choice_2).abs} stops"
+          puts "That's a trip of #{(choice_1 - choice_2).abs} stops"
 
   end
-
-
