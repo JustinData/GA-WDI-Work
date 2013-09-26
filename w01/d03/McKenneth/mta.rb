073551b2255cea99e3a3ba802f46bb6c663ee894
DrRobotmck <mckenneth.scott@icloud.com>
2013-09-25 20:40:02 -0400

    Added functionality for 3 lines. needs to be refactored

--- w01/d03/McKenneth/mta.rb
+++ w01/d03/McKenneth/mta.rb
@@ -1,28 +1,105 @@
-n_train = ["Times Square", "34th Street", "28th Street -N", "23rd Street - N", "Union Square"]
-l_train = ["8th Avenue", "6th Avenue", "Union Square", "3rd Ave", "1st Ave"]
-s_train = ["Grand Central", "33rd Street", "28th Street - 6 line", "23rd Street - 6 line", "Union Square"]
+n_train = ["times square", "34th", "28th-n", "23rd-n", "union square"]
+l_train = ["8th", "6th", "union square", "3rd", "1st"]
+s_train = ["grand central", "33rd", "28th-s", "23rd-s", "union square"]
 mta= {n: n_train, l: l_train, s: s_train}
-i=0
-
+trains = ["(N) train", "(L) train","(S)ix train"]
 
 puts "Welcome to the MTA Subway System! The best darn train system this side of the Hudson!"
 puts "These are the trains you can take:"
 
+puts trains
 
-while i < n_train.length
-  print (i + 1).to_s + " - "
-  puts n_train[i]
-  i += 1
-end
-
-puts "Where do you want to start your trip??"
+puts "What train do you want to start your trip on??"
 print ">"
-choice_1 = gets.chomp.to_i - 1
+train_choice = gets.chomp.downcase.to_sym
 
-puts "Where do you want to go??"
+puts "What stop do you want to start at??"
+puts mta[train_choice][0..5]
 print ">"
-choice_2 = gets.chomp.to_i - 1
 
-trip_length = (choice_1 - choice_2).abs
-puts "OK! So you want to start at #{n_train[choice_1]} and go to #{n_train[choice_2]}"
-puts "That would be #{trip_length} stops!"
+start_trip = gets.chomp.downcase
+  case start_trip
+    when mta[train_choice][0]
+        choice_1 = 0
+    when mta[train_choice][1]
+        choice_1 = 1
+    when mta[train_choice][2]
+        choice_1 = 2
+    when mta[train_choice][3]
+        choice_1 = 3
+    when mta[train_choice][4]
+        choice_1 = 4
+    else
+       puts "That's not a stop!"
+    end
+
+puts "Do you want to [stay] on this train or [transfer]"
+transfer = gets.chomp
+  if transfer == "transfer"
+    
+    point_of_transfer = "union square"
+      case point_of_transfer
+      when mta[train_choice][4]
+        trip_1 = (4 -choice_1)
+      when mta[train_choice][2]
+        trip_1 = (2-choice_1).abs
+      end
+    
+    puts "Which train do you want to transfer to?"
+    puts trains
+    print ">"
+    transfer_to = gets.chomp.to_sym
+    
+    puts "Where do you want to go??"
+    puts mta[transfer_to][0..5]
+    print ">"
+    last_stop= gets.chomp.downcase
+
+    case last_stop
+          when mta[transfer_to][0]
+            choice_2 = 0
+          when mta[transfer_to][1]
+            choice_2 = 1
+          when mta[transfer_to][2]
+            choice_2 = 2
+          when mta[transfer_to][3]
+            choice_2 = 3
+          when mta[transfer_to][4]
+            choice_2 = 4
+          else
+            puts "That's not a stop!"
+          end
+          
+      post_transfer = "union square"
+      case post_transfer
+      when mta[transfer_to][4]
+        trip_2 = 4 - choice_2
+      when mta[transfer_to][2]
+        trip_2 = (2-choice_2).abs
+      end
+      puts "That's a trip of #{trip_1+trip_2} stops"
+
+  else
+    puts "Where do you want to go??"
+    puts mta[train_choice][0..5]
+    print ">"
+    end_trip = gets.chomp.downcase
+          case end_trip
+          when mta[train_choice][0]
+            choice_2 = 0
+          when mta[train_choice][1]
+            choice_2 = 1
+          when mta[train_choice][2]
+            choice_2 = 2
+          when mta[train_choice][3]
+            choice_2 = 3
+          when mta[train_choice][4]
+            choice_2 = 4
+          else
+            puts "That's not a stop!"
+          end
+          puts "That's a trip of #{trip_length = (choice_1 - choice_2).abs} stops"
+
+  end
+
+
