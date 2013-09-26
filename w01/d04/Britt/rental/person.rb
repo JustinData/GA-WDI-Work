8704309c0761a889c94d83e7b106853264162001
brittlewis12 <brittlewis12@gmail.com>
2013-09-26 18:20:59 -0400

    adding require_relative to main.rb

--- w01/d04/Britt/rental/person.rb
+++ w01/d04/Britt/rental/person.rb
@@ -10,4 +10,7 @@ end
 
 
 
-# only create attr_accessor for attributes you'd like to be able to change/write USING the method_name_set=('stuff') syntax
+# only create attr_accessor for
+# attributes you'd like to be
+# able to change/write USING the
+# method_name_set=('stuff') syntax
