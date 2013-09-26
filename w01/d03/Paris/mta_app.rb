93a871d73619424cf925c3b4a257f734ecbd6522
My Name <myemail@example.com>
2013-09-25 23:15:00 -0400

    mta.rb with l line and n line...I think it works...??

--- w01/d03/Paris/mta_app.rb
+++ w01/d03/Paris/mta_app.rb
@@ -1,60 +1,59 @@
 n = ['times square', '34th', '28th-n', '23rd-n', 'union station']
-# l = ['8th', '6th', 'us', '3rd', '1st']
-# s = ['gc', '33rd', '28th-s', '23rd-s', 'us']
-# #mta = {'n' => n, 'l' => l, 's' => s}
-
-# def print_line_stops(x)
-#   stop_on
-# end
-
-# def ask_for_stop
-#   puts "What stop would you like to get on?"
-#   stop_on = gets.chomp.downcase
-#   puts "What stop would you like to get off?"
-#   stop_off = gets.chomp.downcase
-# end
-
-#PHASE 2
-# puts "What line would you like to use?"
-# line_on = gets.chomp.downcase
-# line_on = ask_for_stop
-
+l = ['8th', '6th', 'us', '3rd', '1st']
+s = ['gc', '33rd', '28th-s', '23rd-s', 'us']
+#mta = {'n' => n, 'l' => l, 's' => s}
 
 # mta = {}
 # mta[:n] = n
 # mta[:l] = l
 # mta[:s] = s
 
-puts "Welcome to the N line. Here are your stops: #{n}"
+#PHASE 2
+########## THIS BLOCK ASKS FOR THE LINE ON AND STOP ON #############
+
+puts "What line would you like to get on?"
+line_on = gets.chomp.downcase
+
+if line_on == "n"
+  puts "Welcome to the N line. Here are your stops: 'times square', '34th', '28th-n', '23rd-n', 'union station'."
+else line_on == "l"
+  puts  "Welcome to the L line. Here are your stops: '8th', '6th', 'union station', '3rd', '1st'."
+end 
 
 puts "What stop would you like to get on?"
 stop_on = gets.chomp.downcase
+
+########## THIS BLOCK ASKS FOR THE LINE OFF AND STOP OFF #############
+
+puts "What line would you like to get off?"
+line_off = gets.chomp.downcase
+
+if line_off == "n"
+  puts "Here are your stops: 'times square', '34th', '28th-n', '23rd-n', 'union station'."
+elsif line_off == "l"
+  puts  "Here are your stops: '8th', '6th', 'union station', '3rd', '1st'."
+end
+
 puts "What stop would you like to get off?"
 stop_off = gets.chomp.downcase
 
-n_hash = Hash[n.map.with_index.to_a]
-stop_on_index = n_hash[stop_on]
-stop_off_index = n_hash[stop_off]
+########## THIS BLOCK CALCULATES THE NUMBER OF STOPS PER LINE AND ADDS THEM TOGETHER. #############
 
-find_difference = (stop_off_index.to_i - stop_on_index.to_i).abs
-
-puts "There are #{find_difference} stops on your trip. Bon voyage."
-
-#the method that triggers the appropriate array.
-# if line_on == "n"
-#   print_n
-# elsif line_on == "l" 
-#   puts "Which stop would you like on the #{l}"
-# else line_on == "s"
-#   puts "Which stop would you like on the #{s}" 
-# end 
+if line_on || line_off == "n"
+    n_hash = Hash[n.map.with_index.to_a]
+    n_stop_on_index = n_hash[stop_on] 
+    n_stop_off_index = n_hash[stop_off] 
+    total_n_line_stops = (n_stop_on_index.to_i - n_stop_off_index.to_i).abs 
+    puts "these are your n stops: #{total_n_line_stops}"
+end
 
 
+if line_on || line_off == "l"
+  l_hash = Hash[l.map.with_index.to_a]
+  l_stop_on_index = l_hash[stop_on] 
+  l_stop_off_index = l_hash[stop_off] 
+  total_l_line_stops = (l_stop_on_index.to_i - l_stop_off_index.to_i).abs
+  puts "these are your l stops: #{total_l_line_stops}"
+end
 
-#PHASE 2
-
-# create a case to capture the desired line.
-# ask for stop_on
-# ask for stop_off
-
-
+puts "Your total subway stops are: #{ (total_n_line_stops.to_i + total_l_line_stops.to_i).abs }."
