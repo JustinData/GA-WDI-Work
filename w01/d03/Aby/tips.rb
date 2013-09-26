18444cc14bcda5b51da8e750b9d8b0268cc11d66
Aby Bandoh <aby.bandoh@gmail.com>
2013-09-25 21:39:17 -0400

    day 3 homework, second try

--- w01/d03/Aby/tips.rb
+++ w01/d03/Aby/tips.rb
@@ -1,10 +1,9 @@
 puts "Enter the price of your meal"
 price = gets.chomp.to_f
 puts "What's the tax where you are?"
-tax = gets.chomp.to_f
+tax = gets.chomp.to_f / 100
 puts "what percent would you like to tip"
-tip = gets.chomp.to_f 
-pwt = price + (price *  tax/100)
-pwtat = price + pwt + (pwt * tip/100)
-total = price + pwt + pwtat
-puts "Your total is #{total}"
+tip = gets.chomp.to_f / 100
+pwt = price + (price * tax)
+pwtat = pwt + (pwt * tip)
+puts "Your total is #{pwtat.round(2)}"
