4af0103a00bf42179a93cbab33e3e809de4b0947
David Fox Powell <dtothefp@gmail.com>
2013-09-26 10:10:49 -0400

    Second attempt to add calculator_refactor.rb

--- w01/d03/David/calculator_refactor.rb
+++ w01/d03/David/calculator_refactor.rb
@@ -1,23 +1,7 @@
-#correct possible input for main menu
-good_input = ["b", "a", "q"]
-
-#input that will also work
-ok_input = {"basic" => "b", "advanced" => "a", "quit" => "q"}
-
 #prints initial menu for calculator
-def menu(input_list, map_input)
+def menu
   puts "(b)asic, (a)dvanced, or (q)uit:"
-  request = gets.chomp.downcase
-  while request != (input_list[0] ||  input_list[1] || input_list[2] )
-    mapped_request = map_input[request]
-    if mapped_request != nil
-      request = mapped_request
-    else
-      puts "Please enter 'b', 'a', or 'q'"
-      request = gets.chomp.downcase
-    end
-  end
-  return request
+  request= gets.chomp.downcase
 end
 
 #prints the basic calulator menu prompt a/s/m/d
@@ -40,40 +24,57 @@ end
     both_numbers_output = [number1, number2]
   end
 
-  # response = menu(good_input, ok_input)
+  puts menu
+  request = menu
 
-  # case response
-  #   when "b"
-  #     #prints add/sub/mult/etc....
-  #     basic_calc
-  #     #gets two numbers from the user
-  #     both_numbers = get_two_numbers
-  #     if request == "a"
-  #      puts "your answer is #{both_numbers[0] + both_numbers[1]}"
-  #     elsif request == "s"
-  #       puts "your answer is #{both_numbers[0] - both_numbers[1]}"
-  #     elsif request == "m"
-  #       puts "your answer is #{both_numbers[0] * both_numbers[1]}"
-  #     elsif request == "d"
-  #       puts "your answer is #{both_numbers[0] / both_numbers[1]}"
-  #     end
-  #   when "a"
-  #     advanced_calc
-  # end
+  if request== "b"
+
+    #prints add/sub/mult/etc....
+    basic_calc
+    #gets two numbers from the user
+    both_numbers = get_two_numbers
+    if request == "a"
+     puts "your answer is #{both_numbers[0] + both_numbers[1]}"
+    elsif request == "s"
+      puts "your answer is #{both_numbers[0] - both_numbers[1]}"
+    elsif request == "m"
+      puts "your answer is #{both_numbers[0] * both_numbers[1]}"
+    elsif request == "d"
+      puts "your answer is #{both_numbers[0] / both_numbers[1]}"
+    end
+  end
 
  
+# Only tackle these once you get all of the basic_calc operations working
+# A user should be able to enter numbers to perform the operation on
+# A user should be shown the result
+if request== "a"
 
+#prints advanced calculator menut
+advanced_calc
+
+request3 = gets.chomp.downcase
+if request3 == "p"
+  puts "give me a number"
+  number9 = gets.chomp.to_i
+  puts "give me another number"
+  number10 = gets.chomp.to_i
+  puts "your answer is #{number9**number10}"
+end
+if request3 == "s"
+  puts "give me a number"
+  number11 = gets.chomp.to_i
+  puts "your answer is #{Math.sqrt(number11)}"
+end
+end
+end
 
 
 ######### DEFINITIONS OVER....PROGRAMS BEGIN #########
-response = menu(good_input, ok_input)
 
-
-#This process should continue until the user selects a quit option from the menu
+response = menu
+# This process should continue until the user selects a quit option from the menu
 while response != 'q'
-  if response == "b"
-    basic_calc
-  elsif response == "a"
-    advanced_calc
-  end
+
+        response = menu
 end
