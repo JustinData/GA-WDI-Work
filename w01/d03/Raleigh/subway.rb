0bca343ba4da298edd7975c64115dba03b3c28a6
Raleigh D. Froeber <RDFroeber@gmail.com>
2013-09-26 00:01:51 -0400

    MTA: Phase 4 (bonus) complete. Bug found: end of lines throwing nil

--- w01/d03/Raleigh/subway.rb
+++ w01/d03/Raleigh/subway.rb
@@ -1,21 +1,23 @@
 # MTA Subway App
 
-###Definitions Begin
-
 # N Subway Line
-line_n = ["ts", "34th", "28th-n", "23rd-n", "us", "8th"]
+line_n = ["Times Square", "34th", "28th-North", "23rd-North", "Union Square", "8th"]
 # L Subway Line
-line_l = ["8th", "6th", "us", "3rd", "1st"]
+line_l = ["8th", "6th", "Union Square", "3rd", "1st"]
 # 6 (Six) Subway Line
-line_six = ["gc", "33rd", "28th-s", "23rd-s", "us", "astor"]
+line_six = ["Grand Central", "33rd", "28th-South", "23rd-South", "Union Square", "Astor"]
+# 1 (One) Subway Line
+line_one = ["59th", "50th", "Times Square", "34th", "28th"]
+
+# Good Line Inputs
 
 def user_choice_line_start
-   puts "What line would you like to get on at? The N, L, or Six: "
+   puts "What line would you like to get on at? The N, L, Six, or One: "
    user_choice_line_start = gets.chomp.downcase
 end
 
 def user_choice_line_end
-   puts "What line would you like to get off at? The N, L, or Six: "
+   puts "What line would you like to get off at? The N, L, Six, or One: "
    user_choice_line_stop = gets.chomp.downcase
 end
 
@@ -26,60 +28,81 @@ end
 
 def get_off
    puts "\nWhat stop would you like to get off at?"
-   user_stop = gets.chomp
+   user_stop = gets.chomp.downcase
 end
-### Definitions End
 
+########### DEFINITIONS OVER, PROGRAM BEGINS ##############
 
-### Program Begins
 puts "\n***MTA***\n"
 
+# Gets users starting line
 user_line_start = user_choice_line_start
 
 case user_line_start
 when "n"
-   puts "N stops: " + line_n[0..7].join(", ")
+   puts "N stops: " + line_n[0..5].join(", ")
    line_start = line_n
 when "l"
-   puts "L stops: " + line_l[0..6].join(", ")
+   puts "L stops: " + line_l[0..4].join(", ")
    line_start = line_l
 when "six"
-   puts "Six stops: " + line_six[0..7].join(", ")
+   puts "Six stops: " + line_six[0..5].join(", ")
    line_start = line_six
+when "one"
+   puts "One stops: " + line_one[0..4].join(", ")
+   line_start = line_one
 else
    puts "That line is not an option."
-   line_start
+   user_choice_line_start
 end
 
+# Gets users beginning stop on line
 user_start = get_on
 
+# Gets users ending line
 user_line_end = user_choice_line_end
 
 case user_line_end
 when "n"
-   puts "N stops: " + line_n[0..7].join(", ")
+   puts "N stops: " + line_n[0..5].join(", ")
    line_stop = line_n
 when "l"
-   puts "L stops: " + line_l[0..6].join(", ")
+   puts "L stops: " + line_l[0..4].join(", ")
    line_stop = line_l
 when "six"
-   puts "Six stops: " + line_six[0..7].join(", ")
+   puts "Six stops: " + line_six[0..5].join(", ")
    line_stop = line_six
+when "one"
+   puts "One stops: " + line_one[0..4].join(", ")
+   line_stop= line_one
 else
    puts "That line is not an option."
+   user_choice_line_end
 end
 
+# Gets users end stop on line
 user_stop = get_off
 
+# Calculate number of stops 
 if user_line_start == user_line_end
    total_stops = (line_start.index(user_start) - line_stop.index(user_stop)).abs
+elsif (user_line_start == "one" && user_line_end == "n") || (user_line_start == "n" && user_line_end == "one") 
+   total_stops = ((line_start.index(user_start) - line_start.index("Times Square"))).abs + ((line_stop.index(user_stop) - line_stop.index("Times Square"))).abs
+elsif user_line_start == "one" && (user_line_end == "l" || user_line_end == "six")
+   puts "\nSorry those lines do not connect."
+   total_stops = "?"
+elsif user_line_end == "one" && (user_line_start == "l" || user_line_start == "six")
+   puts "\nSorry those lines do not connect."
+   total_stops = "?"
 else
-   total_stops = ((line_start.index(user_start) - line_start.index("us"))).abs + ((line_stop.index(user_stop) - line_stop.index("us"))).abs
-   
+   #unless total_stops.nil?
+      total_stops = ((line_start.index(user_start) - line_start.index("Union Square"))).abs + ((line_stop.index(user_stop) - line_stop.index("Union Square"))).abs
+   #end
 end
 
+# Prints number of stop(s)
 if total_stops == 1
-   puts "You have #{total_stops} stop."  
+   puts "You have #{total_stops} stop." 
 else
    puts "You have #{total_stops} stops."  
 end
