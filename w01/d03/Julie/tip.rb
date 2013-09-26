35afaec53ac2164cd78b92d9f770fa31817e8b1c
Julie lappano <julie.lappano@gmail.com>
2013-09-25 21:36:35 -0400

    homework 01-03

--- w01/d03/Julie/tip.rb
+++ w01/d03/Julie/tip.rb
@@ -1,26 +1,13 @@
 puts "How much was your meal?"
-price = gets.chomp.to_i
+price = gets.chomp.to_f
 puts "What is the tax percentage on your bill?"
-tax = gets.chomp.to_i
+tax = gets.chomp.to_f
 puts "What percentage would you like to tip?"
-percent_tip = gets.chomp.to_i
+percent_tip = gets.chomp.to_f
+tax_amount = (price * tax) / 100
+total_with_tax = (tax_amount + price)
+tip_amount = (price * percent_tip) / 100
+total_with_tip = (tip_amount + total_with_tax)
 
-
-def find_total_with_tax (price, tax)
-   tax_amount = (price * tax) / 100
-   $total_with_tax = (tax_amount + price)
-   puts total_with_tax
-end 
-
-
-
-
-def find_total_with_tip (total_with_tax)
-  tip_amount = ($total_with_tax * percent_tip) / 100
-  total_with_tip = (tip_amount + total_with_tax)
-end
-
-find_total_with_tax(price, tax)
-find_total_with_tip ($total_with_tax)
 
 puts "Your bill is #{total_with_tip}"
