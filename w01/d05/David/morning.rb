5c33cac10c31601c87ff936440e7d91e6dbd63bf
David Fox Powell <dtothefp@gmail.com>
2013-09-27 09:37:21 -0400

    Complete morning exercise

---
+++ w01/d05/David/morning.rb
@@ -0,0 +1,25 @@
+###A
+puts "Exercise A"
+a = ["Peter", "PJ", "Jeff"]
+puts a[0]
+a.push "David"
+print a
+a.delete("PJ")
+print a
+
+
+  #B
+  puts "Exercise B"
+  h = {0 => "Zero", 1 => "One", :two => "Two", "two" => 2}
+  puts h[1]
+  puts h.key(2)
+  puts h["two"]
+  h[3] = "three"
+  print h
+  h[:four] = 4
+  print h
+  h.delete(1)
+  print h
+
+  
+  
