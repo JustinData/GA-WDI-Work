cbb195b01285d57b293e9abd1a4be46e963f61ac
brittlewis12 <brittlewis12@gmail.com>
2013-09-27 14:53:16 -0400

    started main.rb with created Shelter object 'HappyTails'

---
+++ w01/d05/Britt/lab/main.rb
@@ -0,0 +1,11 @@
+require_relative 'animal'
+require_relative 'client'
+require_relative 'shelter'
+
+# creates a shelter automaticall
+# menu
+	# create an animal
+	# create a shelter
+	# quit
+
+my_shelter = Shelter.new("HappiTails")
