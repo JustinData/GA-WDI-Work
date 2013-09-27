8647abbd96e1d0907b4eae39eacb73f2802515e5
brittlewis12 <brittlewis12@gmail.com>
2013-09-27 15:13:53 -0400

    added basic menu method that simply puts the menu options.

--- w01/d05/Britt/lab/main.rb
+++ w01/d05/Britt/lab/main.rb
@@ -10,4 +10,14 @@ require 'pry'
 	# create a shelter
 	# quit
 
+# currently JUST puts the menu.
+# actions and conditionals to
+# be added later
+def menu
+	puts "*****  Welcome to HappiTales!  *****"
+	puts "1. Add a new animal"
+	puts "2. Add a new client"
+	puts "3. Create a new shelter"
+end
+
 binding.pry
