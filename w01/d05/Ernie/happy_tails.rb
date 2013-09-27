d575d2839c43873335c8980907743b88373d07c0
Ernie Casilla <ecasilla@icloud.com>
2013-09-27 17:31:14 -0400

    Newest Version

--- w01/d05/Ernie/happy_tails.rb
+++ w01/d05/Ernie/happy_tails.rb
@@ -1,100 +1,101 @@
 class Animal
           
-          def initialize(name,species,toys)
-                @name = name
-                @species = species
-                @toys = []
-          end
-         
-          def name
-               @name
-          end
+    def initialize(name,species)
+          @name = name
+          @species = species
+          @toys = []
+    end
+   
+    def name
+         @name
+    end
 
-          def species
-               @species
-          end
+    def species
+         @species
+    end
+    def list_toys
+          @toys.join(" , ")
+  end
 
-          def list_toys
-                @toys.join(" , ")
-        end
-
-         def to_s
-           "Im #{self.name} whose a #{self.species} with #{@toys.count}"
-         end
+   def to_s
+     "Im #{self.name} whose a #{self.species} with #{@toys.count}"
+   end
 end
 
 # dog = Animal.new("boobie","dog","bone")
 
 class Client
-        
-        def initialize(name,age,animal)
-          @name = name
-          @age = age
-          @animals = animals
-        end
-        
-        def name
-             @name
-        end
-        
-        def age
-             @age
-        end
-        
-        def animal
-              @animal.join(" , ")
-        end
+    
+    def initialize(name, age)
+      @name = name
+      @age = age
+      @pets = []
+    end
+    
+    def name
+         @name
+    end
+    
+    def age
+         @age
+    end
+    
+    def pets
+         @pets
+    end
+    
+    def animals
+          @animals.join(" , ")
+    end
 
-        def to_s
-            "Im #{self.name} whose #{self.age} with these many #{self.animal}"
-        end
+    def to_s
+        "Im #{self.name} whose #{self.age} with these many #{self.pets}"
+    end
 
-        # ernie = Client.new("ernie",27,["Dog","Cat"])
-        # puts ernie
+  
   
 end
 
 class Shelter
-        def initialize(name,animal,clients)
-              @name = name
-              @animals = animals
-              @clients = {}
-              @all_animals =[]
-        end
+  def initialize(name)
+        @name = name
+        @animals = []
+        @clients = []
+  end
 
-        def name
-             @name
-        end
-        
-        def clients
-             @clients
-        end
-
-        def  animals
-              @animals
-        end
-
-        def list_animals
-            @all_animals.join(" , ")
-        end
-
-        def list_clients
-            @clients.values.join(" , ")
-        end
-
-        def to_s
-           "Hello we are #{self.name} who has these many animals #{@list_animals.count} and these many #{self.clients}"
-        end
-        
-  
-end
-
-
-
-class CatLady
-  def initialize(args)
-    
+  def name
+       @name
   end
   
+  def clients
+       @clients
+  end
+
+  def  animals
+        @animals
+  end
+
+  def list_animals
+      @animals.join(" , ")
+  end
+
+  def list_clients
+      @clients.join(" , ")
+  end
+
+  def to_s
+     "Hello we are #{self.name} who has these many animals #{@list_animals.count} and these many #{self.clients}"
+  end
+        
   
 end
+
+
+
+# class CatLady
+#   def initialize(a)
+    
+#   end
+  
+  
+# end
--- w01/d05/Ernie/main.rb
+++ w01/d05/Ernie/main.rb
@@ -4,11 +4,14 @@ require_relative "happy_tails"
 #Newly created animals and clients should be added to the shelter.
 
       
+happy_tails= Shelter.new("Happy Tails")
+
 def menu
   puts "\n\n*** Hello Welcome to Happy Tails ***\n\n"
   puts '1 : Create Animal' 
-  puts '2 : Look up Animals'
+  puts '2 : Display Animals'
   puts '3 : Create Client'
+  puts '4 : Display Clients'
   puts "Q : Quit\n\n"
   gets.chomp.downcase
 end
@@ -22,31 +25,40 @@ response = menu
 while response != 'q'
   case response
   when '1'
-    # Create a new Animal
-    puts "Please Enter you animal its species and a new toy"
-    animal = gets.chomp.downcase
-    animal = Animal.new(animal)
+    # Create a new Animal!!!!!
+     puts "Please Enter your Animal name"
+     a_name = gets.chomp.downcase
+     puts = "OK great What kind of Animal is it?"
+     species = gets.chomp.downcase
+     puts = "Cool so what toys do you want?"
+     toys = gets.chomp.downcase
+     animal = Animal.new(a_name, species)
+     happy_tails.animals.push(animal)
+     
+  
   when '2'
-    # Look up Animals
-    puts "Please Enter you animal its species and a new toy"
-    animal = gets.chomp.downcase
-    animal = Animal.new(animal)
+    # Look up Animals!!!!
+    happy_tails.list_animals
+  
   when '3'
-    # Create a new Shelter
-    puts "Please Enter you animal its species and a new toy"
-    animal = gets.chomp.downcase
-    animal = Animal.new(animal)
-  when '4'
     # Create a new Person
     puts "Please Enter your name"
     name = gets.chomp.downcase
     puts "Please Enter your age"
     age = gets.chomp.to_i
-    puts "Please Enter your animal"
-    animal = get.chomp.downcase
-    new_client = Client.new("Hello #{name},#{age}#{animal}")
+    new_client = Client.new(name, age)
+    happy_tails.clients.push(new_client)
+    happy_tails.clients
+    menu
+  when '4'
+      happy_tails.list_clients
+
+  when "q"
+      break
+
   else
-    puts "Wrong input Please Choose a Number 1-4 or enter Q to leave "
+    puts "Wrong input Please Choose a Number 1-4 or enter Q to leave! "
+  end
 end
 
 
