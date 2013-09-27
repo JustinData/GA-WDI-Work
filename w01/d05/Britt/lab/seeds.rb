37ca516676a44b0868003d4353e811643c4ce9e4
brittlewis12 <brittlewis12@gmail.com>
2013-09-27 15:11:18 -0400

    created new seeds.rb file with all the shelter, client, and animal initializations to run before the program begins.

---
+++ w01/d05/Britt/lab/seeds.rb
@@ -0,0 +1,21 @@
+
+my_shelter = Shelter.new("HappiTails")
+
+client_1 = Client.new("Peter", 25)
+client_2 = Client.new("PJ", 25)
+client_3 = Client.new("Jeff", 25)
+client_4 = Client.new("Nicky", 20)
+
+animal_1 = Animal.new("Sparky", "dog")
+animal_2 = Animal.new("Lucy", "cat")
+animal_3 = Animal.new("Molly", "dog")
+animal_4 = Animal.new("Shadow", "cat")
+animal_5 = Animal.new("Corky", "dog")
+animal_6 = Animal.new("Tweetie", "bird")
+
+my_shelter.rescue_animal(animal_1)
+my_shelter.rescue_animal(animal_2)
+my_shelter.rescue_animal(animal_3)
+my_shelter.rescue_animal(animal_4)
+my_shelter.rescue_animal(animal_5)
+my_shelter.rescue_animal(animal_6)
