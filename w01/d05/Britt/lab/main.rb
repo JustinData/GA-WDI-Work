ac30ed056d7ccb2a8965ef141c019d55458202a0
brittlewis12 <brittlewis12@gmail.com>
2013-09-27 16:12:15 -0400

    implemented 'new_animal' menu item in the menu loop

--- w01/d05/Britt/lab/main.rb
+++ w01/d05/Britt/lab/main.rb
@@ -23,23 +23,58 @@ require 'pry'
 # currently JUST puts the menu.
 # actions and conditionals to
 # be incorporated later
+def new_animal
+	puts "What is the name of the animal?"
+	name = gets.chomp.capitalize
+	puts "What species of animal is #{name}?"
+	species = gets.chomp.downcase
+	# instantiating new Animal object
+	animal = Animal.new(name, species)
+	# setting animal instance to $my_shelter
+	$my_shelter.rescue_animal(animal)
+end
+
+def new_client; end
+
+def new_shelter; end
+
 def menu
 	puts "*****  Welcome to HappiTales!  *****\n"
 	
-	puts "1. Add a new animal"
-	puts "2. Add a new client"
-	puts "3. Create a new shelter"
-	puts "4. Display all animals"
-	puts "5. Display all clients"
+	puts "Type the number that corresponds with what you'd like to do:\n"
+
+	puts "1 : Add a new animal"
+	puts "2 : Add a new client"
+	puts "3 : Create a new shelter"
+	puts "4 : Display all animals"
+	puts "5 : Display all clients"
+
 	gets.chomp.downcase
 end
 
-
+#####----- BEGIN PROGRAM -----#####
 choice = menu
 
 binding.pry
 
 while choice != "q"
+	case choice
+	when "1"
+		# add a new animal
+		new_animal
+	when "2"
+		# add a new client
+		new_client
+	when "3"
+		# create a new shelter
+		new_shelter
+	when "4"
+		$my_shelter.list_animals
+	when "5"
+		$my_shelter.list_clients
+	else
+		puts "I don't understand your request."
+	end
 
 	menu
 end
