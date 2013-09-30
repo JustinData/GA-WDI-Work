4390cb7a0574293b4c40e75e10aaff784e33c785
brittlewis12 <brittlewis12@gmail.com>
2013-09-30 16:54:14 -0400

    added calculator history id lookup

--- w02/d01/Britt/server.rb
+++ w02/d01/Britt/server.rb
@@ -3,3 +3,23 @@ require 'sinatra'
 get "/" do
 	"Welcome to Britt's Sinatra Calculator"
 end
+
+# hash for all historical calculations
+calc_history = {}
+
+get "/calculator" do
+	calc_history.keys.each do
+		# something that works to show all calculations
+	end
+end
+
+get "/calculator/:id" do
+	id = params[:id].to_i
+	entry = calc_history[id]
+
+	if entry
+		entry
+	else
+		"No calculation with that id found."
+	end
+end
