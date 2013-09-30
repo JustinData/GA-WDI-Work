508e73f3ca4e374358283e4e3d507d90d23c5438
brittlewis12 <brittlewis12@gmail.com>
2013-09-30 17:11:36 -0400

    added crucial to_i to each number input from user, because everything coming in will be a string.

--- w02/d01/Britt/server.rb
+++ w02/d01/Britt/server.rb
@@ -27,29 +27,29 @@ end
 
 post "/calculator/add/:num_1/:num_2" do
 	last_id = calc_history.keys.max
-	num_1 = params[:num_1]
-	num_2 = params[:num_2]
+	num_1 = params[:num_1].to_i
+	num_2 = params[:num_2].to_i
 	calc_history[last_id + 1] = "#{num_1} + #{num_2} = #{num_1 + num_2}"
 end
 
 post "calculator/subtract/:num_1/:num_2" do
 	last_id = calc_history.keys.max
-	num_1 = params[:num_1]
-	num_2 = params[:num_2]
+	num_1 = params[:num_1].to_i
+	num_2 = params[:num_2].to_i
 	calc_history[last_id + 1] = "#{num_1} - #{num_2} = #{num_1 - num_2}"
 end
 
 post "calculator/multiply/:num_1/:num_2" do
 	last_id = calc_history.keys.max
-	num_1 = params[:num_1]
-	num_2 = params[:num_2]
+	num_1 = params[:num_1].to_i
+	num_2 = params[:num_2].to_i
 	calc_history[last_id + 1] = "#{num_1} * #{num_2} = #{num_1 * num_2}"
 end
 
 post "calculator/divide/:num_1/:num_2" do
 	last_id = calc_history.keys.max
-	num_1 = params[:num_1]
-	num_2 = params[:num_2]
+	num_1 = params[:num_1].to_i
+	num_2 = params[:num_2].to_i
 	calc_history[last_id + 1] = "#{num_1} / #{num_2} = #{num_1 / num_2}"
 end
 
