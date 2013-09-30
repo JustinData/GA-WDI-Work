a8618a15b6fe29a8280fb9cd917f745f8fbdcc3a
Maria Mercedes Martinez <pistachiopony@gmail.com>
2013-09-29 20:07:50 -0400

    happyThomework tries

--- w01/d05/Maria/w15Notes.rb
+++ w01/d05/Maria/w15Notes.rb
@@ -1,4 +1,4 @@
-a = ["Peter", "PJ", "Jeff"]
+	a = ["Peter", "PJ", "Jeff"]
 
 1. How would you return the string "Peter"?
 puts a[0]
@@ -137,7 +137,60 @@ end
 get_keys(b)
 ]
 __________________
+shelter has animals(live in shelter) and clients(also have pets)
+I will be adding shelters animal to clients animals.
 
+__________________
+
+ast login: Fri Sep 27 15:25:40 on ttys000
+ॐ  ~  pry
+[1] pry(main)> class Bin
+[1] pry(main)*   def initialize
+[1] pry(main)*     @container = []
+[1] pry(main)*   end  
+[1] pry(main)*   def container
+[1] pry(main)*     @container
+[1] pry(main)*   end  
+[1] pry(main)* end  
+=> nil
+[2] pry(main)> my_bin = Bin.new
+=> #<Bin:0x007faf110f59e8 @container=[]>
+[3] pry(main)> my_bin.container
+=> []
+[4] pry(main)> my_bin.container.push "stuff"
+=> ["stuff"]
+[5] pry(main)> my_bin.container
+=> ["stuff"]
+[6] pry(main)> [1,2,3].join
+=> "123"
+[7] pry(main)> [1,2,3].join.push :AS
+NoMethodError: undefined method `push' for "123":String
+from (pry):14:in `__pry__'
+[8] pry(main)> class Widget
+[8] pry(main)* end  
+=> nil
+[9] pry(main)> my_widget= Widget.new
+=> #<Widget:0x007faf111bce58>
+[10] pry(main)> my_b
+NameError: undefined local variable or method `my_b' for main:Object
+from (pry):18:in `__pry__'
+[11] pry(main)> my_bin
+=> #<Bin:0x007faf110f59e8 @container=["stuff"]>
+[12] pry(main)> 
+
+
+[12] pry(main)> my_bin
+=> #<Bin:0x007faf110f59e8 @container=["stuff"]>
+[13] pry(main)> my_widget
+=> #<Widget:0x007faf111bce58>
+[14] pry(main)> my_bin.container
+=> ["stuff"]
+[15] pry(main)> my_bin.container.push my_widget
+=> ["stuff", #<Widget:0x007faf111bce58>]
+[16] pry(main)> 
+ __________________
+ for using Pry require'pry'
+ end with  Binding.pry
 
 
 
