b665aa615136d77014232d84411ba4201213f764
Nichol Alexander <nichol.alexander@gmail.com>
2013-09-30 17:55:03 -0400

    fixed calculator/:id bug

--- w02/d01/Nichol/calculator.rb
+++ w02/d01/Nichol/calculator.rb
@@ -5,7 +5,7 @@ require 'pry'
 calculation_history = Array.new
 
 calculation_history << "you added 4 to 7 and got 11"
-calculation_history << "you added 4 to 7 and got 11"
+calculation_history << "you added 2 to 3 and got 5"
 
 get "/" do
 	"<h1>Welcome To Calulator - Sinatra Style</h1>"
@@ -19,9 +19,10 @@ end
 
 get "/calculator/:id" do
  	#see 3rd calculation in past or if not available, Id wasn't found
- 	calculation_number = param[:id].to_i
+ 	binding.pry
+ 	calculation_number = params[:id].to_i
  	if calculation_number < calculation_history.size then
- 		calculation_history[params[:id.to_i]]
+ 		calculation_history[calculation_number]
  	else 
  		"no such id"
  	end
