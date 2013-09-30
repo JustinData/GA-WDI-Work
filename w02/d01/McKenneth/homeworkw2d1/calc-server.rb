340f1a4616bfd3452bd49ddc7c970abb3d78e3c4
DrRobotmck <mckenneth.scott@icloud.com>
2013-09-30 18:10:18 -0400

    ADD and SUB post completed

--- w02/d01/McKenneth/homeworkw2d1/calc-server.rb
+++ w02/d01/McKenneth/homeworkw2d1/calc-server.rb
@@ -1,6 +1,6 @@
 require 'sinatra'
 
-calculator = {1 => 34, 2 => 3}
+calculator = {0 => "INITIALIZED"}
 
 get("/") do
   "Welcome to the Calculator!
@@ -18,11 +18,21 @@ get("/calculator") do
   #{calcs}"
 end
 
-get("/calculator/add/:num_1/:num_2") do
+post("/calculator/add") do
   num_1 = params[:num_1].to_i
   num_2 = params[:num_2].to_i
   sum = num_1 + num_2
   key = calculator.keys.max + 1
-  calculator[key] = sum
-  "The sum of those numbers is : #{sum}" 
+  calculator[key] = "The sum of #{num_1} and #{num_2} is : #{sum}" 
+  "The sum of #{num_1} and #{num_2} is : #{sum}" 
 end
+
+post("/calculator/sub") do
+  num_1 = params[:num_1].to_i
+  num_2 = params[:num_2].to_i
+  net = num_1 - num_2
+  key = calculator.keys.max + 1
+  calculator[key] = "The net of #{num_1} minus #{num_2} is : #{net}"
+  "The net of #{num_1} minus #{num_2} is : #{net}"
+end
+
