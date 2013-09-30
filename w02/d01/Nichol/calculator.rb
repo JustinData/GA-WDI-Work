14b2b58904589661f48fe8b65da487e67b956c73
Nichol Alexander <nichol.alexander@gmail.com>
2013-09-30 17:20:18 -0400

    got addition and adding to calculation history

--- w02/d01/Nichol/calculator.rb
+++ w02/d01/Nichol/calculator.rb
@@ -19,25 +19,33 @@ end
 
 get "/calculator/:id" do
  	#see 3rd calculation in past or if not available, Id wasn't found
- 	if params[:id].to_i < calculation_history.size then
- 		"id not found"
- 	else 
+ 	calculation_number = param[:id].to_i
+ 	if calculation_number < calculation_history.size then
  		calculation_history[params[:id.to_i]]
+ 	else 
+ 		"no such id"
  	end
 end
 
-# post "calculator/add" do 
-# 	#2 numbers will be in body of request
+post "/calculator/add" do
+ 	#2 numbers will be in body of request
+ 	binding.pry
+ 	"you're adding two numbers!"
+ 	num1 = params[:num1].to_i
+ 	num2 = params[:num2].to_i
+ 	result = num1 + num2
+ 	calculation_history << "you added #{num1} to #{num2} and got #{result}"
+
+end
+
+# post "/calculator/subtract" do
+
 # end
 
-# post "calculator/subtract" do
-
+# post "/calculator/multiply" do
 # end
 
-# post "calculator/multiply" do
-# end
-
-# post "calculator/divide" do
+# post "/calculator/divide" do
 # end
 
 
