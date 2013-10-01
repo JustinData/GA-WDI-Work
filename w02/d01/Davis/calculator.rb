a093b97a180dec16b35f17b990a7bafc9f86f452
daviskoh <koh.davis.0@gmail.com>
2013-10-01 00:34:29 -0400

    should be fully functional, not the best DRY and not thoroughly tested

--- w02/d01/Davis/calculator.rb
+++ w02/d01/Davis/calculator.rb
@@ -1,4 +1,53 @@
+# require 'sinatra'
+
+# calculator = {
+# 	1 => {
+# 		"add" => [1,2,3]
+# 	}, 
+# 	2 => {
+# 		"subtract" => [3,2,1]
+# 	}
+# }
+
+# get "/" do
+# 	"Ello der mate! This be the calculator!"	
+# end
+
+# get "/calculator" do
+# 	#show history of calculations
+# 	#final return value for Sintra has to be String???
+# 	result_string = ""
+
+# 	calculator.each do |log_num, operation_hash|
+# 		result_string << "#{log_num}: #{operation_hash.keys.join} #{operation_hash.values[0][0]} and #{operation_hash.values[0][1]} = #{operation_hash.values[0][2]}\n"
+# 	end
+
+# 	result_string
+# end
+
+# get "/calculator/:id" do
+# 	id = params[:id].to_i
+# 	entry = calculator[id]
+	
+# 	if entry
+# 		"Calculation #{id}: #{entry.keys.join} #{entry.values[0][0]} and #{entry.values[0][1]} = #{entry.values[0][2]}"
+# 	else
+# 		"Calculation doesn't exist"
+# 	end
+# end
+
+# post "/calculator/add/:n1/:n2" do
+# 	last_id = calculator.keys.max
+# 	n1 = params[:n1].to_i
+# 	n2 = params[:n2].to_i
+
+# 	calculator[last_id + 1] = { params["add"]=>[n1, n2, n1+n2] }
+# end
+
+#mistakes above for learning / remembering
 require 'sinatra'
+
+#worst worst set hash key => string
 
 calculator = {
 	1 => {
@@ -9,13 +58,11 @@ calculator = {
 	}
 }
 
-get "/" do
-	"Ello der mate! This be the calculator!"	
+get '/' do
+	"Welcome! This is a calculator!"
 end
 
-get "/calculator" do
-	#show history of calculations
-	#final return value for Sintra has to be String???
+get '/calculator' do
 	result_string = ""
 
 	calculator.each do |log_num, operation_hash|
@@ -25,7 +72,7 @@ get "/calculator" do
 	result_string
 end
 
-get "/calculator/:id" do
+get '/calculator/:id' do
 	id = params[:id].to_i
 	entry = calculator[id]
 	
@@ -36,30 +83,31 @@ get "/calculator/:id" do
 	end
 end
 
-post "/calculator" do
+#in other words: user types for ex /calculator/add -X POST -F n1=1 -F n2=2
+#not distinction bet what is passed via URL and what is passed via -F
+post '/calculator/:operation' do
 	last_id = calculator.keys.max
-	calculator[last_id + 1] = params[:name]
-end
+	operation = params[:operation]
+	n1 = params[:n1].to_i
+	n2 = params[:n2].to_i
 
-post "/calculator/:operation/:n1/:n2" do
-	operation = params[:operation] # == "add"
-	n1 = params[:n1]
-	n2 = params[:n2]
-	calculator[last_id][:operation].push(n1, n2)
-
-	case operation
+	case params[:operation]
 	when 'add'
-		calculator[last_id][:operation].push(n1 + n2)
+		calculator[last_id + 1] = { operation => [n1, n2, n1+n2] }
+		"#{operation} #{n1} and #{n2} = #{n1+n2}"
 	when 'subtract'
-		calculator[last_id][:operation].push(n1 - n2)
+		calculator[last_id + 1] = { operation => [n1, n2, n1-n2] }
+		"#{operation} #{n1} and #{n2} = #{n1-n2}"
 	when 'multiply'
-		calculator[last_id][:operation].push(n1 * n2)
+		calculator[last_id + 1] = { operation => [n1, n2, n1*n2] }
+		"#{operation} #{n1} and #{n2} = #{n1*n2}"
 	when 'divide'
-		calculator[last_id][:operation].push(n1 / n2)
+		n1.to_f
+		n2.to_f
+		calculator[last_id + 1] = { operation => [n1, n2, n1/n2] }
+		"#{operation} #{n1} and #{n2} = #{n1/n2}"
+	else
+		'Operation not exist / not allowed'
 	end
-		
 end
 
-# "/:p1/:p2"
-# params[:p1]
-# params[:p2]
