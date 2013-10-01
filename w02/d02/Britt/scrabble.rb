8d9761e29dd2de877b85885a8ff0a5016074f58f
brittlewis12 <brittlewis12@gmail.com>
2013-10-01 09:35:57 -0400

    refactored, working, completed the 'for fun' part of the morning exercise.

--- w02/d02/Britt/scrabble.rb
+++ w02/d02/Britt/scrabble.rb
@@ -1,6 +1,7 @@
 class ScrabbleWord
 	def initialize(word)
 		@word = word
+		@word_array = word.split(//)
 	end
 
 	def word
@@ -17,7 +18,7 @@ class ScrabbleWord
 }
 
 	def to_array
-		@word_array = word.split(//)
+		@word_array
 	end
 
 	def char_at(index)
@@ -32,6 +33,27 @@ class ScrabbleWord
 		points
 	end
 
-	def multiplier_score(param); end
+	def multiplier_score(multiplier)
+		score * multiplier
+	end
 
 end
+
+konowitch = ScrabbleWord.new("konowitch")
+
+puts konowitch.score
+
+######## For fun ########
+
+TILE_VALUES = {
+  "a"=>1, "b"=>3, "c"=>3, "d"=>2, "e"=>1,
+  "f"=>4, "g"=>2, "h"=>4, "i"=>1, "j"=>8,
+  "k"=>5, "l"=>1, "m"=>3, "n"=>1, "o"=>1,
+  "p"=>3, "q"=>10, "r"=>1, "s"=>1, "t"=>1,
+  "u"=>1, "v"=>4, "w"=>4, "x"=>8, "y"=>4,
+  "z"=>10
+}
+
+TILE_VALUES.select { |letter, points| points == 4 }
+
+letter_array.map { |letter| TILE_VALUES[letter] }
