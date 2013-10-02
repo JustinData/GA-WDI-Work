dcfb41adb16572b48496fc70db30e837114f6658
Jeffrey Konowitch <jeff.konowitch@generalassemb.ly>
2013-10-02 15:14:32 -0400

    use GET

--- w02/d04/Instructor/forms/server.rb
+++ w02/d04/Instructor/forms/server.rb
@@ -5,19 +5,17 @@ require 'httparty'
 require 'pry'
 
 get "/" do
-  erb :index
-end
-
-post "/" do
-  @stock_symbol = params[:stock_symbol].upcase
+  if params[:stock_symbol]
+    @stock_symbol = params[:stock_symbol].upcase
   
-  response = HTTParty.get("http://dev.markitondemand.com/Api/Quote/json?symbol=#{@stock_symbol}")
+    response = HTTParty.get("http://dev.markitondemand.com/Api/Quote/json?symbol=#{@stock_symbol}")
 
-  if response["Data"].nil?
-    @error = response["Message"]
-    erb :index
-  else
-    @last_price = response["Data"]["LastPrice"]
-    erb :results
+    if response["Data"].nil?
+      @error = response["Message"]
+    else
+      @last_price = response["Data"]["LastPrice"]
+    end
   end
+
+  erb :index
 end
