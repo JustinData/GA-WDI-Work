3c8266929d3416e14f57f223ef5c3859665cf388
brittlewis12 <brittlewis12@gmail.com>
2013-10-02 13:52:31 -0400

    updates after lunch

--- w02/d03/Britt/morning.rb
+++ w02/d03/Britt/morning.rb
@@ -1,34 +1,34 @@
-# class Person
+class Person
 
-# 	def initialize(name)
-# 	  @name = name
-# 	end
+	def initialize(name)
+	  @name = name
+	end
 
-# 	def self.learn
-# 	  puts "I'm a class method"
-# 	  puts self
-# 	end
+	def self.learn
+	  puts "I'm a class method"
+	  puts self
+	end
 
-# 	def learn
-# 	  puts "I'm an instance method"
-# 	  puts self
-# 	end
+	def learn
+	  puts "I'm an instance method"
+	  puts self
+	end
 
-# 	def hello
-# 	  self.learn
-# 	end
+	def hello
+	  self.learn
+	end
 
-# 	def hello_again
-# 	  learn
-# 	end
+	def hello_again
+	  learn
+	end
 
-# end
+end
 
-# pj = Person.new("PJ")
+pj = Person.new("PJ")
 
-# pj.learn
-# pj.hello
-# pj.hello_again
+pj.learn
+pj.hello
+pj.hello_again
 
 puts "" 
 
