8f948220eb5d1f77a9367ed0dc85a32cb097ce99
Nichol Alexander <nichol.alexander@gmail.com>
2013-10-02 00:27:55 -0400

    fixing probs with git

---
+++ w02/d02/Nichol/doesitmove/server.rb
@@ -0,0 +1,86 @@
+require "sinatra"
+require "sinatra/reloader"
+require "erb"
+require "pry"
+
+class Problem
+	attr_accessor :doesitmove, :shoulditmove
+
+end
+
+$my_problem = Problem.new
+
+get "/" do	
+	erb :splash
+end
+
+get "/doesitmove/" do
+	@string = "does it move?"
+	@choice1 = "yes"
+	@choice2 = "no"
+	
+	@string_for_url1 = @string.chomp("?").delete " "
+	@url_link_choice1 = @string_for_url1 +  "/" + @choice1
+	
+	@string_for_url2 = @string.chomp("?").delete " "
+	@url_link_choice2 = @string_for_url2 +  "/" + @choice2
+
+	erb :yes_no
+end
+
+get "/doesitmove/yes" do
+	$my_problem.doesitmove = true
+
+	@string = "should it?"
+	@choice1 = "yes"
+	@choice2 = "no"
+	
+	@string_for_url = @string.chomp("?").delete " "
+	@url_link_choice1 = @string_for_url +  "/" + @choice1
+	
+	@string_for_url2 = @string.chomp("?").delete " "
+	@url_link_choice2 = @string_for_url2 +  "/" + @choice2
+
+	erb :yes_no
+end
+
+get "/doesitmove/no" do
+	$my_problem.doesitmove = false
+
+	@string = "should it?"
+	@choice1 = "yes"
+	@choice2 = "no"
+	
+	@string_for_url = @string.chomp("?").delete " "
+	@url_link_choice1 = @string_for_url +  "/" + @choice1
+	
+	@string_for_url2 = @string.chomp("?").delete " "
+	@url_link_choice2 = @string_for_url2 +  "/" + @choice2
+
+	erb :yes_no
+end
+
+get "/shouldit/yes" do
+	$my_problem.shoulditmove = true
+	#binding.pry
+	if ($my_problem.doesitmove == true) && ($my_problem.shoulditmove == false) then
+		erb :ductape
+	elsif ($my_problem.doesitmove == false) && ($my_problem.shoulditmove == true) then
+		erb :wd40
+	else
+		erb :no_problem
+	end
+
+end
+
+get "/shouldit/no" do
+	$my_problem.shoulditmove = false
+	
+	if ($my_problem.doesitmove == true) && ($my_problem.shoulditmove == false) then
+		erb :ductape
+	elsif ($my_problem.doesitmove == false) && ($my_problem.shoulditmove == true) then
+		erb :wd40
+	else
+		erb :no_problem
+	end
+end
