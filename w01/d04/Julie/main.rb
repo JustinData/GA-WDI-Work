b00c1efecf17574936f4a996975bc1523ebccc37
Russell TAFF <rtaff2012@RTs-MacBook-Pro.local>
2013-10-02 14:28:41 -0400

    fix merges

--- w01/d04/Julie/main.rb
+++ w01/d04/Julie/main.rb
@@ -4,11 +4,7 @@
 require_relative "./person"
 require_relative "./apartment"
 require_relative "./building"
-<<<<<<< HEAD
-require "pry"
-=======
 #require "pry"
->>>>>>> master
      
 ####Create the building
 ####                        (address, style, doorman, walkup, floors)
