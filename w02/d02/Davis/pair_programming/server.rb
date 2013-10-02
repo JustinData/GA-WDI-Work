1cd5e6c65c4a919eaeb76b152f783631e6877df1
daviskoh <koh.davis.0@gmail.com>
2013-10-01 20:53:42 -0400

    refactored using yield, atached copy of original source css w/ slightly diff colors

--- w02/d02/Davis/pair_programming/server.rb
+++ w02/d02/Davis/pair_programming/server.rb
@@ -4,23 +4,22 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'erb'
-require 'pry'
 
 get '/' do
   @statement = "Do you have a test for that?"
   @link_url1 = "yes"
   @link_url2 = "no"
 
-  erb :index
+  erb :choice
 end
 
-get '/:first' do
-  if params[:first] == 'yes'
+get '/:answer' do
+  if params[:answer] == 'yes'
     @statement = "Does the test pass?"
     @link_url1 = "yes/yes"
     @link_url2 = "yes/no"
     
-    erb :index
+    erb :choice
   else
     @statement = "Write a test."
     @link_url = "/"
@@ -29,13 +28,13 @@ get '/:first' do
   end
 end
 
-get '/yes/:second' do
-  if params[:second] == 'yes'
+get '/yes/:answer' do
+  if params[:answer] == 'yes'
     @statement = "Need to Refactor?"
     @link_url1 = "yes/yes"
     @link_url2 = "yes/no"
 
-    erb :index
+    erb :choice
   else
     @statement = "Write just enough code for the test to pass."
     @link_url = "/"
@@ -44,8 +43,8 @@ get '/yes/:second' do
   end
 end
 
-get '/yes/yes/:third' do
-  if params[:third] == 'yes'
+get '/yes/yes/:answer' do
+  if params[:answer] == 'yes'
     @statement = "Refactor the Code"
     @link_url = "/"
 
