3de5e73721779e6b10f78223a797eb94cb10d813
teabait <hello.teaho@gmail.com>
2013-10-01 23:50:55 -0400

    actually add flowchart app/ finished and stylized

--- w02/d02/Tea/server.rb
+++ w02/d02/Tea/server.rb
@@ -4,25 +4,36 @@ require 'erb'
 require "pry"
 
 get '/' do   
-  "don't you want somebody to love"
-end
-
-get '/want' do  
-  "don't you want somebody to love" 
+  "lala"
+  @question = "Don't you want somebody to love?"
+  @yes = "need"
+  erb :y_n
 end
 
 get '/need' do   
-  "don't you need somebody to love"
+  @question = "Don't you need somebody to love?"
+  @yes = "love"
+  erb :y_n
 end
 
 get '/love' do  
-  "wouldn't you love somebody to love" 
+  @question = "Wouldn't you love somebody to love?" 
+  @yes = "find"
+  erb :y_n
 end
 
 get '/find' do  
-  "you better find somebody to love" 
+  @question = "You better find somebody to love!" 
+
+  erb :done
 end
 
 get '/notfind' do   
-  "you better not find somebody to love."
+  @question = "You better not find somebody to love!"
+  erb :done
+end
+
+get '/done' do
+  @question = "Sing it, baby!"
+  erb :done
 end
