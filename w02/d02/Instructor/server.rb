6444360e77a9c997ebb36455345e0d1da749df83
Peter Lai <peter.yd.lai@gmail.com>
2013-10-02 15:14:10 -0400

    fix merge

--- w02/d02/Instructor/server.rb
+++ w02/d02/Instructor/server.rb
@@ -3,24 +3,6 @@ require "sinatra/reloader"
 require "erb"
 require "pry"
 
-<<<<<<< HEAD
-get( "/profiles/:profile_name" ) do
-  if params[:profile_name] == "ann-margret"
-    output_name = "Ann Margret"
-    output_img = "<img src='http://www.whatgoesaroundnyc.com/blog/wp-content/uploads/2013/08/1964-elvis-e-ann-margret-in-viva-las-wegas.jpg' width='400' />"
-  elsif params[:profile_name] == "elvis"
-    output_name = "Elvis Presley"
-    output_img = "<img src='http://www.whatgoesaroundnyc.com/blog/wp-content/uploads/2013/08/1964-elvis-e-ann-margret-in-viva-las-wegas.jpg' width='400' />"
-  end
-
-  output_string = "<!doctype html><html><head></head>"
-  output_string += "<body><h1>#{output_name}</h1>#{output_img}</body></html>"
-
-
-  # implicit return
-  output_string
-end
-=======
 #if the URL is the root directory, load ERB template "profile"
 get( "/" ) do
   erb :profile
@@ -41,4 +23,3 @@ end
 
 
 
->>>>>>> f18dc631c82b838eb8f5654b5651c227039b6fda
