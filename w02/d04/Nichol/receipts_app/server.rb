7bc6a92ca93993a760e84087865daff8ce65b104
Nichol Alexander <nichol.alexander@gmail.com>
2013-10-03 17:28:24 -0400

    working, getting there...

---
+++ w02/d04/Nichol/receipts_app/server.rb
@@ -0,0 +1,49 @@
+require "sinatra"
+require "sinatra/reloader"
+require "pry"
+
+my_file = File.new("receipts.txt", "a+")
+
+get "/" do
+	erb :index
+end
+
+post "/newreceipt" do
+	store_name = params[:store_name]
+	product_name = params[:product_name]
+	price = params[:price]
+	
+	#logic for processing new receipt
+	#TODO need to add unique id!
+	my_file = File.new("receipts.txt", "a+")
+	my_file << store_name + "," + product_name + "," + price + "\n"
+	my_file.close
+	
+	erb :success
+end
+
+get "/newreceipt" do
+	erb :receipt
+end
+
+get "/receipts" do
+	#print all receipts
+	
+	my_file = File.new("receipts.txt", "a+")
+	binding.pry
+	
+	my_file.each do |line|
+		line_array = line.split(",")
+		@UID = line[0]
+		@store_name = line[0]
+		@product_name = line[1]
+		@price = line[2]
+		erb :display_receipt
+	end
+	
+	my_file.close
+
+end
+
+
+
