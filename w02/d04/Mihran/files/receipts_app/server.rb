255a7faa1822134fe799d392db77cfc28338ced2
Mihran Abrahamian <abrahamian.mihran@gmail.com>
2013-10-03 16:50:59 -0400

    save

---
+++ w02/d04/Mihran/files/receipts_app/server.rb
@@ -0,0 +1,22 @@
+require "sinatra"
+require "sinatra/reloader"
+require "erb"
+
+get ("/") do
+  erb :index
+end
+
+# get ("/receipts/new")
+# end
+
+
+# get ("/receipts")
+# end
+
+# post ("/receipts")
+# end
+
+
+# get ("receipts/id")
+# end
+
