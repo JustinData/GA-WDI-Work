71498b0054409722cbfd961855682907a1f1c1d9
teabait <hello.teaho@gmail.com>
2013-10-03 14:58:01 -0400

    more erb fun

--- w02/d04/Tea/stocks/server2.rb
+++ w02/d04/Tea/stocks/server2.rb
@@ -10,7 +10,6 @@ end
 # http://localhost:4567/stocks?stock_symbol=GOOG
 
 get "/stocks" do
-  binding.pry
   @stock_symbol = params[:stock_symbol]
   response = HTTParty.get("http://dev.markitondemand.com/Api/Quote/json?symbol=#{@stock_symbol}")
   @price = response["Data"]["LastPrice"]
