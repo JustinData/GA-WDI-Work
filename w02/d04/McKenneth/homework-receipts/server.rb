be1816eda0dca5f45b9ccf2ae860b4071181575a
DrRobotmck <mckenneth.scott@icloud.com>
2013-10-03 18:25:58 -0400

    All parts working, saving to file, id works, adding css now

--- w02/d04/McKenneth/homework-receipts/server.rb
+++ w02/d04/McKenneth/homework-receipts/server.rb
@@ -1,6 +1,6 @@
 require 'sinatra'
 require 'sinatra/reloader'
-i=1
+
 #welcome screen
   get("/") do 
     erb :index
@@ -13,32 +13,26 @@ i=1
 
 #creates new receipts
   post("/receipts") do
-    @id = i
+    file = File.new("views/receipts.txt", "a+")
+
+    @id = file.readlines.count + 1
     @store = params[:store]
     @item = params[:item]
     @cost = params[:cost]
     input = []
-    input <<@id
+    input << @id
     input << @store
     input << @item 
     input << @cost
-    file = File.new("views/receipts.txt", "a+")
+    
     file.puts input.join(",")
     file.close
-    i+=1
+    erb :receipt_read
   end
 
 #prints all receipts
   get("/receipts") do 
-    # file = File.new("receipts.txt", "r")
-    # file.each do |lines|
-    #   each_one = lines.split(",")
-    #   puts "Store: #{each_one[1]}"
-    #   puts "Item: #{each_one[2]}"
-    #   puts "Cost: #{each_one[3]}"
-    #   puts
-    # end
-    # file.close
+    
     erb :receipt_read
 
   end
@@ -46,18 +40,6 @@ i=1
 #shows a particular receipt
   get("/receipts/:id") do 
     @id = params[:id]
-    # file = File.new("receipts.txt", "r")
-    # file.each do |lines|
-    #   each_one = lines.split(",")
-    #     if each_one[0] = @id
-    #       puts "Store: #{each_one[1]}"
-    #       puts "Item: #{each_one[2]}"
-    #       puts "Cost: #{each_one[3]}"
-    #       puts
-    #     else
-    #      puts "Sorry, that's not a receipt you input!"
-    #     end
-    # end
-    # file.close
+    
     erb :receipt_read
   end
