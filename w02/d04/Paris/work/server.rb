1ae74bd3698c4c08b7e35a46160ca81a2dee0f27
My Name <myemail@example.com>
2013-10-03 14:26:46 -0400

    forms app

---
+++ w02/d04/Paris/work/server.rb
@@ -0,0 +1,24 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'httparty'
+require 'pry'
+
+# GET /
+# => User to be presented with "Welcome to our stock app."
+
+# GET /stocks?symbol=GOOG
+# => 845.45
+
+get "/" do
+
+erb :index
+end
+
+get "/stocks" do
+@stock_symbol = params[:stock_symbol]
+response = HTTParty.get("http://dev.markitondemandcom/Api/Quote/json?symbol=#{@stock_symbol}")
+@price = response["Data"]["LastPrice"]
+erb :stocks
+end 
+
+# type localhost:4567/stocks?symbol=GOOG
