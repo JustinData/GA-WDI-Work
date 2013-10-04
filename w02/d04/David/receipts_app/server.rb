d7c4cb3cc7853c32f801cf537252e98771e25da6
David Fox Powell <dtothefp@gmail.com>
2013-10-03 23:24:12 -0400

    Revert "Complete HW W2D4"
    This reverts commit fc2d496167664d0e4521b047b43eb2a17481de06.

--- w02/d04/David/receipts_app/server.rb
+++ w02/d04/David/receipts_app/server.rb
@@ -6,41 +6,21 @@ get "/" do
   erb :index
 end
 
-get "/receipts" do
-  erb :receipts
-end
-
 get "/receipts/new" do
   erb :new
 end
 
 post "/receipts" do
-  info = Array.new
-  info << params[:store]
-  info << params[:product]
-  info << params[:price]
-  file = File.new("receipts.txt", "a+")
-  file.puts info.join(",")
-  file.close
+  
+  
+end
+
+get "/receipts" do
+
   erb :receipts
 end
 
 get "/receipts/:id" do
-  id_hash=Hash.new
-  id = 0
-  file = File.new("receipts.txt", "r")
-    file.each do |line| 
-      info = line.split(",") 
-      id_hash[id] = info
-      id +=1
-    end 
-    file.close
- @receipt_by_id = id_hash[params[:id].to_i]
- binding.pry
- erb :receipts
+  
+  erb :receipts
 end
-
-
-
-
-
