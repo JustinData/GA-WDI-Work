49e015552e4a7f3a938014493c3ec4dd3a5b11db
My Name <myemail@example.com>
2013-10-03 23:41:37 -0400

    receipts io done

--- w02/d04/Paris/work/files/receipts_app/server.rb
+++ w02/d04/Paris/work/files/receipts_app/server.rb
@@ -1,47 +1,72 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'pry'
-require "httparty"
+
+receipts = []
 
 get "/" do
   @make = "/receipts/new"
+  @print_one ="/receipts/1"
+  @print_all = "/receipts"
   erb :index
 end
 
-get "/receipts/" do
-  @make = "/receipts/new"
-  erb :receipts
-end
-
-
 get "/receipts/new" do
+  @make = "/receipts/new"
+  @print_one ="/receipts/1"
+  @print_all = "/receipts"
+  @receipt_text = "Create your receipt:"
+  erb :forms
+end
 
-  # @stock_symbol = params[:stock_symbol]
-  # @random_string_from_user = params[:stuff]
-  
-  # response = HTTParty.get("http://dev.markitondemand.com/Api/Quote/json?symbol=#{@stock_symbol}")
+post "/receipts" do
+  @make = "/receipts/new"
+  @print_one ="/receipts/1"
+  @print_all = "/receipts"
+  count = %x{wc -l receipts.txt}.split[0].to_i
+  purchase = []
+  purchase << count + 1
+  purchase << params[:store]
+  purchase << params[:product]
+  purchase << params[:price]
+  file = File.new("receipts.txt", "a+")
+  file.puts purchase.join(", ")
+  file.close
+  erb :forms
+end
 
-  # @price = response["Data"]["LastPrice"]
-
+get "/receipts" do
+  @make = "/receipts/new"
+  @print_one ="/receipts/1"
+  @print_all = "/receipts"
+  @receipt_text = "Here are all of your receipts:"
+  @string = ""
+  file = File.new("receipts.txt", "r")
+  file.each do |line|
+  purchase = line.split(",") 
+  @string += "#{purchase[0]} store #{purchase[1]} product #{purchase[2]} price #{purchase[3]}<br> " 
+  end
+  file.close
   erb :receipts
 end
 
-# get "/receipts/:id" do
+get "/receipts/:id" do
+  @make = "/receipts/new"
+  @print_one ="/receipts/1"
+  @print_all = "/receipts"
+  @receipt_text="Here is your receipt:"
+  @hash = {}
+  @id = params[:id].to_i
+  file = File.new("receipts.txt", "r")
+  file.each do |line|
+  purchase = line.split(",") 
+  @hash[purchase[0].to_i] = purchase
+  end
+  erb :single_receipt
+end
 
-#   # @stock_symbol = params[:stock_symbol]
-#   # @random_string_from_user = params[:stuff]
-  
-#   # response = HTTParty.get("http://dev.markitondemand.com/Api/Quote/json?symbol=#{@stock_symbol}")
 
-#   # @price = response["Data"]["LastPrice"]
 
-#   erb :receipts
-# end
-
-# post "/receipts" do
-
-#   erb :receipts
-# end
 
 
 
