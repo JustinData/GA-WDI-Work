d751af7eec84e4db0bb81728e6d5823e77a825fd
Raleigh D. Froeber <RDFroeber@gmail.com>
2013-10-04 08:41:40 -0400

    Receipt App almost done... still need to fix printing and get by ID

--- w02/d04/Raleigh/receipt_app/server.rb
+++ w02/d04/Raleigh/receipt_app/server.rb
@@ -15,21 +15,25 @@ post "/receipts" do
    file = File.new("receipts.txt", "a+")
    # binding.pry
 
-   line_num = 1
-   File.open("receipts.txt") {|n| line_num = n.read.count("\n")}
+   line_num = 0
+   File.open("receipts.txt") {|n| line_num = n.read.count("\n") + 1}
 
    entry = [line_num, params[:store],params[:item],params[:price]]
    file.puts entry.join(",")
    file.close
 
+   # REDIRECT with Sinatra
+   redirect "/receipts"
 end
 
 get "/receipts" do 
-   file = File.new("receipts.txt", "r")
+   @past_receipts = []
+   file = File.open("receipts.txt", "r")
 
    file.each do |line|
       entry = line.split(",")
-      @past_receipt = "\nStore: #{entry[1]}\nItem: #{entry[2]}\nPrice: $#{entry[3]}"
+      last = "Store: #{entry[1]}", "Item: #{entry[2]}", "Price: $#{entry[3]}"
+      @past_receipts.push(last)
    end
 
    file.close
