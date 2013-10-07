8a0a72adc1b6016677c46cc81da3521f91d9d048
daviskoh <koh.davis.0@gmail.com>
2013-10-07 09:57:59 -0400

    working morning ex

---
+++ w03/d01/Davis/morning/server.rb
@@ -0,0 +1,16 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'erb'
+require 'pry'
+
+get '/' do 
+	erb :home
+end
+
+# Note: the / at end of multiple 
+# Note: don't need /:num
+get '/multiple/' do
+	@num = params[:num].to_i
+
+	erb :multiple
+end
