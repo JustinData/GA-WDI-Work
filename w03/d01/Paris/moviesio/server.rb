b0735b6e19703bae51cdbf9f02a06c8ffbe6a0a9
My Name <myemail@example.com>
2013-10-07 23:15:11 -0400

    moviesio with sql

---
+++ w03/d01/Paris/moviesio/server.rb
@@ -0,0 +1,89 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'httparty'
+require 'json'
+require 'pry'
+require 'pg'
+
+
+get "/" do   
+ @greeting = "Welcome to Movies.io"
+ @search = "/movies/search"
+  erb :index
+end
+
+get "/movies/search" do
+  erb :forms
+end
+
+get "/movies" do
+
+fs = File.new( "movies" + ".csv", "r" )
+db_conn = PG.connect( dbname: "movies" + "_db"  )
+results = db_conn.exec( "SELECT * FROM movies;"  )
+results.each do |row|
+@output_str = "Number #{row['id']}: #{row['year']} "
+@output_str += "#{row['poster']}, #{row['title']} )" 
+
+end
+db_conn.close
+
+  erb :allmovies
+end
+
+get "/movies/:id" do   
+fs = File.new( "movies" + ".csv", "r" )
+db_conn = PG.connect( dbname: "movies" + "_db"  )
+results = db_conn.exec( "SELECT * FROM movies;"  )
+
+id = params[:id]
+results = "SELECT * FROM movies WHERE id=#{id}"
+@puts_movie = results
+db_conn.close
+
+  erb :singlemovie
+end
+
+post "/movies" do
+
+##### HTTP PARTY!!!!!#####
+name = params[:title].gsub(" ", "+")
+url = "http://www.omdbapi.com/?t=#{name}"
+response = HTTParty.get(url)
+parsed_movie_info = JSON(response)
+
+fs = File.new( "movies" + ".csv", "a+" )
+
+ db_conn = PG.connect( dbname: "movies" + "_db"  )
+
+    @query_str = "INSERT INTO movies "
+    @query_str += "( year, poster, title) VALUES "
+    @query_str += "( #{parsed_movie_info[:year]}, '#{parsed_movie_info[:poster]}', '#{parsed_movie_info[:title]}' );"
+
+    db_conn.close
+
+  redirect to("/movies/#{"id"}")
+end
+
+
+# use the where clause to do id
+
+# CREATE TABLE movies (
+#   id serial PRIMARY KEY,
+#   poster varchar(300) NOT NULL,
+#    varchar(50) NOT NULL,
+# );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
