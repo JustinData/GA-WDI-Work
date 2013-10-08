dcb28359771fdebb9b3d474cab8c0bf6df9ba3a3
JustinGA <justinkemmerer@gmail.com>
2013-10-08 15:35:52 -0400

    index and show actions

--- w03/d02/Justin/guest_book/server.rb
+++ w03/d02/Justin/guest_book/server.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'sinatra/reloader'
 require 'active_record'
 require 'pry'
 
@@ -13,29 +14,20 @@ ActiveRecord::Base.establish_connection(
 require_relative './models/entry'
 
 
-# Create
-# Read
-# Update
-# Destroy
-# Guest Book entries
-
-# GET /guest_book
-# List of all people who have ever registered
-# "These people have registered: Jeff, PJ, Peter"
-
-# { 1 => "Jeff", 2 => "PJ", 3 => "Peter" }
-
-# Index
 
 get "/guest_book" do
- 
+  @entries = Entry.all
+
+  erb :index
 end
 
 # Show (just one entry)
 # I know the key (or the id)
 
 get "/guest_book/:id" do
-  
+  @entry = Entry.find(params[:id])
+
+  erb :show
 end
 
 # GET /guest_book/1
