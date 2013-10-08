de015071407431d935855f180d26c9d752983134
Mihran Abrahamian <abrahamian.mihran@gmail.com>
2013-10-08 16:03:58 -0400

    guestbook works

--- w03/d02/Mihran/guest_book/server.rb
+++ w03/d02/Mihran/guest_book/server.rb
@@ -34,25 +34,25 @@ require_relative './models/entry'
 get "/guest_book" do
   @entries = Entry.all
   erb :index
-
 end
 
-# Show (just one entry)
-# I know the key (or the id)
+
+get "/guest_book/new" do
+  erb :new
+end
+
 
 get "/guest_book/:id" do
   @entry = Entry.find(params[:id])
   erb :show
 end
 
-# GET /guest_book/1
-
-# POST /guest_book
-# Create a new entry in the `guest_book` hash
-# Each entry should have a unique id
 
 post "/guest_book" do
-
+  
+  entry = Entry.new(comment: params[:comment], name: params[:name])
+  entry.save
+  redirect "/guest_book/#{entry.id}"
 end
 
 # Expect params[:name] == new_name
