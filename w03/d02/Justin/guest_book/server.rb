f7ef09901f00855de6cec2acd6c97b6cf7d5a390
JustinGA <justinkemmerer@gmail.com>
2013-10-08 14:38:57 -0400

    skeleton setup

---
+++ w03/d02/Justin/guest_book/server.rb
@@ -0,0 +1,58 @@
+require 'sinatra'
+require 'active_record'
+require 'pry'
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "postgresql",
+  :host => "localhost",
+  :username => "postgres",
+  :password => "postgres",
+  :database => "sandbox"
+)
+
+require_relative './models/entry'
+
+
+# Create
+# Read
+# Update
+# Destroy
+# Guest Book entries
+
+# GET /guest_book
+# List of all people who have ever registered
+# "These people have registered: Jeff, PJ, Peter"
+
+# { 1 => "Jeff", 2 => "PJ", 3 => "Peter" }
+
+# Index
+
+get "/guest_book" do
+ 
+end
+
+# Show (just one entry)
+# I know the key (or the id)
+
+get "/guest_book/:id" do
+  
+end
+
+# GET /guest_book/1
+
+# POST /guest_book
+# Create a new entry in the `guest_book` hash
+# Each entry should have a unique id
+
+post "/guest_book" do
+
+end
+
+# Expect params[:name] == new_name
+put "/guest_book/:id" do
+
+end
+
+delete "/guest_book/:id" do
+  
+end
