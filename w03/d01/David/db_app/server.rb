604ef5d07abb14c2c8dd92056540cb1f640e7333
David Fox Powell <dtothefp@gmail.com>
2013-10-07 22:20:30 -0400

    Complete Bonus Exercises

--- w03/d01/David/db_app/server.rb
+++ w03/d01/David/db_app/server.rb
@@ -21,16 +21,32 @@ get "/movies/search" do
   erb :search
 end
 
+get "/movies/recent" do
+  @results = db_conn.exec("SELECT * FROM movies WHERE year >= '1988';")
+  erb :recent
+end
+
+get "/movies/good" do
+  @results = db_conn.exec("SELECT * FROM movies WHERE rating >= '8.0';")
+
+  erb :good
+end
+
+get "/movies/funny" do
+  @results = db_conn.exec("SELECT * FROM movies WHERE genre LIKE '%Comedy%';")
+
+  erb :funny
+end
+
 post "/movies" do 
   name = params[:title]
   name = name.gsub(" ", "+")
   url = "http://www.omdbapi.com/?t=#{name}"
   response = HTTParty.get(url)
   parsed = JSON(response)
-  
   query_str = "INSERT INTO movies "
-  query_str += "(title, year, poster) VALUES "
-  query_str += "('#{parsed["Title"]}', '#{parsed["Year"]}', '#{parsed["Poster"]}');"
+  query_str += "(title, year, poster, rating, genre) VALUES "
+  query_str += "('#{parsed["Title"]}', '#{parsed["Year"]}', '#{parsed["Poster"]}', '#{parsed["imdbRating"]}', '#{parsed["Genre"]}');"
   db_conn.exec( query_str )
   #finds ID of last entered movie and redirects to that page
   results = db_conn.exec( "SELECT lastval();")
@@ -42,7 +58,8 @@ get "/movies/:id" do
   id = params[:id]
   #gets a row of the most recently entered movie
   @results =  db_conn.exec( "SELECT * FROM movies WHERE id = lastval();")
-  binding.pry
   erb :movies
 end
+
+
 
