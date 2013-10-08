14a876d3c48faca1713b5464fc126731f568aef2
DrRobotmck <mckenneth.scott@icloud.com>
2013-10-08 17:04:15 -0400

    Home page works, tackling add

---
+++ w03/d02/McKenneth/classwork/homework/server.rb
@@ -0,0 +1,22 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'active_record'
+require 'pry'
+
+ActiveRecord::Base.establish_connection(
+  adapter: "postgresql",
+  host: "localhost",
+  username: "macadocious",
+  password: "",
+  database: "sandbox"
+  )
+
+require_relative './models/entry'
+
+  get("/welcome") do 
+    @pictures = Picture.all 
+    erb :index
+  end
+
+  get("/welcome") do
+ end
