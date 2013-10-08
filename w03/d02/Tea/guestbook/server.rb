ad231f6dee145f46fea24f8f9ca37b52df6f7eca
teabait <hello.teaho@gmail.com>
2013-10-08 15:38:48 -0400

    ActiveRecord working with HTML guestbook

--- w03/d02/Tea/guestbook/server.rb
+++ w03/d02/Tea/guestbook/server.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'active_record'
 require 'pry'
+require 'sinatra/reloader'
  
 ActiveRecord::Base.establish_connection(
   :adapter => "postgresql",
@@ -11,36 +12,21 @@ ActiveRecord::Base.establish_connection(
 )
 
 require_relative './models/entry'
-
-binding.pry
- 
-guest_book = { 1 => "Jeff", 2 => "PJ", 3 => "Peter" }
- 
-# GET /guest_book
-# List of all people who have ever registered
-# "These people have registered: Jeff, PJ, Peter"
- 
-# { 1 => "Jeff", 2 => "PJ", 3 => "Peter" }
- 
-# Index
  
 get "/guest_book" do
-  names = guest_book.values.join(", ")
-  "These people have registered: #{names}"
+  # accessing information through acessing model Entry that we linked to
+  # via require relative
+  # gives as array
+  @entries = Entry.all
+
+  erb :index
 end
  
 # Show (just one entry)
-# I know the key (or the id)
- 
 get "/guest_book/:id" do
-  id = params[:id].to_i
-  entry = guest_book[id]
- 
-  if entry
-    entry
-  else
-    "NONE FOUND SILLY!"
-  end
+  @entry = Entry.find(params[:id])
+  
+  erb :show
 end
  
 # GET /guest_book/1
@@ -50,17 +36,14 @@ end
 # Each entry should have a unique id
  
 post "/guest_book" do
-  last_id = guest_book.keys.max
-  guest_book[last_id + 1] = params[:name]
+  
 end
  
 # Expect params[:name] == new_name
 put "/guest_book/:id" do
-  id = params[:id].to_i
-  guest_book[id] = params[:name]
+
 end
  
 delete "/guest_book/:id" do
-  id = params[:id].to_i
-  guest_book.delete(id)
+  
 end
