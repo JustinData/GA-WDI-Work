865b5fccf610584b73ed44633b7717214f76f5d9
DrRobotmck <mckenneth.scott@icloud.com>
2013-10-08 14:38:39 -0400

    added views files

--- w03/d02/McKenneth/classwork/post_lunchwork/server.rb
+++ w03/d02/McKenneth/classwork/post_lunchwork/server.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'sinatra/reloader'
 require 'active_record'
 
 require 'pry'
@@ -10,16 +11,14 @@ ActiveRecord::Base.establish_connection(
   :password => "",
   :database => "sandbox"
   )
+ 
  require_relative './models/entry'
 
- binding.pry
 # Create
 # Read
 # Update
 # Destroy
 # Guest Book entries
- 
-guest_book = { 1 => "Jeff", 2 => "PJ", 3 => "Peter" }
  
 # GET /guest_book
 # List of all people who have ever registered
@@ -30,22 +29,14 @@ guest_book = { 1 => "Jeff", 2 => "PJ", 3 => "Peter" }
 # Index
  
 get "/guest_book" do
-  names = guest_book.values.join(", ")
-  "These people have registered: #{names}"
+ 
 end
  
 # Show (just one entry)
 # I know the key (or the id)
  
 get "/guest_book/:id" do
-  id = params[:id].to_i
-  entry = guest_book[id]
- 
-  if entry
-    entry
-  else
-    "NONE FOUND SILLY!"
-  end
+  
 end
  
 # GET /guest_book/1
@@ -55,17 +46,14 @@ end
 # Each entry should have a unique id
  
 post "/guest_book" do
-  last_id = guest_book.keys.max
-  guest_book[last_id + 1] = params[:name]
+  
 end
  
 # Expect params[:name] == new_name
 put "/guest_book/:id" do
-  id = params[:id].to_i
-  guest_book[id] = params[:name]
+  
 end
  
 delete "/guest_book/:id" do
-  id = params[:id].to_i
-  guest_book.delete(id)
+  
 end
