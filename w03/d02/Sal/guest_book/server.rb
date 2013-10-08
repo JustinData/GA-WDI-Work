e3da0d3a002b9719f9821a8855723ae26df2a5a0
slardizzone <slardizzone@gmail.com>
2013-10-08 14:38:43 -0400

    skeleton setup

--- w03/d02/Sal/guest_book/server.rb
+++ w03/d02/Sal/guest_book/server.rb
@@ -12,41 +12,27 @@ ActiveRecord::Base.establish_connection(
 
 require_relative './models/entry'
 
-binding.pry
-
 # Create
 # Read
 # Update
 # Destroy
 # Guest Book entries
 
-guest_book = { 1 => "Jeff", 2 => "PJ", 3 => "Peter" }
-
 # GET /guest_book
 # List of all people who have ever registered
 # "These people have registered: Jeff, PJ, Peter"
 
-# { 1 => "Jeff", 2 => "PJ", 3 => "Peter" }
-
 # Index
 
 get "/guest_book" do
-  names = guest_book.values.join(", ")
-  "These people have registered: #{names}"
+ 
 end
 
 # Show (just one entry)
 # I know the key (or the id)
 
 get "/guest_book/:id" do
-  id = params[:id].to_i
-  entry = guest_book[id]
 
-  if entry
-    entry
-  else
-    "NONE FOUND SILLY!"
-  end
 end
 
 # GET /guest_book/1
@@ -56,17 +42,14 @@ end
 # Each entry should have a unique id
 
 post "/guest_book" do
-  last_id = guest_book.keys.max
-  guest_book[last_id + 1] = params[:name]
+ 
 end
 
 # Expect params[:name] == new_name
 put "/guest_book/:id" do
-  id = params[:id].to_i
-  guest_book[id] = params[:name]
+
 end
 
 delete "/guest_book/:id" do
-  id = params[:id].to_i
-  guest_book.delete(id)
+
 end
