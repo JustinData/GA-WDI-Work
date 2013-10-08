dd1d7138c124a542b23a6fdfa9d246666757f5d0
Nichol Alexander <nichol.alexander@gmail.com>
2013-10-08 18:46:11 -0400

    create new entry, redirect, and display all

--- w03/d02/Nichol/wdinstagram/server.rb
+++ w03/d02/Nichol/wdinstagram/server.rb
@@ -22,3 +22,20 @@ end
 get "/new/?" do
     erb :new
 end
+
+#handle creating new photo
+post "/photo_albumn/?" do
+  
+  entry = Entry.new(author: params[:author], photo: params[:photo], date_taken: params[:date_taken])
+  binding.pry
+  entry.save
+
+  redirect "/photo_albumn/#{entry.id}"
+
+end
+
+#handle showing individual photo
+get "/photo_albumn/:id" do
+    @entry = Entry.find(params[:id])
+    erb :show
+end
