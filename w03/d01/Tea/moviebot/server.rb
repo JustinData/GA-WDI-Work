528a88234897d5c7b8522c6892a20431a08c943c
teabait <hello.teaho@gmail.com>
2013-10-07 21:21:59 -0400

    Nonworking database. Says it doesn't exist.

---
+++ w03/d01/Tea/moviebot/server.rb
@@ -0,0 +1,77 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'httparty'
+require 'json'
+require 'pry'
+require "pg"
+
+FILENAME = "movie_list"
+
+get "/" do
+  erb :greeting
+end
+
+get "/movies/search" do
+  erb :form
+end
+
+post "/movies" do
+  # file = File.new("movies.txt", "a+")
+  db_conn = PG.connect( dbname: FILENAME + "_db", host: "localhost" )
+  title = params[:title]
+  title = title.gsub(" ", "+")
+
+  url = "http://www.omdbapi.com/?t=#{title}"
+
+  # http request to get the movie info
+  response = HTTParty.get(url)
+
+  # Fetches movie data from OMDB
+  parsed = JSON(response)
+
+  # set variables for the title, year, and poster
+  title = parsed["Title"]
+  year = parsed["Year"].to_i
+  poster = parsed["Poster"]
+  plot = parsed["Plot"]
+  plot_reformatted = plot.to_s.gsub("'",'\\\'')
+  # count line.number in the file to assign incremental id for entry
+  # id_num = 0
+  # id_num += file.readlines.size
+
+  # take that info and turn it into a string
+  # movie_info = "#{id_num}, #{title}, #{year}, #{poster}, #{plot}"
+  # Make query
+  query = "INSERT INTO movie_list "
+  query += "(title, date, poster, plot) VALUES"
+  query += "("
+  query += "'#{title}', '#{year}', '#{poster}', '#{plot_reformatted}'"
+  query += ");"
+  db_conn.exec( query )
+
+
+
+  # adds entry as a CSV to movies.txt
+  db_conn.close
+  # file.puts movie_info
+  # file.close
+
+  # redirect to ("movies/#{id_num}")
+
+end
+
+# shows all movies
+get "/movies" do
+  erb :display_movies
+  
+  # * Using a erb "partial" here for a single movie may help you here to keep your code DRY. You'll be able to render that same erb template for EACH movie that you have to display.
+end
+
+get "/movies/:id" do
+
+  if params[:id].to_i.is_a? Fixnum
+    @id_num = params[:id]
+    erb :display_one
+  end
+
+end
