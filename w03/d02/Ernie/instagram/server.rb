e6966782a13cc1301c9c68fcd78a4c28e406f0ab
Ernie Casilla <ecasilla@icloud.com>
2013-10-08 17:53:40 -0400

    Instagram

---
+++ w03/d02/Ernie/instagram/server.rb
@@ -0,0 +1,34 @@
+require 'sinatra'
+require 'pry'
+require 'active_record'
+require 'sinatra/reloader'
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "postgresql",
+  :host => "localhost",
+  :username => "ecasilla",
+  :password => "",
+  :database => "instagram"
+  )
+
+require_relative './models/entry'
+
+get '/gram'do 
+  @entries = Entry.all
+  erb :index
+end
+
+get'/gram/new'do
+  erb :new
+end
+
+get "/gram/:id" do
+  @entry = Entry.find(params[:id])
+  erb :show
+end
+
+post "/gram" do
+  entry = Entry.new(author: params[:author],photos: params[:photos],my_date: params[:date] )
+  entry.save
+  redirect "/gram/#{entry.id}"
+end
