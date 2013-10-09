b4f53e5c83790c5741522584821fdfce56fe504f
daviskoh <koh.davis.0@gmail.com>
2013-10-09 10:28:08 -0400

    working sql imple

--- w03/d03/Davis/morning/morning.rb
+++ w03/d03/Davis/morning/morning.rb
@@ -1,5 +1,6 @@
 require 'faker'
-require 'active_record'
+# require 'active_record'
+require 'pg'
 require 'pry'
 
 class User
@@ -22,21 +23,36 @@ end
 # end
 
 # establish connection
-ActiveRecord::Base.establish_connection(
-  :adapter => "postgresql",
-  :host => "localhost",
-  :username => "daviskoh",
-  :password => "",
-  :database => "sandbox"
-)
+# ActiveRecord::Base.establish_connection(
+#   :adapter => "postgresql",
+#   :host => "localhost",
+#   :username => "daviskoh",
+#   :password => "",
+#   :database => "sandbox"
+# )
 
-require_relative './morning_user.rb'
+# require_relative './morning_user.rb'
 
-(1..50).each do |n|
-	name = Faker::Name.name
-	street_address = Faker::Address.street_address
-	email_address = Faker::Internet.email
+# (1..50).each do |n|
+# 	name = Faker::Name.name
+# 	street_address = Faker::Address.street_address
+# 	email_address = Faker::Internet.email
 	
-	morning_user = MorningUser.new(name: name, street_address: street_address, email_address: email_address)
-	morning_user.save
+# 	morning_user = MorningUser.new(name: name, street_address: street_address, email_address: email_address)
+# 	morning_user.save
+# end
+
+db_conn = PG.connect( dbname: "sandbox" )
+
+50.times do
+	name = Faker::Name.name.gsub("'","")
+	street_address = Faker::Address.street_address.gsub("'","")
+	email_address = Faker::Internet.email
+
+	query_str = "INSERT INTO morning_users (name,street_address,email_address) "
+	query_str += "VALUES ('#{name}','#{street_address}','#{email_address}');"
+
+	db_conn.exec( query_str )
 end
+
+db_conn.close
