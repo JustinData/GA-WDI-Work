88e29254c72fe56b6b1810541b0a179193960ba5
daviskoh <koh.davis.0@gmail.com>
2013-10-09 09:31:02 -0400

    working morning ex

---
+++ w03/d03/Davis/morning/morning.rb
@@ -0,0 +1,22 @@
+require 'faker'
+require 'pry'
+
+class User
+	def initialize(id, name, street_address, email_address)
+		@id = id
+		@name = name
+		@street_address = street_address
+		@email_address = email_address
+	end
+
+	def to_s
+		"Person #{@name} has id of #{@id} lives in #{@street_address}. Contact at #{@email_address}."
+	end
+end
+
+hash = {}
+
+(1..50).each do |n|
+	hash[n.to_s] = User.new(n, Faker::Name.name, Faker::Address.street_address, Faker::Internet.email)
+end
+
