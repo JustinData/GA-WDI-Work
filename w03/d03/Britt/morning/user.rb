09639beb0e459ea78e63d8376d137f8f801fab5d
brittlewis12 <brittlewis12@gmail.com>
2013-10-09 09:31:18 -0400

    added id to User class

--- w03/d03/Britt/morning/user.rb
+++ w03/d03/Britt/morning/user.rb
@@ -4,24 +4,26 @@ require 'pry'
 class User
 	attr_reader :name, :address, :email
 
-	def initialize(name,address,email)
+	def initialize(id, name,address,email)
+		@id = id
 		@name = name
 		@address = address
 		@email = email
 	end
 
 	def to_s
-		"#{@name} has an email #{@email} and lives at #{@address}."
+		"Name:#{@name} \nEmail: #{@email} \nAddress: #{@address}."
 	end
 end
 
 @junk_data = {}
 
 50.times do |i|
+	id = i
 	name = Faker::Name.name
 	address = Faker::Address.street_address
 	email = Faker::Internet.email
 
-	@junk_data[i] = User.new(name, address, email)
+	@junk_data[id] = User.new(id, name, address, email)
 	
 end
