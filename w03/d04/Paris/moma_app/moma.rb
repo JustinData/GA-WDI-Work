688c0f22b25cba862127e7fa3e95f5d6d34ae793
My Name <myemail@example.com>
2013-10-10 19:17:17 -0400

    forgot to save some files jic

--- w03/d04/Paris/moma_app/moma.rb
+++ w03/d04/Paris/moma_app/moma.rb
@@ -19,16 +19,5 @@ class Artist < ActiveRecord::Base
 end
 
 binding.pry
-# gogh = Artist.new("Vincent van Gogh", "Dutch", "March 30, 1853")
-
-# rum = Painting.new("Still Life with a Bottle of Rum", 1911, "http://upload.wikimedia.org/wikipedia/en/7/7f/Pablo_Picasso%2C_1911%2C_Still_Life_with_a_Bottle_of_Rum%2C_oil_on_canvas%2C_61.3_x_50.5_cm%2C_Metropolitan_Museum_of_Art%2C_New_York.jpg")
-
-#   irises = Painting.new("Irises", 1889, "http://www.vangoghgallery.com/catalog/image/0608/Irises.jpg")
-
-#   potato = Painting.new("The Potato Eaters", 1885, "http://upload.wikimedia.org/wikipedia/commons/b/b1/Van-willem-vincent-gogh-die-kartoffelesser-03850.jpg")
 
 
-# gogh.save
-# rum.save
-# irises.save
-# potato.save
