13edd1fe15152810cca8f09212f4f7697c9ecc92
Maria Mercedes Martinez <pistachiopony@gmail.com>
2013-10-10 15:56:08 -0400

    day 3 stuff

--- w03/d03/Maria/morning/user_db.rb
+++ w03/d03/Maria/morning/user_db.rb
@@ -1,20 +1,18 @@
+require 'pg'
 require 'faker'
-require_relative 'user'
 
-for x in 1..50 do 
+db_conn = PG.connect(:dbname => 'sandbox', :host => 'localhost')
 
- id = x
- name = Faker::Name.name
- street = Faker::Address.street_address
- email = Faker::Internet.email_address
- user_object = User.new(id, name, street_address, email )
- user_hash[id] = user_object
- morning_user.insert
+50.times do
+  name = Faker::Name.name.gsub("'","")
+  street_address = Faker::Address.street_address.gsub("'","")
+  email_address = Faker::Internet.email
+
+  sql = "INSERT INTO morning_users (name, street_address, email_address)"
+  sql += "VALUES ( '#{name}', '#{street_address}', '#{email_address}');"
+
+  db_conn.exec(sql)
+
 end
 
-# also can be 
-#(1..50).each do |n|
-#etc like above
-
-#open psql connect to db \c sandbox
-#psql -d sandbox -h localhost -f schema.sql
+db_conn.close
