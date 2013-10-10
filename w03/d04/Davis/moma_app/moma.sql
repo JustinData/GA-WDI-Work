ef632154aed4bbc6df8c1e9cb1ad07feef3c8929
daviskoh <koh.davis.0@gmail.com>
2013-10-10 16:17:41 -0400

    part1 working db

---
+++ w03/d04/Davis/moma_app/moma.sql
@@ -0,0 +1,15 @@
+CREATE TABLE artists (
+	id serial PRIMARY KEY,
+	name varchar(50) NOT NULL,
+	birth_date date NOT NULL
+);
+
+CREATE TABLE paintings (
+	id serial PRIMARY KEY,
+	title varchar(100) NOT NULL,
+	year integer NOT NULL,
+	image_url varchar(300),
+	-- painting has only 1 artist, needs to ref artist id
+	artist_id integer references artists(id)
+);
+
