d8f55600a67ab6f9aa95b039bcfb8256514af7f3
Jeffrey Konowitch <jeff.konowitch@generalassemb.ly>
2013-10-10 16:02:33 -0400

    clowns

---
+++ w03/d04/Instructor/clowns/schema.sql
@@ -0,0 +1,17 @@
+create table clown_cars (
+  id serial primary key,
+  color varchar(20) not null,
+  capacity integer not null
+);
+
+create table clowns (
+  id serial primary key,
+  name varchar(50) not null,
+  clown_car_id integer references clown_cars(id)
+);
+
+-- Bozo, Pozo, and Dozo
+
+-- Blue, 2
+
+-- Bozo -> Blue clown car
