5592888df5fdaca7d932af1c492efb855ce890c8
My Name <myemail@example.com>
2013-10-11 19:54:14 -0400

    working code broadway.ly app

--- w03/d05/Paris/broadway/models/broadway.rb
+++ w03/d05/Paris/broadway/models/broadway.rb
@@ -1,5 +1,25 @@
-class Show < ActiveRecord::Base
-end
+require 'pry'
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(
+  adapter: "postgresql",
+  host: "localhost",
+  username: "Paris",
+  password: "",
+  database: "broadway_db"
+)
 
 class Song < ActiveRecord::Base
+  belongs_to :shows
+
+  validates :song_title, :embed_url, presence: true, uniqueness: true
 end
+
+
+class Show < ActiveRecord::Base
+  has_many :songs
+
+  validates :title, :year, :composer, :img_url, presence: true, uniqueness: true
+end
+
+binding.pry
