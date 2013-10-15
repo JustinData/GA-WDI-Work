e2dd47328e1654553b6848f2e32550ffd1bfea14
Raleigh D Froeber <RDFroeber@gmail.com>
2013-10-15 19:02:59 -0400

    Search by author functionality added

--- w04/d02/Raleigh/wdinstagram2_app/app/controllers/entries_controller.rb
+++ w04/d02/Raleigh/wdinstagram2_app/app/controllers/entries_controller.rb
@@ -1,26 +1,26 @@
 class EntriesController < ApplicationController
    def index
-    @instagrams = Entry.all
-    render :index
-  end
+      @instagrams = Entry.all
+      render :index
+   end
 
-  def new
-    render :new
-  end
+   def new
+      render :new
+   end
 
-  def create
-    new_gram = Actor.new
-    new_gram.author = params[:author]
-    new_gram.photo_url = params[:photo_url]
-    new_gram.date_taken = params[:date_taken]
+   def create
+       new_gram = Entry.new
+       new_gram.author = params[:author]
+       new_gram.photo_url = params[:photo_url]
+       new_gram.date_taken = params[:date_taken]
 
-    new_gram.save
+       new_gram.save
 
-    redirect_to "/..."
-  end
+       redirect_to "/entries"
+   end
 
-  def show
-    @instagram = Entry.find(params[:id])
-    render :show
-  end
+   def show
+       @instagram = Entry.find(params[:id])
+       render :show
+   end
 end
