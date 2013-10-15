3dfc1e82791769be2415262afa80e8bd89597c9a
DrRobotmck <mckenneth.scott@icloud.com>
2013-10-15 11:58:22 -0400

    routing test/practice

--- w04/d02/McKenneth/my_first_app/config/routes.rb
+++ w04/d02/McKenneth/my_first_app/config/routes.rb
@@ -3,7 +3,13 @@ MyFirstApp::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  
+  # root 'static#index'
+  #   get 'woah' => 'static#woah'
+
+  root 'actor#about'
+  
+  get 'film' => 'actor#film'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
