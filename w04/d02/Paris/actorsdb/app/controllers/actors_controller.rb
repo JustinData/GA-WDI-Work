fa5fd03a000f789be74ea287719910a4b564ab6a
My Name <myemail@example.com>
2013-10-15 16:31:36 -0400

    working code

--- w04/d02/Paris/actorsdb/app/controllers/actors_controller.rb
+++ w04/d02/Paris/actorsdb/app/controllers/actors_controller.rb
@@ -5,4 +5,24 @@ class ActorsController < ApplicationController
     render :index
   end
 
+  def new
+    render :new
+  end
+
+  def create
+        actor = Actor.new(name: params[:name], photo_url: params[:photo_url], birth_date: params[:birth_date])
+    actor.save
+    redirect_to "/actors"
+  end
+
+  def show
+    @actor = Actor.find(params[:id])
+    render :show
+  end
+
 end
+
+# actor.name = params[:name]
+# etc.
+
+# <!--   <li><a href="<%= "/shows/#{show.id}"%>"><img src= '<%= show.img_url %>'></a></li> -->
