438ccbc8d69923d5e8d26f108fbbf9890b939a32
Jeffrey Konowitch <jeff.konowitch@generalassemb.ly>
2013-10-15 16:32:26 -0400

    finished

--- w04/d02/Instructor/actordb/app/controllers/actors_controller.rb
+++ w04/d02/Instructor/actordb/app/controllers/actors_controller.rb
@@ -5,4 +5,24 @@ class ActorsController < ApplicationController
     render :index
   end
 
+  def new
+    render :new
+  end
+
+  def create
+    actor = Actor.new
+    actor.name = params[:name]
+    actor.photo_url = params[:photo_url]
+    actor.birth_date = params[:birth_date]
+
+    actor.save
+
+    redirect_to "/actors"
+  end
+
+  def show
+    @actor = Actor.find(params[:id])
+    render :show
+  end
+
 end 
