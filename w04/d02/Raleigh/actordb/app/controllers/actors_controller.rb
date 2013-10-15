0c855ad57e4de927c9a3af406240763987d0c80c
Raleigh D Froeber <RDFroeber@gmail.com>
2013-10-15 16:33:14 -0400

    Actor individual pages

--- w04/d02/Raleigh/actordb/app/controllers/actors_controller.rb
+++ w04/d02/Raleigh/actordb/app/controllers/actors_controller.rb
@@ -5,4 +5,22 @@ class ActorsController < ApplicationController
       render :index
    end
 
+   def new
+      render :new
+   end
+
+   def create
+      new_actor = Actor.new
+      new_actor.name = params[:name]
+      new_actor.photo_url = params[:photo_url]
+      new_actor.birth_date = params[:birth_date]
+      new_actor.save
+
+      redirect_to '/actors'
+   end
+
+   def show
+      @a_actor = Actor.find(params[:id])
+   end
+
 end
