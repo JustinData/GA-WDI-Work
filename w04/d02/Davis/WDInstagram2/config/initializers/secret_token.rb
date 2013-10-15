25b7648dfcb414d32e3fb600969e143288c56987
daviskoh <koh.davis.0@gmail.com>
2013-10-15 16:51:27 -0400

    create WDInstagram app

---
+++ w04/d02/Davis/WDInstagram2/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WDInstagram2::Application.config.secret_key_base = '838994fabfdf7e8899d4c955cf721e5d17bd15da44f490b9a5bd29456294d29c453f5879c946fd8d6e82aafcaf0e2dfea81948ce75c04da6dbee2b8fadab89e1'
