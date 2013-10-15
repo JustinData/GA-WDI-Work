46615cecfa555057a6a1bf588d3d9c30c386c272
Erica Obersi <evobersi@gmail.com>
2013-10-15 16:31:36 -0400

    CRUD app ActorsDB

--- actordb/app/controllers/actors_controller.rb
+++ actordb/app/controllers/actors_controller.rb
@@ -5,5 +5,27 @@ class ActorsController < ApplicationController
     @actors = Actor.all 
     render :index
   end
+
+  def new
+    render :new
+  end
+
+  def create
+    actor = Actor.new
+    actor.name = params[:name]
+    actor.photo_url = params[:photo_url]
+    actor.birth_date = params[:birth_date]
+    actor.save
+    redirect_to "/actors"
+  end
+
+  def show 
+  @actor = Actor.find(params[:id])
+  render  :show
+  end
+
 end
+
+
+
 
--- actordb/config/routes.rb
+++ actordb/config/routes.rb
@@ -1,4 +1,4 @@
 Actordb::Application.routes.draw do
-  resources :actors, only: [:index, :new]
+  resources :actors, only: [:index, :new, :create, :show]
 end
 
