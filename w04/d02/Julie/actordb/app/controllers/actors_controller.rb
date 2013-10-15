9edd984fb75f99cf406793b604277d37d9d49734
Julie lappano <julie.lappano@gmail.com>
2013-10-15 16:43:04 -0400

    CRUD is uppp

--- w04/d02/Julie/actordb/app/controllers/actors_controller.rb
+++ w04/d02/Julie/actordb/app/controllers/actors_controller.rb
@@ -5,4 +5,19 @@ class ActorsController < ApplicationController
     render :index
   end
 
+  def new
+    render :new
+  end
+
+  def create
+    Actor.create(name: params[:name], photo_url: params[:photo_url], birth_day: params[:birth_day])
+
+    redirect_to "/actors"
+  end
+
+  def show
+    @actor = Actor.find(params[:id])
+    render :show
+  end
+
 end
