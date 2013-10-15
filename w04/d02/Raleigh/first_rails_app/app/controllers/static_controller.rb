805dab9c2161a18afb25a0790f5b934c2d96296a
Raleigh D Froeber <RDFroeber@gmail.com>
2013-10-15 11:58:19 -0400

    Static route examples

---
+++ w04/d02/Raleigh/first_rails_app/app/controllers/static_controller.rb
@@ -0,0 +1,10 @@
+class StaticController < ApplicationController
+   def index
+      render :index
+   end
+
+   def woah
+      render :woah
+   end
+
+end
--- w04/d02/Raleigh/first_rails_app/config/routes.rb
+++ w04/d02/Raleigh/first_rails_app/config/routes.rb
@@ -3,7 +3,13 @@ FirstRailsApp::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'static#index'
+
+  get 'woah' => 'static#woah'
+
+  get 'about' => 'actor#about'
+
+  get 'film' => 'actor#film'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
