18a6b3327adcef24257e6ada66875f8a94508cb3
Aby Bandoh <aby.bandoh@gmail.com>
2013-10-15 22:07:00 -0400

    HW W4D2

--- w04/d02/Aby/homework/wdinstagram/config/routes.rb
+++ w04/d02/Aby/homework/wdinstagram/config/routes.rb
@@ -1,6 +1,4 @@
-Actordb::Application.routes.draw do
-  resources :actors, only: [:index, :new, :create, :show]
-end
+Wdinstagram::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -55,3 +53,4 @@ end
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+end
