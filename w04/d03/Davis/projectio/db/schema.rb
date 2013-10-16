208fa1f82cf030d9018a636fcdf709cf35925617
daviskoh <koh.davis.0@gmail.com>
2013-10-16 12:32:26 -0400

    migrations

--- w04/d03/Davis/projectio/db/schema.rb
+++ w04/d03/Davis/projectio/db/schema.rb
@@ -11,15 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131016160129) do
+ActiveRecord::Schema.define(version: 20131016162637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "students", force: true do |t|
-    t.string "name"
-    t.text   "project_description"
-    t.text   "github_link"
+    t.string   "name"
+    t.text     "project_description"
+    t.text     "github_link"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
