0b07796344ec9bb5105aae890fc8254cd5d2801c
Ernie Casilla <ecasilla@icloud.com>
2013-10-16 16:57:35 -0400

    project.io

--- w04/d03/Ernie/project.io/db/migrate/20131016160109_students.rb
+++ w04/d03/Ernie/project.io/db/migrate/20131016160109_students.rb
@@ -1,4 +1,12 @@
 class Students < ActiveRecord::Migration
-  def change
+  def up
+    create_table :students do |t|
+      t.string :name
+      t.text :project_description
+      t.string :github_link
+    end
+  end
+  def down
+    drop_table :students
   end
 end
