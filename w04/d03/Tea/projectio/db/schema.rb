5244fd22bc71d4b704aea668a82014a27b6ce6b2
teabait <hello.teaho@gmail.com>
2013-10-16 12:33:51 -0400

    Add Timestamp Column

--- w04/d03/Tea/projectio/db/schema.rb
+++ w04/d03/Tea/projectio/db/schema.rb
@@ -11,15 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131016161605) do
+ActiveRecord::Schema.define(version: 20131016162638) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "students", force: true do |t|
-    t.string "name"
-    t.text   "project_description"
-    t.string "github_link"
+    t.string   "name"
+    t.text     "project_description"
+    t.string   "github_link"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
