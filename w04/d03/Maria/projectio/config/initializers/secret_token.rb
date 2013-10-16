a1f7d0e9a47401fbe1775a0cacdaf15b8c533703
Maria Mercedes Martinez <pistachiopony@gmail.com>
2013-10-16 12:08:59 -0400

    add notes

---
+++ w04/d03/Maria/projectio/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Projectio::Application.config.secret_key_base = '3e17507241c038c8d970134eb47275b3b0300d727da7816143f62d3e822f31f632363a768bca1fa9de5b601de573a9921fafba2ab0a6d74d79267424c36a0fe0'
