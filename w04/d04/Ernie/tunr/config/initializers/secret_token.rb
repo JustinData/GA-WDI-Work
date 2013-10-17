207041a785f96d417575731cf6f6ac20b1452d71
Ernie Casilla <ecasilla@icloud.com>
2013-10-17 10:40:07 -0400

    tunr inital commit

---
+++ w04/d04/Ernie/tunr/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tunr::Application.config.secret_key_base = 'e52c64ed639da721200df1add359cfd6b8a0c92f2318731e0350f3ba1d2605bc4f9763046a55820b99ceefcb189c6dc5c8efdfa82669cb1878a2de76d4510710'
