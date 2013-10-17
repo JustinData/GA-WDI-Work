7031c3e08b1c7cdaf28495d00f0bac55f996051c
Ernie Casilla <ecasilla@icloud.com>
2013-10-17 13:19:30 -0400

    tunr WDI

--- w04/d04/Ernie/tunr/config/routes.rb
+++
@@ -1,5 +0,0 @@
-Tunr::Application.routes.draw do
-
-  resources :artists, only: [:index, :show]
-  resources :songs, only: [:index, :show]
-end
--- w04/d04/Ernie/tunr/db/seeds.rb
+++
@@ -1,5 +0,0 @@
-Song.destroy_all
-Artist.destroy_all
-
-kesha = Artist.create(name: "Ke$ha")
-kesha.songs << Song.new(name: 'warrior', spotify_uri: "spotify:track:0EX9OHeYY10o6QWPa1nuAQ")
