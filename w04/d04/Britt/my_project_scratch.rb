4c18465f3eb5eb74fccaa805438df5972ef040ad
brittlewis12 <brittlewis12@gmail.com>
2013-10-18 15:10:54 -0400

    NOW I added the trackName method.

--- w04/d04/Britt/my_project_scratch.rb
+++ w04/d04/Britt/my_project_scratch.rb
@@ -10,6 +10,11 @@ def gimme_da_id(search_term)
   response["results"][0]["artistId"]
 end
 
+def gimme_da_track_name(search_term)
+  query = search_term.gsub(" ","+")
+  response = HTTParty.get("https://itunes.apple.com/search?term=#{query}&entity=software&limit=1")
+  response["results"][0]["trackName"]
+end
 # Nokogiri
 #
 # CSS selectors: .app-version-note p , h5
