a67d53b60652946549bae52b475cf0e50911b6e9
daviskoh <koh.davis.0@gmail.com>
2013-10-28 17:05:44 -0400

    js late afternoon ex

--- w06/d01/Davis/intro.js
+++ w06/d01/Davis/intro.js
@@ -2,6 +2,66 @@
 // console.log is puts
 window.onload = console.log( "Hello World!" );
 
-console.log( "Hello world?" );
-a = 1;
+function add( val1, val2 ) {
+  return val1 + val2;
+}
+
+function subtract(val1,val2) {
+  return val1 - val2;
+}
+
+function multiply(val1,val2) {
+  return val1 * val2;
+}
+
+function divide(val1,val2) {
+  return val1 / val2;
+}
+
+function mod(val1,val2) {
+  return val1 % val2;
+}
+
+// function exp(base, power) {
+//   if( val2 == 0 ) {
+//     return val1;
+//   }
+
+//   result = base;
+//   for(val i = 1; i < power; i++) {
+//     result = result * base
+//   };
+
+//   return result;
+// }
+
+function srt( val ) {
+  var i = 0;
+
+  for(var i=0; i <= val; i++) {
+    if(val / i === i) {
+      return i;
+    };
+  };
+}
+
+function hypotenuse( arr1, arr2 ) {
+
+}
+
+function hello(){
+  return "Hello world!";
+}
+
+
+// pass in blocks, usingimplicit returns
+function hello( val, quoter ) {
+  return "Hello " + quoter( val ) + "!";
+}
+function x( arg ) {
+  return "'" + arg + "'";
+}
+function y( arg ) {
+  return "+" + arg + "+";
+}
 
