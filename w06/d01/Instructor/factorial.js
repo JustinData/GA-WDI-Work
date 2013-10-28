e1e8390fe6b5e8e775fc538da028e033e636263a
Peter Lai <peter.yd.lai@gmail.com>
2013-10-28 16:35:27 -0400

    add homework

---
+++ w06/d01/Instructor/factorial.js
@@ -0,0 +1,17 @@
+// Copy and paste this into the JavaScript console in Chrome!
+
+function factorial(num)
+{
+    console.log("We are calling the factorial function on: " + num);
+    if (num === 1) {
+        console.log("The number is now " + num);
+        return 1;
+    }
+    else {
+        var factorialOfOneLess = factorial(num - 1);
+        console.log("The number is now " + num + " and the factorial of one less than that number is: " + factorialOfOneLess);
+        return (num * factorialOfOneLess);
+    }
+}
+
+factorial(5);
