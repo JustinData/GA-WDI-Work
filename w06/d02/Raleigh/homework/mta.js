37ab53aa05015e24394db8d4ba4285fc6a176f87
Raleigh D Froeber <RDFroeber@gmail.com>
2013-10-29 19:30:51 -0400

    Geometry homework mostly done

---
+++ w06/d02/Raleigh/homework/mta.js
@@ -0,0 +1,26 @@
+//MTA
+
+var start_line = prompt("Which line would you like to start on?");
+var start_station = prompt("Which station would you like to start at?");
+var end_line = prompt("Which line would you like to end on?");
+var end_station = prompt("Which station would you like to end at?");
+
+function Line(){
+   distance
+};
+
+Line.prototype.distance = function(start, end) {
+   // body...
+};
+
+function SubwaySystem(){
+   takeTrip = 
+   line_n = ["Times Square", "34th", "28th-North", "23rd-North", "Union Square", "8th"]
+   line_l = ["8th", "6th", "Union Square", "3rd", "1st"]
+   line_six = ["Grand Central", "33rd", "28th-South", "23rd-South", "Union Square", "Astor"]
+   line_one = ["59th", "50th", "Times Square", "34th", "28th"]
+};
+
+
+
+alert("You have " + " stops.")
