1388ff21e6a586f0b5d7e816f4f83b37aaa2e5f9
teabait <hello.teaho@gmail.com>
2013-10-29 16:32:10 -0400

    Calc

--- w06/d02/Tea/calc/spec/calc_spec.rb
+++ w06/d02/Tea/calc/spec/calc_spec.rb
@@ -24,31 +24,52 @@ describe Calculator do
       expect(Calculator.new.sum([])).to eq(0)
     end
 
-    it "computes the sum of an array of one number"
-    it "computes the sum of an array of two numbers"
-    it "computes the sum of an array of many numbers"
+    it "computes the sum of an array of one number" do
+      expect(Calculator.new.sum([8])).to eq(8)
+    end
+    it "computes the sum of an array of two numbers" do
+      expect(Calculator.new.sum([8, 10])).to eq(18)
+    end
+    it "computes the sum of an array of many numbers" do
+      expect(Calculator.new.sum([8, 10, 20, 40, 1, 3, 7])).to eq(89) 
+    end
   end
 
 # Once the above tests pass, fill in the tests and code for the following:
 
   describe "#multiply" do
 
-    it "multiplies two numbers"
-    it "multiplies several numbers"
-
+    it "multiplies two numbers" do
+      expect(Calculator.new.mul(2, 4)).to eq(8)
+    end
+    it "multiplies several numbers" do
+      expect(Calculator.new.mul(2, 4, 1)).to eq(8)
+    end
   end
 
   describe "#power" do
-    it "raises one number to the power of another number"
+    it "raises one number to the power of another number" do
+      expect(Calculator.new.pow(2, 3)).to eq(8)
+    end
   end
 
   # You can do this either using a loop or recursively
   describe "#factorial" do
-    it "computes the factorial of 0"
-    it "computes the factorial of 1"
-    it "computes the factorial of 2"
-    it "computes the factorial of 5"
-    it "computes the factorial of 10"
+    it "computes the factorial of 0" do
+      expect(Calculator.new.fac(0)).to eq(0)
+    end
+    it "computes the factorial of 1" do
+      expect(Calculator.new.fac(1)).to eq(1)
+    end
+    it "computes the factorial of 2" do
+      expect(Calculator.new.fac(2)).to eq(2)
+    end
+    it "computes the factorial of 5" do
+      expect(Calculator.new.fac(5)).to eq(120)
+    end
+    it "computes the factorial of 10" do
+      expect(Calculator.new.fac(10)).to eq(3628800)
+    end
   end
 
 end
