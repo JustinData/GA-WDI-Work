318396706cde00908955dc0655722ce63b7dd724
daviskoh <koh.davis.0@gmail.com>
2013-10-29 18:21:38 -0400

    part 1 complete

--- w06/d02/Davis/hw/geometry.js
+++ w06/d02/Davis/hw/geometry.js
@@ -20,4 +20,19 @@ function Triangle(sideA, sideB, sideC) {
   Triangle.prototype.isEquilateral = function() {
     return this.sideA === this.sideB && this.sideB === this.sideC;
   }
-  Triangle.prototype
+  Triangle.prototype.isIsosceles = function() {
+    return this.sideA === this.sideB || this.sideB === this.sideC || this.sideA === this.sideC;
+  }
+  Triangle.prototype.area = function() {
+    s = (this.sideA + this.sideB + this.sideC) / 2;
+    under = s * (s - this.sideA) * (s - thisSideB) * (s - thisSideC);
+    return Math.sqrt(under);
+  }
+  Triangle.prototype.isObtuse = function() {
+    aSq = this.sideA * this.sideA;
+    bSq = this.sideB * this.sideB;
+    cSq = this.sideC * this.sideC;
+    return (aSq + bSq) < cSq || (bSq + cSq) < aSq || (cSq + aSq) < bSq;
+  }
+
+  
