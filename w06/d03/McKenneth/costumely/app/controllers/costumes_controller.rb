957fec8dc849b8b8f7e584ea8ddd94f415b0d679
DrRobotmck <mckenneth.scott@icloud.com>
2013-10-30 10:37:26 -0400

    finished most routes for costume

--- w06/d03/McKenneth/costumely/app/controllers/costumes_controller.rb
+++ w06/d03/McKenneth/costumely/app/controllers/costumes_controller.rb
@@ -2,10 +2,11 @@ class CostumesController < ApplicationController
 
 
   def index
-    @costumes = Costume.all
+    @costume = Costume.all
     render :index
   end
   def show
+    @costume = Costume.find(params[:id])
     render :show
   end
 
@@ -16,7 +17,23 @@ class CostumesController < ApplicationController
 
   def create
     @costume = Costume.new(costume_params)
+    @costume.save
   end
+
+  def edit
+    @costume = Costume.find(params[:id])
+    render :edit
+  end
+
+  def update
+    if @costume.update_attributes(costume_params)
+      redirect_to costume_path(@costume)
+    else
+      render :edit
+    end
+  end
+
+
 end
 
 private
