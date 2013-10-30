18b498adfd9f4439c99bf81520ff6e9a81ed25c8
Russell Taff <rtaff2012@gmail.com>
2013-10-30 00:28:04 -0400

    file changes to day work

--- w06/d02/Russell/js/objects.js
+++ w06/d02/Russell/js/objects.js
@@ -1,48 +1,46 @@
+// name, favoriteColor, disneyCharacter
 var me = {
-  name: "Russell",
-  favoriteColor: "pewter",
-  disneyCharacter: "Rafiki",
+  name: "Jeff",
+  favoriteColor: "Blue",
+  disneyCharacter: "Baloo",
   hiya: sayHello,
-  character: sayDisney,
-  }
-
-
+  disney: whatsMyCharacter
+}
 
 function sayHello() {
-  return "HELLO! I'm " + this.name;
-
+  return "Hello, I'm " + this.name;
 }
 
-function sayDisney() {
-  return "Hello! I'm a lot like" + this.disneyCharacter;
-
+function whatsMyCharacter() {
+  return "I am similar to " + this.disneyCharacter;
 }
 
-for (var key in me) {
-  console.log(key, me[key]);
-}
+// for (var currentKey in me) {
+//   console.log(currentKey, me[currentKey]);
+// }
 
 function forEachObj(obj, action) {
- for (var key in obj) {
-  action(key, obj[key]);
- }
-
+  for (var key in obj) {
+    action(key, obj[key]);
+  }
 }
 
 function keys(obj) {
   var keys = [];
+
   for (var key in obj) {
     keys.push(key);
   }
+
   return keys;
 }
 
-
+/* 
 var shmoopy = {
   name: "Shmoopy",
   onTree: false,
-  speak: function(){
-    return "Hello, I am" + this.name;
+  speak: function() {
+    return "Hello, I am " + this.name;
   },
   climb: function() {
     this.onTree = true;
@@ -52,13 +50,14 @@ var shmoopy = {
 var bloopy = {
   name: "Bloopy",
   onTree: false,
-  speak: function(){
-    return "Hello, I am" + this.name;
+  speak: function() {
+    return "Hello, I am " + this.name;
   },
   climb: function() {
     this.onTree = true;
   }
 }
+*/
 
 function Monkey(name) {
   this.name = name;
@@ -66,17 +65,34 @@ function Monkey(name) {
 }
 
 Monkey.prototype.speak = function() {
-  return "Hello, I am" + this.name;
+  return "Hello, I am " + this.name;
 }
 
 Monkey.prototype.climb = function() {
   return this.onTree = true;
 }
 
+var shmoopy = new Monkey("Shmoopy");
+var bloopy = new Monkey("Bloopy");
 
 
+/*
+forEachObj(me, function(key, val) {
+  // have access to both key and value for
+  // each key/value pair in the obj
+  console.log("They key is: " + key);
+  console.log("The value is: " + val);
+});
 
 
+keys(me);
+#=> ["name", "favoriteColor", "disneyCharacter"]
+
+*/
 
 
+// "I am similar to ___"
 
+// for (var i = 0; i < array.length; i++) {
+//   array[i];
+// }
--- w06/d02/Russell/js/shelter.js
+++ w06/d02/Russell/js/shelter.js
@@ -1,15 +1,32 @@
+// happitails = new Shelter("Happitails")
 
+// happitails.animals
+// [animal1, animal2]
+
+// happitails.adopt(animal)
+// [animal]
+
+// happitails.toString()
+// "Happitails has x animals up for adoption"
+
+// happitails.hasAnimal(animal)
+// true/false
+
+// lincoln = new Animal("Lincoln", "Dog");
+
+// lincoln.name && lincoln.type
+
+// lincoln.speak()
+// "Hi! I am a Dog named Lincoln."
 
 
 function Shelter(name) {
   this.name = name;
-  this.animals - [];
+  this.animals = [];
 }
-
 
 Shelter.prototype.adopt = function(animal) {
   this.animals.splice(this.animals.indexOf(animal), 1);
-
 }
 
 Shelter.prototype.rescue = function(animal) {
@@ -17,10 +34,8 @@ Shelter.prototype.rescue = function(animal) {
 }
 
 Shelter.prototype.toString = function() {
-  return this.name + " has " + this.animals.length + " animals up for adoption ";
+  return this.name + " has " + this.animals.length + " animals up for adoption";
 }
-
-
 
 Shelter.prototype.hasAnimal = function(animal) {
   return this.animals.indexOf(animal) != -1;
@@ -29,20 +44,9 @@ Shelter.prototype.hasAnimal = function(animal) {
 function Animal(name, type) {
   this.name = name;
   this.type = type;
-
 }
 
 Animal.prototype.speak = function() {
   return "Hi! I am a " + this.type + " named " + this.name;
 }
 
-// shelter name = happitails
-
-// happitails has x animals up for adoption
-
-// happitails.hasAnimal(animal)
-// true/false
-
-// lincoln = new Animal('Lincoln', "Dog")
-
-// lincoln name && lincoln type
