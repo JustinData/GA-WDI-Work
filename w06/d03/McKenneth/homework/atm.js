221e3c31f04dba09eb67be92cc6f8d6de026c51a
DrRobotmck <mckenneth.scott@icloud.com>
2013-10-30 18:56:56 -0400

    withdrawal for checking account works

--- w06/d03/McKenneth/homework/atm.js
+++ w06/d03/McKenneth/homework/atm.js
@@ -1,14 +1,30 @@
 window.onload = function(){
-
-var deposit = document.getElementById("checking_deposit");
 var balance = 0;
+var deposit = document.getElementById("checking_deposit");
+var withdraw = document.getElementById("checking_withdraw");
+// var amount = document.getElementById("checking_amount").value;
+var displayedBalance = document.getElementById("checking_balance");
 
 deposit.onclick = function(){
   var amount = document.getElementById("checking_amount").value;
     balance += parseInt(amount);
 
-  var displayedBalance = document.getElementById("checking_balance");
+  // var displayedBalance = document.getElementById("checking_balance");
   displayedBalance.innerHTML = balance;
 };
+
+withdraw.onclick = function(){
+  var amount = document.getElementById("checking_amount").value;
+    if (amount <= balance ){
+    balance -= parseInt(amount);
+    }
+  // var displayedBalance = document.getElementById("checking_balance");
+  displayedBalance.innerHTML = balance;
+};
+
+
+
+
+
 
 }
