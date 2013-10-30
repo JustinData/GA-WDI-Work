b4a39e9e24fe285a6f2b40b2023f380d2e216dd7
Tom Brennan <tommybrennan@gmail.com.com>
2013-10-30 17:13:07 -0400

    committing before adding the setTimer on the cheeseburger cat page

---
+++ w06/d03/Tom/dom.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+  console.log("Are we ready?");
+
+  /* By placing in window.onload we make sure that the window has loaded before it does anything else */
+  var cheesy = document.getElementById("add-cheese");
+  cheesy.onclick = function(event){
+    alert("now we're ready!");
+  };
+
+  var buildbackground = function(){
+    var element = document.getElementById( "lightbox" );
+    element.style.backgroundImage = "url('http://placekitten.com/" + (Math.floor(Math.random() * (500 - 300)) + 300).toString() + "/" + (Math.floor(Math.random() * (500 - 300)) + 300).toString() + "')";
+  };
+
+  buildbackground();
+};
+
