b771732848d5e5673eac282f5e9416f73dce56d8
Mihran Abrahamian <abrahamian.mihran@gmail.com>
2013-10-30 10:54:12 -0400

    controller

--- w06/d03/Mihran/costumely/app/controllers/costumes_controller.rb
+++ w06/d03/Mihran/costumely/app/controllers/costumes_controller.rb
@@ -3,21 +3,26 @@ class CostumesController < ApplicationController
   def index
     @costumes = Costume.all
     render 'index'
-    
+
   end
 
   def show
-    @costumes = Costume.all
-
+    @costume = Costume.find(params[:id])
+    render 'show'
   end
-
 
 
   def new
-
+    @costume = Costume.new
+    render 'new'
   end
 
 
+  def create
+
+  end
+
+  
 
   def update
 
