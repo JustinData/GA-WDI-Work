13a3862bdf4a990d3063bfee1671a66efddbb23b
teabait <hello.teaho@gmail.com>
2013-10-30 09:36:44 -0400

    User can create new costume

--- w06/d03/Tea/costumely/app/controllers/costumes_controller.rb
+++ w06/d03/Tea/costumely/app/controllers/costumes_controller.rb
@@ -5,5 +5,18 @@ class CostumesController < ApplicationController
     render :index
   end
 
+  def new
+    @costume = Costume.new
+    render :new
+  end
+
+  def create
+    whitelisted = params.require(:costume).permit([:name, :image])
+    costume = Costume.new(whitelisted)
+
+    costume.save
+
+    redirect_to "/"
+  end
 
 end
