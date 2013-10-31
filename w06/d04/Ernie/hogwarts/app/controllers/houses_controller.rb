94ae7d021a8e34b6706afea050522e5d64216fc2
Ernie Casilla <ecasilla@icloud.com>
2013-10-31 09:46:00 -0400

    seeds fix

--- w06/d04/Ernie/hogwarts/app/controllers/houses_controller.rb
+++ w06/d04/Ernie/hogwarts/app/controllers/houses_controller.rb
@@ -1,11 +1,11 @@
 class HousesController < ApplicationController
 
   def index
-    @houses = Houses.all
+    @houses = House.all
   end
 
   def show
-    @houses = Houses.all
+    @houses = House.find_by(params[:id])
   end
 
 end
