a2b94baf5752d9f06fcf77680af30806d6a3b0cf
Maria Mercedes Martinez <pistachiopony@gmail.com>
2013-10-31 15:59:53 -0400

    continue ghoulify

--- w06/d04/Maria/ghoulify/ghoul.js
+++ w06/d04/Maria/ghoulify/ghoul.js
@@ -1,17 +1,43 @@
-window.onload = function(){
-  console.log("ooooooh!");
-};
-
-
-function GhoulContainer(){
+function GhoulContainer() {
   this.containmentUnit = [];
 }
 
-GhoulContainer.prototype.push = function(ghoulname) {
-  this.containmentUnit.push(ghoulname);
-};
-var ghouls = new GhoulContainer();
+GhoulContainer.prototype.push = function(ghoulName) {
+  // OOOOHHHHHH!
+  this.containmentUnit.push(ghoulName);
+} 
 
-ghoulsOne.push("Jeff");
+function appendLi(newContent, ul) {
+  var li = document.createElement("li");
+  li.innerHTML = newContent;
+  ul.appendChild(li);
+}
 
-// ghouls.collection
+function appendGhouls(localGhouls, ul) {
+  ul.innerHTML = "";
+  WTK.each(localGhouls.containmentUnit, function(ghoul) {
+    appendLi(ghoul, ul);
+  });
+}
+
+window.ghouls = new GhoulContainer();
+
+ghouls.push("Jeff");
+ghouls.push("DrRobotMck");
+
+function ourSweetLoadFunction() {
+  console.log("Loaded");
+  var ul = document.querySelector("ul.containment-unit");
+  var form = document.querySelector("form.add-ghouls");
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    var input = this.elements["ghoulName"];
+    window.ghouls.push(input.value);
+    window.appendGhouls(window.ghouls, ul);
+    input.value = "";
+  });
+
+  window.appendGhouls(window.ghouls, ul);
+}
+
+window.onload = ourSweetLoadFunction;
