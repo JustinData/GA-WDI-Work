e4c930d404acd928b77f3f1ecbbedec2f88268ac
My Name <myemail@example.com>
2013-10-30 23:50:06 -0400

    homework

--- w06/d03/Paris/atm/atm-starter/atm.js
+++ w06/d03/Paris/atm/atm-starter/atm.js
@@ -1,39 +1,49 @@
+
 window.onload = console.log( "Loaded!" );
 
+// checkingDebit = document.getElementById("checking_withdraw")
+// checkingBalance = document.getElementById("checking_balance")
+// checkingAmount = document.getElementById("checking_amount")
+// savingsDeposit = document.getElementById("savings_deposit")
+// savingsDebit = document.getElementById("savings_withdraw")
+// savingsBalance = document.getElementById("savings_balance")
+// savingsAmount = document.getElementById("savings_amount") 
 
-// var add = document.getElementById("checking_balance")
-// <div class="balance" id="checking_balance">$0</div>
+window.onload = function(){
 
+// Account object
 function Account(name){
   this.name = name;
   this.balance = 0;
 }
 
-this.balance = Account.prototype.checkingdeposit = function( val ){
+// instances of Account
+checking = new Account("checking");
+savings = new Account("savings");
+
+// Account functions
+Account.prototype.deposit = function( val ){
   return (this.balance += val);
 }
 
-this.balance = Account.prototype.checkingdebit = function( val ){
+Account.prototype.debit = function( val, otheraccount ){
   if ((this.balance -= val) >= 0) {
-    return (this.balance -= val);
-  } else {
-    return false;
-  }
+    return (this.balance -= val); 
+  } else if ((this.balance + otheraccount.balance) <  val) {
+    return (this.balance + otheraccount.balance -= 0);
+    } else { ((this.balance + otheraccount.balance -= 0) < 0);
+    return null;
+  };
 }
 
-this.balance = Account.prototype.savingsdeposit = function( val ){
-  return (this.balance += val);
-}
+// checking deposit button functionality
+checkingDeposit = document.getElementById("checking_deposit");
+checking.deposit = document.getElementById("checking_amount").value;
+currentBalanceString = checking_balance.innerHTML;
+currentBalance = parseInt(currentBalanceString[1]);
+newBalance = checking.deposit + currentBalance;
+checkingDeposit.onclick = newBalance;
 
-this.balance = Account.prototype.savingsdebit = function( val ){
-  if ((this.balance -= val) >= 0) {
-    return (this.balance -= val);
-  } else {
-    return false;
-  }
-}
 
-var checking = new Account("checking");
-var savings = new Account("savings");
 
 
