8a0628563f4d8821bfae936e660eecd6393ad3b3
teabait <hello.teaho@gmail.com>
2013-10-31 07:02:39 -0400

    commented out old stuff

--- w06/d03/Tea/atm/atm.js
+++ w06/d03/Tea/atm/atm.js
@@ -1,33 +1,33 @@
 
-var dollar_amount;
-var checkingBalance = parseInt(document.getElementById('checking_balance').innerHTML.slice(1));
-var savingsBalance = parseInt(document.getElementById('savings_balance').innerHTML.slice(1));
+// var dollar_amount;
+// var checkingBalance = parseInt(document.getElementById('checking_balance').innerHTML.slice(1));
+// var savingsBalance = parseInt(document.getElementById('savings_balance').innerHTML.slice(1));
 
-function deposit(dollar_amount, account){
-  dollars = parseInt(dollar_amount);
-  checkingBalance = ("$" + (dollars + checkingBalance));
-  return checkingBalance;
-}
+// function deposit(dollar_amount, account){
+//   dollars = parseInt(dollar_amount);
+//   checkingBalance = ("$" + (dollars + checkingBalance));
+//   return checkingBalance;
+// }
 
 
-function withdraw(dollar_amount, account){
-  dollars = parseInt(dollar_amount);
-  newAmount = (checkingBalance - dollars);
-  if (newAmount < 0 ){
-    newAmount = (checkingBalance + savingsBalance - dollars);
-    if (newAmount < 0) {
-      return ("$" + checkingBalance);
-    } else {
-      checkingBalance = 0;
-      savingsBalance = newAmount;
-      return ("$" + checkingBalance);
-    }
-  }
-  else {
-    checkingBalance = newAmount;
-    return ("$" + checkingBalance);
-  }
-}
+// function withdraw(dollar_amount, account){
+//   dollars = parseInt(dollar_amount);
+//   newAmount = (checkingBalance - dollars);
+//   if (newAmount < 0 ){
+//     newAmount = (checkingBalance + savingsBalance - dollars);
+//     if (newAmount < 0) {
+//       return ("$" + checkingBalance);
+//     } else {
+//       checkingBalance = 0;
+//       savingsBalance = newAmount;
+//       return ("$" + checkingBalance);
+//     }
+//   }
+//   else {
+//     checkingBalance = newAmount;
+//     return ("$" + checkingBalance);
+//   }
+// }
 
 
 // make bank object like shelter :: has accounts
