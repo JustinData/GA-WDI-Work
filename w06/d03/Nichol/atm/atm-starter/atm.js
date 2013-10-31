5f7de64c7ac4262db82b0fad6a6bc9f580b7092b
Nichol Alexander <nichol.alexander@gmail.com>
2013-10-31 15:09:49 -0400

    updated atm to be even more beautiful!  and also ghoulify working

--- w06/d03/Nichol/atm/atm-starter/atm.js
+++ w06/d03/Nichol/atm/atm-starter/atm.js
@@ -1,3 +1,67 @@
+/********** Define Account Object *************/
+function Account (type, balance){
+  this.type = type;
+  this.balance = balance;
+}
+
+// Add Prototype to Deposit to Account
+// expect that it returns the new balance if the deposit was successful.
+Account.prototype.deposit = function(amount){
+  this.balance += amount;
+  return this.balance;
+}
+
+// Add Prototype to Debit from Account
+// if able to debit from given account, execute, otherwise, 
+// check total balance, and withdraw appropriately.
+Account.prototype.debit = function(amount, otherAccount){
+  if((this.balance - amount) >= 0){
+    this.balance = this.balance - amount;  
+  } else {
+      total = this.balance + otherAccount.balance;
+      if((total - amount) >= 0) {
+        this.balance -= amount;
+        otherAccount.balance += this.balance;
+        this.balance = 0;
+      } else {
+        alert("YOU DONT HAVE ENOUGH MONEY");
+      }
+    }
+  return this.balance;
+}
+/**************************************************/
+
+//instantiate the two accounts
+myChecking = new Account("checking", 0);
+mySavings = new Account("savings", 0);
+
+
+//helper functions
+function getValue(field){
+  return parseInt(value = document.getElementById(field).value);
+}
+
+function updateDisplay(){
+  //update balance elements
+  balanceElement = document.getElementById('checking_balance');
+  balanceElement.innerHTML = myChecking.balance;
+
+  balanceElement = document.getElementById('savings_balance');
+  balanceElement.innerHTML = mySavings.balance;
+
+  //update classes
+  if(myChecking.balance > 0){
+      document.getElementById("checking_balance").className = "balance";
+      } else {
+        document.getElementById("checking_balance").className = "zero";
+      }
+  if(mySavings.balance > 0){
+      document.getElementById("savings_balance").className = "balance";
+      } else {
+        document.getElementById("savings_balance").className = "zero";
+      }
+}
+
 window.onload = function(){
   console.log("loaded!");
   
@@ -32,71 +96,5 @@ window.onload = function(){
     updateDisplay();
   }, false);
 }
-
-// Define Account Object
-function Account (type, balance){
-  this.type = type;
-  this.balance = balance;
-}
-
-// Add Prototype to Deposit to Account
-// expect that it returns the new balance if the deposit was successful.
-Account.prototype.deposit = function(amount){
-  this.balance += amount;
-  return this.balance;
-}
-
-// Add Prototype to Debit from Account
-// if able to debit from given account, execute, otherwise, 
-// check total balance, and withdraw appropriately.
-Account.prototype.debit = function(amount, otherAccount){
-  if((this.balance - amount) >= 0){
-    this.balance = this.balance - amount;  
-  } else {
-      total = this.balance + otherAccount.balance;
-      if((total - amount) >= 0) {
-        this.balance -= amount;
-        otherAccount.balance += this.balance;
-        this.balance = 0;
-      } else {
-        alert("YOU DONT HAVE ENOUGH MONEY");
-      }
-    }
-  return this.balance;
-}
-
-//instantiate the two accounts
-myChecking = new Account("checking", 0);
-mySavings = new Account("savings", 0);
-
-
-//helper functions
-function getValue(field){
-    return parseInt(value = document.getElementById(field).value);
-  }
-
-function updateDisplay(){
-  //update balance elements
-  balanceElement = document.getElementById('checking_balance');
-  balanceElement.innerHTML = myChecking.balance;
-
-  balanceElement = document.getElementById('savings_balance');
-  balanceElement.innerHTML = mySavings.balance;
-
-  //update classes
-  if(myChecking.balance > 0){
-      document.getElementById("checking_balance").className = "balance";
-      } else {
-        document.getElementById("checking_balance").className = "zero";
-      }
-  if(mySavings.balance > 0){
-      document.getElementById("savings_balance").className = "balance";
-      } else {
-        document.getElementById("savings_balance").className = "zero";
-      }
-}
-
-
-
 
 
