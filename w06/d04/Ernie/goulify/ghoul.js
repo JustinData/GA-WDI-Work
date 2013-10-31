5a40d315b57d3cf2b6d75becea5375c8547044ba
Ernie Casilla <ecasilla@icloud.com>
2013-10-31 13:45:52 -0400

    stuff

--- w06/d04/Ernie/goulify/ghoul.js
+++ w06/d04/Ernie/goulify/ghoul.js
@@ -6,9 +6,25 @@ function GhoulContainer () {
   this.containmentUnit = [];
 }
 
-GhoulList.prototype.push = function(ghoulName) {
-  this.containmentUnit.push(ghoulName)
-};
+GhoulContainer.prototype.push = function(ghoulName) {
+  this.containmentUnit.push(ghoulName);
+}
 
-var ghoulsOne = new GhoulContainer();
+var ghouls = new GhoulContainer();
 ghouls.push("Jeff");
+ghouls.push("Ernie");
+
+
+function nameChange (name) {
+ document.querySelector("ul.containment-unit li").innerHTML = name;
+}
+
+function appendLi (newContent) {
+  var newLI = document.createElement("li");
+  newLI.innerHTML = newContent;
+  var ul = document.querySelector("ul.containment-unit li")
+  ul.appendChild(newLI);
+}
+
+
+appendLi(ghouls);
