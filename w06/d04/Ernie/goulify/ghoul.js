c028d88484a696b76992d755ba80a743f918ab67
Ernie Casilla <ecasilla@icloud.com>
2013-10-31 11:17:59 -0400

    container

--- w06/d04/Ernie/goulify/ghoul.js
+++ w06/d04/Ernie/goulify/ghoul.js
@@ -1,3 +1,14 @@
 window.onload = function () {
-  
+  console.log("OOOHHHHHHH");
 }
+
+function GhoulContainer () {
+  this.containmentUnit = [];
+}
+
+GhoulList.prototype.push = function(ghoulName) {
+  this.containmentUnit.push(ghoulName)
+};
+
+var ghoulsOne = new GhoulContainer();
+ghouls.push("Jeff");
