4463219f53673116f4dd0cb41c2c7f50e8a1d0ec
Nichol Alexander <nichol.alexander@gmail.com>
2013-10-31 08:48:19 -0400

    ready to push

--- w06/d03/Nichol/atm/atm-starter/deob.js
+++ w06/d03/Nichol/atm/atm-starter/deob.js
@@ -1,4 +1,9 @@
-var _0x66f4 = ["balance", "name", "checking", "savings", "makeDeposit", "prototype", "value", "amount", "updateDisplay", "zero", "add", "classList", "balanceDisplay", "remove", "innerHTML", "$", "", "initialize", "_balance", "getElementById", "depositButton", "_deposit", "withdrawButton", "_withdraw", "_amount", "click", "addEventListener", "onload"];
+var _0x66f4 = ["balance", "name", "checking", "savings", "makeDeposit", "prototype", 
+                "value", "amount", "updateDisplay", "zero", "add", 
+                "classList", "balanceDisplay", "remove", "innerHTML", "$", 
+                "", "initialize", "_balance", "getElementById", "depositButton", 
+                "_deposit", "withdrawButton", "_withdraw", "_amount", "click", 
+                "addEventListener", "onload"];
 
 var Account = function (name, balance) {
     this.balance = balance;
@@ -6,61 +11,59 @@ var Account = function (name, balance) {
 };
 
 var checking_atm = new Account(checking, 0);
-var savings_atm = new Account(saavings, 0);
+var savings_atm = new Account(savings, 0);
 
 Account.prototype.makeDeposit = function () {
-    var _0x381dx6 = parseInt(this.amount.value);
-    this.balance += _0x381dx6;
+    var value = parseInt(this.amount.value);
+    this.balance += value;
     this.updateDisplay();
 };
 
-/////////////////////////////////////////////
-/////// http://jsbeautifier.org/  ///////////
-/////// STOPPING HERE             //////////
-////////////////////////////////////////////
-
-Account[_0x66f4[5]][_0x66f4[8]] = function () {
-    if (this[_0x66f4[0]] <= 0) {
-        this[_0x66f4[12]][_0x66f4[11]][_0x66f4[10]](_0x66f4[9]);
+Account.prototype.updateDisplay = function() {
+    if (this.balance <= 0) {
+        this.balanceDisplay.classList.add.zero;
     } else {
-        this[_0x66f4[12]][_0x66f4[11]][_0x66f4[13]](_0x66f4[9]);
+        this.balanceDisplay.classList.remove.zero;
     };
-    this[_0x66f4[12]][_0x66f4[14]] = _0x66f4[15] + this[_0x66f4[0]];
-    this[_0x66f4[7]][_0x66f4[6]] = _0x66f4[16];
+    this.balanceDisplay.innerHTML = "$" + this.balance;
+    this.amount.value = ""; //??
 };
-Account[_0x66f4[5]][_0x66f4[17]] = function () {
-    this[_0x66f4[12]] = document[_0x66f4[19]](this[_0x66f4[1]] + _0x66f4[18]);
-    this[_0x66f4[20]] = document[_0x66f4[19]](this[_0x66f4[1]] + _0x66f4[21]);
-    this[_0x66f4[22]] = document[_0x66f4[19]](this[_0x66f4[1]] + _0x66f4[23]);
-    this[_0x66f4[7]] = document[_0x66f4[19]](this[_0x66f4[1]] + _0x66f4[24]);
+
+Account.prototype.initialize = function () {
+    //fancy!
+    this.balanceDisplay = document.getElementById(this.name + "_balance");
+    this.depositButton = document.getElementById(this.name + "_deposit");
+    this.withdrawButton = document.getElementById(this.name + "_withdraw");
+    this.amount = document.getElementById(this.name + "_amount");
     var _0x381dx7 = this;
-    this[_0x66f4[20]][_0x66f4[26]](_0x66f4[25], function (_0x381dx8) {
-        _0x381dx7[_0x66f4[4]]();
+    this.depositButton.addEventListener("click", function (_0x381dx8) {
+        _0x381dx7.makeDeposit();
     });
 };
-window[_0x66f4[27]] = function () {
-    checking_atm[_0x66f4[17]]();
-    savings_atm[_0x66f4[17]]();
-    checking_atm[_0x66f4[22]][_0x66f4[26]](_0x66f4[25], function (_0x381dx8) {
+
+window.onload = function () {
+    checking_atm.initialize();
+    savings_atm.initialize();
+    checking_atm.withdrawButton.addEventListener]("click", function (_0x381dx8) {
         makeWithdrawal(checking_atm, savings_atm);
     });
-    savings_atm[_0x66f4[22]][_0x66f4[26]](_0x66f4[25], function (_0x381dx8) {
+    savings_atm.withdrawButton.addEventListener]("click", function (_0x381dx8) {
         makeWithdrawal(savings_atm, checking_atm);
     });
-    checking_atm[_0x66f4[8]]();
-    savings_atm[_0x66f4[8]]();
+    checking_atm.updateDisplay();
+    savings_atm.updateDisplay();
 };
 
-function makeWithdrawal(_0x381dxa, _0x381dxb) {
-    var _0x381dx6 = parseInt(_0x381dxa[_0x66f4[7]][_0x66f4[6]]);
-    if (_0x381dx6 <= _0x381dxa[_0x66f4[0]]) {
-        _0x381dxa[_0x66f4[0]] -= _0x381dx6;
+function makeWithdrawal(checking_atm, savings_atm) {
+    var value = parseInt(checking.amount.value);
+    if (value <= checking_atm.balance) {
+        checking_atm.balance -= value;
     } else {
-        if ((_0x381dx6 > _0x381dxa[_0x66f4[0]]) && (_0x381dx6 <= (_0x381dxa[_0x66f4[0]] + _0x381dxb[_0x66f4[0]]))) {
-            _0x381dxb[_0x66f4[0]] = (_0x381dxa[_0x66f4[0]] + _0x381dxb[_0x66f4[0]]) - _0x381dx6;
-            _0x381dxa[_0x66f4[0]] = 0;
+        if ((value > checking_atm.balance) && (value <= (checking_atm.balance + savings_atm.balance))) {
+            savings_atm.balance = (checking_atm.balance + savings_atm.balance) - value;
+            checking_atm.balance = 0;
         };
     };
-    _0x381dxa[_0x66f4[8]]();
-    _0x381dxb[_0x66f4[8]]();
+    checking_atm.updateDisplay();
+    savings_atm.updateDisplay();
 };
