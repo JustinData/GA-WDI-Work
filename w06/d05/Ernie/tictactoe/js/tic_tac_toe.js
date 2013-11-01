dd55e5b9e943ed9074b34a47ccdc899f2e2b3942
Ernie Casilla <ecasilla@icloud.com>
2013-11-01 17:30:55 -0400

    tic tac toe

--- w06/d05/Ernie/tictactoe/js/tic_tac_toe.js
+++ w06/d05/Ernie/tictactoe/js/tic_tac_toe.js
@@ -1,49 +1,53 @@
-
-function checkRows(array) 
-    {
-    // array contains a 3x3 matrix representing the Tic Tac Toe board, indexed
-    // from 0 to 8 going from left to right, top to bottom.
-    // Check if a row is equal (i.e. 0 = 1 = 2, 3 = 4 = 5, or 6 = 7 = 8), which
-    // represents a player checking off all boxes in a single row.
-    // Return 0 if no rows are filled. Otherwise return the value of the cells
-    // where a row matches.
-    // Example: if Array = [0, 0, 0, 1, 1, 2, 2, 2, 2], 2,2,2 are equal, so return 2.
-    // Replace this code with your method
-    
-    
+function Board () {
+  this.game = [[0,0,0],
+              [0,0,0],
+              [0,0,0]];
 }
 
-function checkColumns(array) {
-    // array contains a 3x3 matrix representing the Tic Tac Toe board, indexed
-    // from 0 to 8 going from left to right, top to bottom.
-    // Check if a column is equal (i.e. 0 = 3 = 6, 1 = 4 = 7, or 2 = 5 = 8), which
-    // represents a player checking off all boxes in a column.
-    // Return 0 if no columns are filled. Otherwise return the value of the cells
-    // where a column matches.
-    // Example: if Array = [0, 2, 1, 0, 2, 1, 2, 0, 1], cells 2=4=7, so return their value: 1.
-    // Note: here we're using 0 = unused cell, 1 = X, 2 = O
-    // Replace this code with your method
-    
 
-}
+Board.prototype.makePlay = function(row,column,player) {
+  
+};
 
-function checkDiagonals(array){
-    // array contains a 3x3 matrix representing the Tic Tac Toe board, indexed
-    // from 0 to 8 going from left to right, top to bottom.
-    // Check if a diagonal is equal (i.e. 0 = 4 = 8 or 2 = 4 = 6), which
-    // represents a player checking off all boxes in a diagonal.
-    // Return 0 if no diagonals are filled. Otherwise return the value of the cells
-    // where a diagonal matches.
-    // Example: if Array = [2, 1, 0, 1, 2, 0, 1, 0, 2], cells 0=4=8, so return their value: 2.
-    // Note: here we're using 0 = unused cell, 1 = X, 2 = O
-    // Replace this code with your method
-    
-}
+Board.prototype.checkWin = function() {
+  
+  if (
+       (1 == this.game[0][0]) && (1 == this.game[1][0]) && (1 == this.game[2][0]) ||
+       (1 == this.game[0][1]) && (1 == this.game[1][1]) && (1 == this.game[2][1]) ||
+       (1 == this.game[0][2]) && (1 == this.game[1][2]) && (1 == this.game[2][2])
+      ) //vertical wins player 1
+  {
+    console.log("1 WINS VERTICAL");
+  } else if (
+       (2 == this.game[0][0]) && (2 == this.game[1][0]) && (2 == this.game[2][0]) ||
+       (2 == this.game[0][1]) && (2 == this.game[1][1]) && (2 == this.game[2][1]) ||
+       (2 == this.game[0][2]) && (2 == this.game[1][2]) && (2 == this.game[2][2])
+      ) //vertical wins by player 2
+  {
+    console.log("2 WINS VERTICAL!");
+  
+  } else if ( 
+            (1 == this.game[0][0]) && (1 == this.game[0][1]) && (1 == this.game[0][2])||
+            (1 == this.game[1][0]) && (1 == this.game[1][1]) && (1 == this.game[1][2]) ||
+           (1 == this.game[2][0]) && (1 == this.game[2][1]) && (1 == this.game[2][2])
+       )
+  {
 
-function getFirstEmptyMove(array) {
-    /* Get first value in the array that is 0 (meaning: unused)
-     * Array goes from 0-8
-     */
-   //REPLACE THIS CODE WITH YOUR METHOD
+   }else if (
+          (2 == this.game[0][0]) && (2 == this.game[0][1]) && (2 == this.game[0][2])||
+          (2 == this.game[1][0]) && (2 == this.game[1][1]) && (2 == this.game[1][2]) ||
+         (2 == this.game[2][0]) && (2 == this.game[2][1]) && (2 == this.game[2][2]) 
+      )
+  {
+  }
     
-}
+            
+ };
+
+var board;
+var player1 = 1
+var player2 = 2
+
+window.onload = function(){
+  board = new Board();
+};
