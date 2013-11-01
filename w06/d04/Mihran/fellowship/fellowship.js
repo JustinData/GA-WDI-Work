12f711c22e90825dc6e9730e73b698c4c45e509d
Mihran Abrahamian <abrahamian.mihran@gmail.com>
2013-11-01 12:16:21 -0400

    experimenting with find element by id

--- w06/d04/Mihran/fellowship/fellowship.js
+++ w06/d04/Mihran/fellowship/fellowship.js
@@ -65,20 +65,20 @@ keepItSecretKeepItSafe();
 function makeBuddies(buddies) {
   aside = document.createElement("aside");
   aside.innerHTML = "<ul> </ul>" ;
-  document.body.insertBefore(aside, //Rivendell//)
+  document.body.insertBefore(aside, //oldNode//)
 
   WTK.each (buddies, function(buddy){
-    li = document.createElement("li")
-    li.innerHTML = buddy
+    li = document.createElement("li");
+    li.innerHTML = buddy;
     aside = document.querySelector(""); //where is it?//
     aside.append(li);
   })
 }
 
-makeBuddies();
+// makeBuddies();
 
-function beautifulStranger() {
-}
+// function beautifulStranger() {
+// }
 
-function forgeTheFellowShip() {
-}
+// function forgeTheFellowShip() {
+// }
