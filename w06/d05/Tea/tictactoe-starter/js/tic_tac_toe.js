a6c8355e53aa8194e514511cbae8e7727f3cecc7
teabait <hello.teaho@gmail.com>
2013-11-02 16:38:05 -0400

    mostly functional, but not OO. Using this to jump to OO

--- w06/d05/Tea/tictactoe-starter/js/tic_tac_toe.js
+++ w06/d05/Tea/tictactoe-starter/js/tic_tac_toe.js
@@ -1,43 +1,81 @@
-//  Fundamental Objects within Tic Tac Toe
 
-function TicTacToe(){
-  this.players = 
-  this.board  
-  rules
-  clicks 
+window.onload = function(){
+  alert("HELLO");
+  game();
+  boardToDom();
+};
+
+var clicks = 0;
+var gameBoard = new Array(9);
+var domBoard = [];
+var cells = document.getElementsByTagName("td");
+
+function eventListener(clicks, item){
+  returnPlayer(clicks, item);
+  markCell(item, player);
+  markGameBoard(item)
 }
 
-function Board(){
-  var board = [];
- 
-  var tiles = document.querySelectorAll("td");
- 
-  _.each(tiles, function(tile){
-    board.push(tile.getAttribute("id"));
-  });
- 
-  _.each(tiles, function(tile){
-    attachListener(tile);
-  });
- 
-  var twodboard = [[board[0], board[1], board[2]], [board[3],board[4],board[5]], [board[6], board[7],board[8]]];
- 
-  return twodboard;
+function returnPlayer (clicks, item) {
+    if(clicks % 2 === 0){
+      player = "red";
+    } else {
+      player = "black";
+    };
+};
+
+var game = function(){
+  var board = document.querySelector("tbody");
+  board.onclick = function(event) {
+    clicks++;
+  };
+
+};
+
+function markCell(item, player){
+  if(item.className === "box_cell"){
+    item.className = "box_cell " + player;
+  } else {
+    alert( "This space has already been played!");
+    clicks--;
+  }
 }
 
-function Cell(){
-  vacant?
-}
-
-function Player(){
+function markGameBoard(item){
+  gameBoard[domBoard.indexOf(item.id)] = player;
 
 }
 
-//  Behaviors of different elements in Tic Tac Toe
+
+// 0 1 2 
+// 3 4 5
+// 6 7 8
 
 
-//  Bring them together
+var winningSolutions = [
+// Horizontal Wins
+(gameBoard[0] == gameBoard[1] && gameBoard[0] == gameBoard[2]),
+(gameBoard[3] == gameBoard[4] && gameBoard[3] == gameBoard[5]),
+(gameBoard[6] == gameBoard[7] && gameBoard[6] == gameBoard[8]),
 
-function newGame(){
+// Vertical Wins
+(gameBoard[0] == gameBoard[3] && gameBoard[0] == gameBoard[6]),
+(gameBoard[1] == gameBoard[4] && gameBoard[1] == gameBoard[7]),
+(gameBoard[2] == gameBoard[5] && gameBoard[2] == gameBoard[8]),
 
+// Diagonal Wins
+(gameBoard[0] == gameBoard[4] && gameBoard[0] == gameBoard[8]),
+(gameBoard[2] == gameBoard[2] && gameBoard[2] == gameBoard[6])]
+
+// ["r0c0", "r0c1", "r0c2", "r1c0", "r1c1", "r1c2", "r2c0", "r2c1", "r2c2"]
+function boardToDom(){
+  for(i = 0; i < cells.length; i++){
+    domBoard[i] = cells[i].id;
+  }
+}
+
+function checkWinner(){
+  if (clicks > 9) {
+
+  }
 }
