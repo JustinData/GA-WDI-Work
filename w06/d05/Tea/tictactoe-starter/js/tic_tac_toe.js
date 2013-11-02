b414a4db7e84e989f9da37455b8a56d1bdd07559
teabait <hello.teaho@gmail.com>
2013-11-02 18:34:16 -0400

    Working Tic Tac Toe

--- w06/d05/Tea/tictactoe-starter/js/tic_tac_toe.js
+++ w06/d05/Tea/tictactoe-starter/js/tic_tac_toe.js
@@ -1,6 +1,5 @@
 var currentPlayer;
 var clicks = 0;
-// var gameBoard = new Array(9);
 var domBoard = [];
 var playerBoard = new Array(9);
 var cells = document.getElementsByTagName("td");
@@ -17,33 +16,10 @@ function TicTacToe(){
 function Player(playerColor){
   // board
   this.color = playerColor,
-  this.board = playerBoard
+  this.board = new Array(9)
 }
 
-function eventListener(spot){
-  clicks++;
-  setCurrentPlayer();
-  currentPlayer.markCell(spot);
-  currentPlayer.markOwnBoard(spot);
-}
-
-function setCurrentPlayer() {
-    if(clicks % 2 === 0){
-      currentPlayer = player1;
-    } else {
-      currentPlayer = player2;
-    }
-}
-
-// // TicTacToe.prototype.returnWinner =   function(clicks, item) {
-// //     if(clicks % 2 === 0){
-// //       currentPlayer = "red";
-// //     } else {
-// //       currentPlayer = "black";
-// //     };
-// // };
-// // }
-
+// Object functions 
 Player.prototype.markCell = function(item){
   if(item.className === "box_cell"){
     item.className = "box_cell " + currentPlayer.color;
@@ -55,45 +31,59 @@ Player.prototype.markCell = function(item){
 
 Player.prototype.markOwnBoard = function (item){
   this.board[domBoard.indexOf(item.id)] = 1;
+}
 
+Player.prototype.checkBoard = function(){
+var winningSolutions = [
+// // Horizontal Wins
+(this.board[0] + this.board[1] + this.board[2]),
+(this.board[3] + this.board[4] + this.board[5]),
+(this.board[6] + this.board[7] + this.board[8]),
+
+// Vertical Wins
+(this.board[0] + this.board[3] + this.board[6]),
+(this.board[1] + this.board[4] + this.board[7]),
+(this.board[2] + this.board[5] + this.board[8]),
+
+// Diagonal Wins
+(this.board[0] + this.board[4] + this.board[8]),
+(this.board[2] + this.board[2] + this.board[6])];
+
+for(i = 0; i < winningSolutions.length; i++){
+  if(winningSolutions[i]===3){
+    alert(this.color + " wins");
+  } else if (clicks === 9) {
+    alert("It's a tie!");
+    clicks ++
+  }
+}
 }
 
 
-// // 0 1 2 
-// // 3 4 5
-// // 6 7 8
 
-
-// var winningSolutions = [
-// // Horizontal Wins
-// (gameBoard[0] == gameBoard[1] && gameBoard[0] == gameBoard[2]),
-// (gameBoard[3] == gameBoard[4] && gameBoard[3] == gameBoard[5]),
-// (gameBoard[6] == gameBoard[7] && gameBoard[6] == gameBoard[8]),
-
-// // Vertical Wins
-// (gameBoard[0] == gameBoard[3] && gameBoard[0] == gameBoard[6]),
-// (gameBoard[1] == gameBoard[4] && gameBoard[1] == gameBoard[7]),
-// (gameBoard[2] == gameBoard[5] && gameBoard[2] == gameBoard[8]),
-
-// // Diagonal Wins
-// (gameBoard[0] == gameBoard[4] && gameBoard[0] == gameBoard[8]),
-// (gameBoard[2] == gameBoard[2] && gameBoard[2] == gameBoard[6])]
-
-// ["r0c0", "r0c1", "r0c2", "r1c0", "r1c1", "r1c2", "r2c0", "r2c1", "r2c2"]
 function boardToDom(){
   for(i = 0; i < cells.length; i++){
     domBoard[i] = cells[i].id;
   }
 }
 
-// function checkWinner(){
-//   if (clicks > 9) {
+function eachMove(spot){
+  clicks++;
+  setCurrentPlayer();
+  currentPlayer.markCell(spot);
+  currentPlayer.markOwnBoard(spot);
+  currentPlayer.checkBoard();
+}
 
-//   }
-// }
+function setCurrentPlayer() {
+    if(clicks % 2 === 0){
+      currentPlayer = player1;
+    } else {
+      currentPlayer = player2;
+    }
+}
 
 window.onload = function(){
-  alert("HELLO");
   var newGame = new TicTacToe;
   newGame;
 };
