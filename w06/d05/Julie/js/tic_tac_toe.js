fea5f835feb29b023072cb8d6dd461ad5dc25dd2
Julie lappano <julie.lappano@gmail.com>
2013-11-03 16:52:37 -0500

    can play in console

--- w06/d05/Julie/js/tic_tac_toe.js
+++ w06/d05/Julie/js/tic_tac_toe.js
@@ -1,39 +1,131 @@
 
 board = [[0,0],[0,1],[0,2],[1,0],[1,1],[1,2],[2,0],[2,1],[2,2]];
 
+rows = [[board[0],board[1],board[2]], [board[3], board[4], board[5]], [board[6], board[7], board[8]]];
+columns = [[board[0],board[3],board[6]], [board[1], board[4], board[7]], [board[2], board[5], board[8]]];
+diagonals = [[board[0],board[4],board[8]], [board[2],board[4],board[6]]];
+
 // 0 1 2
 // 3 4 5
 // 6 7 8
 
+turn = "X"
+isGameWon = false;
+turnCount = 0
 
-function makeMove(index, player){
+function checkRowforWin(row){
+  if(_.reduce(row, function(b, entry){ return (b && entry.indexOf(turn) > -1)}, true)){
+
+    alert( turn + "  has won");
+    isGameWon = true;
+  }
+  else return null;
+}
+
+
+function checkIfTied(){
+  if( turnCount == 9 && isGameWon == false){
+    alert("It's a tie");
+  }
+  else{
+    return null;
+  }
+}
+// function checkIfTied(){
+//   var full = true;
+//   for (var i = board.length - 1; i >= 0; i--) {
+//     full = board[i].length === 3 && full;
+//   };
+//   isGameTied = true;
+//   alert("It's a tie");
+// }
+
+
+function checkIfWon(){
+
+for (var i = rows.length - 1; i >= 0; i--) {
+  checkRowforWin(rows[i]);
+};
+
+for (var j = columns.length - 1; j >= 0; j--) {
+  checkRowforWin(columns[j]);
+};
+
+for (var g = diagonals.length - 1; g >= 0; g--) {
+  checkRowforWin(diagonals[g]);
+};
+}
+
+
+function makeMove(index){
+
+   if(isGameWon){
+    return false;
+   }
+   else{
     var subArray = board[index];
     if (subArray.length == 2) {
-    board[index].push(player);
+    board[index].push(turn);
     console.log(board); 
   }
     else {
       return false;
      }
+    if (turn === "X"){
+      turn = "O";
+    }
+    else {
+      turn = "X";
+    }   
+  }
+  turnCount ++;
+  checkIfTied();
 }
 
-function checkIfWon(){
- //  row = 0
- // for(col=0; col<ticTacToeBoard.length; col++){
- //  row = 0;
- //  if(ticTacToeBoard[row][col]==ticTacToeBoard[row+1][col]==ticTacToeBoard[row+2][col]){
- //    win = true;
- //  }
- //  row++;
-  
-}
-
+// function checkRow(row){
+//   if (row[0].indexOf("X") > -1 == true && row[1].indexOf("X") > -1 == true && row[2].indexOf("X") > -1 == true){
+//     alert("Player 1 has won");
+//   }
+//   else if (row[0].indexOf("O") > -1 == true && row[1].indexOf("O") > -1 == true && row[2].indexOf("O") > -1 == true){
+//     alert("Player 2 has won");
+//   }
+//   else {
+//     return null;
+//   }
+// }
 
 
 
 
 window.onload = function() {
-  makeMove(2, "X");
+  var elr0c0 = document.getElementById("r0c0");
+  var elr0c1 = document.getElementById("r0c1");
+  var elr0c2 = document.getElementById("r0c2");
+  
+  var elr1c0 = document.getElementById("r1c0");
+  var elr1c1 = document.getElementById("r1c1");
+  var elr1c2 = document.getElementById("r1c2");
+
+  var elr2c0 = document.getElementById("r2c0");
+  var elr2c1 = document.getElementById("r2c1");
+  var elr2c2 = document.getElementById("r2c2");
+
+  
+  elr0c0.addEventListener("click", function(){
+   makePlay(0,0, currentPlayer);
+
+   if(currentPlayer == "x"){
+    elr0c0.className += " red"; 
+   } else {
+    elr0c0.className += " black";
+   } 
+
+   switchPlayer(currentPlayer);
+
+   checkForWin();
+
+  }, false);
+
 }
 
 
