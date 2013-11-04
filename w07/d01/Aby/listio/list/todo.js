ac4b97ea2b7acefa89c5c1371b3bd815115958f7
Aby Bandoh <aby.bandoh@gmail.com>
2013-11-04 11:27:02 -0500

    first user story

---
+++ w07/d01/Aby/listio/list/todo.js
@@ -0,0 +1,21 @@
+function addToDo(user_item){
+   this.item = user_item;
+   var ordered_list = document.getElementById("todo-items"); // <ol>
+
+   var new_item = document.createElement("li"); // <li>
+   new_item.innerHTML = item; // <li>innerHTML</li>
+
+   ordered_list.appendChild(new_item);
+
+}
+
+
+window.onload = function() {
+   var input = document.getElementById("add-item"); // <button>
+   input.addEventListener('click', function() {
+      var item = document.getElementById("new-task-field").value; // <input>.value
+
+      addToDo(item);
+   });
+
+}
