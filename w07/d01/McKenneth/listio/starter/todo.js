e0ddc86ded0eb032e737ef8b6bc6aad8783bf078
DrRobotmck <mckenneth.scott@icloud.com>
2013-11-04 16:22:14 -0500

    transfer between lists works

--- w07/d01/McKenneth/listio/starter/todo.js
+++ w07/d01/McKenneth/listio/starter/todo.js
@@ -1,13 +1,28 @@
 window.onload = function(){
   list();
 }
-  var listItem, button, toDoList, content, removeItem, listed;
+  var listItem, button, toDoList, content, removeItem, listed, completeButton, completeList;
   var count = 0;
+
   
   var removeThis = function(){
+    if (removeItem.className === "comp_delete"){
+
+    }else{
     var item = toDoList.childNodes;
-    var spot = document.getElementById(this.id).value;
-    toDoList.removeChild(item[spot]);
+    var spot = document.getElementById(this.id);
+    toDoList.removeChild(spot);
+    }
+  };
+  
+  var completeTask = function(){
+    
+    var item = toDoList.childNodes;
+    var spot = document.getElementById(this.id);
+    var b = spot.childNodes[0].childNodes[0];
+    spot.childNodes[0].removeChild(b);
+    completeList.appendChild(spot);
+    
   };
   
   var list = function(){
@@ -15,6 +30,7 @@ window.onload = function(){
    toDoList = document.getElementById("todo-items");
    // gets submit button
    button = document.getElementById("add-item");
+   completeList = document.getElementById("completed-items");
    
   
    // click event listener for button
@@ -22,16 +38,26 @@ window.onload = function(){
     listItem = document.createElement("li");
     listItem.id = count;
     listed = document.createElement("div");
+    listed.className = "listed";
+//  delete task button 
     removeItem = document.createElement("button");
+    removeItem.className = "delete";
     removeItem.id = count;
-    
     removeItem.innerHTML = "REMOVE"
     removeItem.addEventListener("click", removeThis);
+// complete task button
+    completeButton = document.createElement("button");
+    completeButton.className = "complete";
+    completeButton.innerHTML = "COMPLETED";
+    completeButton.addEventListener("click", completeTask);
+    completeButton.id = count;
 
-     content = document.getElementById("new-task-field").value;
-    
+// content of list item
+    content = document.getElementById("new-task-field").value;
+    listed.id = count;
     listed.innerHTML = content;
     listed.appendChild(removeItem);
+    listed.appendChild(completeButton);
     
     
 
