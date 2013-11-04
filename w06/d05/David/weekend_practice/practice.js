48cbf97c9efe60f834ff6b3c2f1f91330c963402
David Fox Powell <dtothefp@gmail.com>
2013-11-04 09:03:56 -0500

    Morning commit before exercises

--- w06/d05/David/weekend_practice/practice.js
+++ w06/d05/David/weekend_practice/practice.js
@@ -1,8 +1,29 @@
-document.onload = function(){
+window.onload = function(){
   console.log("LOADED!!");
 }
 
-var hello = function(name) { return "hello: " + name; };
-var moeCreator = _.wrap(hello, function(func) {
-  return "before, " + func("moe") + ", after";
-});
+function Monkey(name) {
+  this.name = name;
+  this.onTree = false;
+}
+
+Monkey.prototype.speak = function() {
+  return "Hello, I'm " + this.name;
+}
+
+Monkey.prototype.climb = function() {
+  return this.onTree = true;
+}
+
+var bim = new Monkey("bim");
+var bop = new Monkey("bop");
+
+function Jungle(arr) {
+  this.animals = arr;
+}
+
+var amazon = new Jungle([bim, bop]);
+
+Jungle.prototype.listAnimals = function(){
+  
+}
