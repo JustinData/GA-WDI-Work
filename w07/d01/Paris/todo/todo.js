803e7ad803cf1d50dd1a332e2e6be24dbf1d4f9b
My Name <myemail@example.com>
2013-11-04 14:59:20 -0500

    working  code on delete and move

--- w07/d01/Paris/todo/todo.js
+++ w07/d01/Paris/todo/todo.js
@@ -1,42 +1,74 @@
-function listContainer() {
+function ListContainer() {
   this.todoList = [];
 }
 
-listContainer.prototype.push = function (listItem){
+ListContainer.prototype.push = function (listItem){
   this.todoList.push(listItem);
 }
 
-function appendTodo(newContent, ul) {
-
+function appendTodo(newContent, ol) {
   var li = document.createElement("li");
   li.innerHTML = newContent;
-  ul.appendChild(li);
-}
+  ol.appendChild(li);
 
-function appendUl(listItem, ul) {
-  ul.innerHTML = "";
-  WTK.each(listItem.todoList, function(listItem) {
-    appendTodo(listItem, ul);
+  // button to delete individual li
+  var buttonDelete = document.createElement("button");
+  buttonDelete.innerHTML = "Delete";
+  buttonDelete.className = "button-delete";
+  li.appendChild(buttonDelete);
+  buttonDelete.addEventListener("click", function() {
+  ol.removeChild(li);
+
+  });
+
+    // move to complete list
+  var ul = document.querySelector("ul");
+  var buttonMove = document.createElement("button");
+  buttonMove.innerHTML = "Move to complete";
+  buttonMove.className = "button-move";
+  li.appendChild(buttonMove);
+  buttonMove.addEventListener("click", function() {
+  // ol.removeChild(li);
+  ul.appendChild(li);
+  // console.log(newList);
+  // console.log(li);
+  var string = li.textContent;
+  var index = string.replace('DeleteMove to complete','');
+  console.log(index);
+  if (index > -1) {
+   var x = newList.todoList.splice(index, 1);
+   console.log(x);
+  }
+
   });
 }
 
-window.newList = new listContainer();
-  
+
+
+function appendOl(listItem, ol) {
+  ol.innerHTML = "";
+  WTK.each(listItem.todoList, function(listItem) {
+    appendTodo(listItem, ol);
+  });
+} 
+
+window.newList = new ListContainer();
+
 
 function ourSweetLoadFunction() {
   console.log("Loaded");
-  var ul = document.querySelector("ul.todo-list");
+  var ol = document.querySelector("ol.todo-list");
   var form = document.querySelector("form.add-todo");
   form.addEventListener("submit", function(e) {
     e.preventDefault();
     var input = this.elements["listItem"];
     window.newList.push(input.value);
-    window.appendUl(window.newList, ul);
+    window.appendOl(window.newList, ol);
     input.value = "";
 
   });
 
-  window.appendUl(window.newList, ul);
+  // window.appendOl(window.newList, ol);
 
 }
 
