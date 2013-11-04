371c5e51fa4f04bfd827b48d101e3e1860ad6c07
Nichol Alexander <nichol.alexander@gmail.com>
2013-11-04 16:47:46 -0500

    index2.html - all in one page

--- w07/d01/Nichol/listio/starter/todo.js
+++ w07/d01/Nichol/listio/starter/todo.js
@@ -112,9 +112,11 @@ window.onload = function() {
 
   }
 
+  //add event listener for clicking on the button
   elAddItem = document.getElementById("add-item");
   elAddItem.addEventListener("click", addItemHandler);
   
+  //add event listener for hitting enter while still in the field
   elTextField = document.getElementById("new-task-field")
   elTextField.addEventListener("keypress", function(event){
     if (event.which == 13 || event.keyCode == 13) {
@@ -123,95 +125,5 @@ window.onload = function() {
   });
   
 
-  // elAddItem.addEventListener("click", function(){
-      
-  //     //create new item, put it in the list
-  //     var text = document.getElementById("new-task-field").value;
-  //     var item = new Item(text); 
-  //     myTodoList.push(item);
-    
-  //     //clear value     
-  //     document.getElementById("new-task-field").value = "";
-      
-  //     //create element for displaying the item
-  //     var li = document.createElement("li");
-  //     var div = document.createElement("div");
-  //     var todoItems = document.getElementById("todo-items");
-  //     div.className = "items";
-  //     div.innerHTML = text;
+}
 
-  //     //create the div for all the actions
-  //     var div2 = document.createElement("div");
-  //     var span = document.createElement("span");
-  //     var complete_button = document.createElement("button");
-  //     var delete_button = document.createElement("button");
-
-  //     //assign propepr class names
-  //     div2.className = "actions";
-  //     span.className = "meta-data";
-  //     span.innerHTML = item.date; //should be formatted!
-  //     complete_button.className = "complete";
-  //     complete_button.innerHTML = "completed";
-  //     delete_button.className = "delete";
-  //     delete_button.innerHTML = "delete me";
-
-  //     //add event listeners to each button, associated with the instance of the item.
-  //     complete_button.addEventListener("click", function(){
-  //       //do the model
-  //       item.complete();
-        
-  //       //remove from current OL
-  //       li = this.parentElement.parentElement.parentElement;
-  //       li.parentNode.removeChild(li);
-
-  //       //add to UL
-  //       var completedItems = document.getElementById("completed-items");
-  //       completedItems.appendChild(li);
-
-  //       //remove complete button
-  //       this.parentElement.children[1].parentNode.removeChild(this.parentElement.children[1]);
-
-  //     });
-
-  //     delete_button.addEventListener("click", function(){
-  //       //do the model
-  //       item.delete(myTodoList);
-
-  //       //update the display
-  //       li = this.parentElement.parentElement.parentElement;
-  //       li.parentNode.removeChild(li);
-
-  //     });
-
-  //     //append all the divs for actions
-  //     div2.appendChild(span);
-  //     div2.appendChild(complete_button);
-  //     div2.appendChild(delete_button); 
-
-  //     //append the item to the OL list
-  //     div.appendChild(div2);
-  //     li.appendChild(div)
-  //     todoItems.appendChild(li);
-      
-  //   }, false);
-  
-  }
-
-
-//notes as follows:
-//
-//methods
-//create new item ----  myItem = new Item("buy milk");
-//delete item (remove it from the todolist) 
-//mark item as completed ---- myItem.complete();
-//delete from completed (remove it from the todolist)
-
-//for display - grab this and append child
-
-// <ol id="todo-items"></ol>
-
-// <div class="actions">
-// <span class="meta-data">Created on: 11/4/2013 </span>
-// <button class="complete">completed</button>
-// <button class="delete">delete</button>
-// </div>
