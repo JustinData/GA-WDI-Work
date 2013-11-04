9c292e46624930c6c51eec17dfa05bd8a5e8ff19
Raleigh D Froeber <RDFroeber@gmail.com>
2013-11-04 12:36:20 -0500

    To Do List array loading

--- w07/d01/Raleigh/list_rho/list/todo.js
+++ w07/d01/Raleigh/list_rho/list/todo.js
@@ -1,22 +1,43 @@
-function addToDo(user_item){
-   this.item = user_item;
+function ToDoList() {
+  this.list_items = [];
+}
+
+ToDoList.prototype.pushItem = function(user_item) {
+  this.list_items.push(user_item);
+} 
+
+
+function addToDo(user_item, ordered_list){
+   var li = document.createElement("li"); // <li>
+   li.innerHTML = user_item; // <li>innerHTML</li>
+
+   ordered_list.appendChild(li);
+}
+
+function appendToDo(toDo, ordered_list){
+   ordered_list.innerHTML = "";
+
+   for( var i = 0; i < toDo.list_items.length; i++ ){
+      addToDo(toDo.list_items[i], ordered_list);
+   }
+}
+
+window.toDo = new ToDoList();
+toDo.pushItem("Buy Milk");
+
+function loadList() {
    var ordered_list = document.getElementById("todo-items"); // <ol>
 
-   var new_item = document.createElement("li"); // <li>
-   new_item.innerHTML = item; // <li>innerHTML</li>
-
-   ordered_list.appendChild(new_item);
-
-}
-
-
-window.onload = function() {
    var input = document.getElementById("add-item"); // <button>
    input.addEventListener('click', function() {
-      var item = document.getElementById("new-task-field").value; // <input>.value
+      var item = document.getElementById("new-task-field"); // <input>
 
-      addToDo(item);
+      window.toDo.pushItem(item.value); // <input>.value
+      item.value = "";
    });
 
+    window.appendToDo(window.toDo, ordered_list);
 }
+
+window.onload = loadList;
 
