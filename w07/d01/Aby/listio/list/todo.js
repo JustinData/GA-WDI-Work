9b5795b0de2fc5f58f5e5120f3116bfe5b8416e2
Aby Bandoh <aby.bandoh@gmail.com>
2013-11-04 12:36:38 -0500

    item shows up on list

--- w07/d01/Aby/listio/list/todo.js
+++ w07/d01/Aby/listio/list/todo.js
@@ -1,21 +1,44 @@
-function addToDo(user_item){
-   this.item = user_item;
-   var ordered_list = document.getElementById("todo-items"); // <ol>
+function ToDoList() {
+	this.list_items = [];
+}
 
-   var new_item = document.createElement("li"); // <li>
-   new_item.innerHTML = item; // <li>innerHTML</li>
+ToDoList.prototype.push = function(user_item) {
+  // OOOOHHHHHH!
+  this.list_items.push(user_item);
+} 
 
-   ordered_list.appendChild(new_item);
+
+function addToDo(user_item, ordered_list){
+ 
+  var li = document.createElement("li"); // <li>
+  li.innerHTML = user_item; // <li>innerHTML</li>
+  ordered_list.appendChild(li);
 
 }
 
+function appendToDo(ToDo, ordered_list){
+	ordered_list.innerHTML = "";
 
-window.onload = function() {
-   var input = document.getElementById("add-item"); // <button>
-   input.addEventListener('click', function() {
-      var item = document.getElementById("new-task-field").value; // <input>.value
+	 for( var i = 0; i < ToDo.list_items.length; i++ ){
+	   addToDo(ToDo.list_items[i], ordered_list);
+	 }
+}
 
-      addToDo(item);
+window.ToDo = new ToDoList();
+ToDo.push("test");
+
+function loadList() {
+	var ordered_list = document.getElementById("todo-items"); // <ol>
+	var input = document.getElementById("add-item"); // <button>
+  input.addEventListener('click', function() {
+  var item = document.getElementById("new-task-field"); // <input>.value
+
+		ToDo.push(item.value);
+		item.value = "";
    });
 
+	window.appendToDo(window.ToDo, ordered_list);
 }
+window.onload = loadList;
+  
+
