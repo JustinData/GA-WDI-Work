ed91fbc4b3504474f4f92c37ba10a9d75abf51ce
Julie lappano <julie.lappano@gmail.com>
2013-11-05 10:28:53 -0500

    input box colors

--- w07/d02/Julie/pixart/pixart.js
+++ w07/d02/Julie/pixart/pixart.js
@@ -3,7 +3,7 @@ window.onload = function() {
  var inputField = document.getElementById("color-field");
  var button = document.getElementById("set-color");
  var brush  = document.querySelector(".brush");
- // var square = document.querySelector(".square");
+
 
 
  function setColor(){
@@ -23,9 +23,7 @@ inputField.addEventListener('keyup', function(event){
     }
   });
 
-makeDiv();
 
-}
 
 function makeDiv(){
   
@@ -35,12 +33,34 @@ function makeDiv(){
   div.setAttribute("class", "square");
   document.body.appendChild(div);
   div.addEventListener("click", function(){
-    this.style.background = "green";
+    var color = inputField.value;
+    this.style.background = color;
   })
   count ++;
   }
 
 }
+
+makeDiv();
+
+
+}
+
+// function makeDiv(){
+  
+//   var count = 0
+//   while(count <= 20){
+//   var div = document.createElement("div");
+//   div.setAttribute("class", "square");
+//   document.body.appendChild(div);
+//   div.addEventListener("click", function(){
+//     var color = inputField.value;
+//     this.style.background = color;
+//   })
+//   count ++;
+//   }
+
+// }
 
 
 
