3caad933c1e0c3016dbeec2ae2e9b7d9e1f20041
Nichol Alexander <nichol.alexander@gmail.com>
2013-11-05 10:29:35 -0500

    starting on swatch

--- w07/d02/Nichol/pixart/pixart.js
+++ w07/d02/Nichol/pixart/pixart.js
@@ -6,9 +6,31 @@ window.onload = function(){
   var colorField = document.getElementById("color-field");
   var brush = document.querySelector(".brush");
 
+
+  var swatch1 = document.querySelector(".swatch1");
+  var swatch2 = document.querySelector(".swatch2");
+  var swatch3 = document.querySelector(".swatch3");
+  var counter = 1;
+
   var changeBrushHandler = function(){
     var value = colorField.value;
     brush.style.background = value;
+
+    if(counter == 1){
+      console.log(swatch1);
+      swatch1.style.background = value;
+    }
+    if(counter == 2){
+      swatch2.style.background = value;
+
+    }
+    if(counter == 3){
+      swatch3.style.background = value;
+      counter = 0;
+    }
+
+    counter++;
+    console.log(counter);
   }
 
   elSetColor.addEventListener("click", function(){
@@ -21,18 +43,17 @@ window.onload = function(){
     }
   });
 
-  for(var i=0; i<20; i++){
+  for(var i=0; i<10000000; i++){
     var newDiv = document.createElement("div");
     newDiv.className = "square";
-    newDiv.addEventListener("click", function(){
+    
+    newDiv.addEventListener("mouseover", function(){
       var color = document.querySelector(".brush").style.background;
       this.style.background = color;
     
     });
 
-    document.getElementsByTagName("body")[0].appendChild(newDiv);
-
-
+    document.body.appendChild(newDiv);
 
   }
 
