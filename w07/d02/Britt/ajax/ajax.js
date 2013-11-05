838b94ffbefdde24f1588d71de1098fa0aea6c4c
brittlewis12 <brittlewis12@gmail.com>
2013-11-05 16:19:16 -0500

    displayMovie function will show movie posters on the page

--- w07/d02/Britt/ajax/ajax.js
+++ w07/d02/Britt/ajax/ajax.js
@@ -2,14 +2,31 @@ window.onload = function(){
   console.log("LOADED!");
 }
 
-function getMovie(movie){
+function getMovie(movie, callback){
+  var movieQuery = movie.replace(" ", "+")
+
   var httpRequest = new XMLHttpRequest();
 
   httpRequest.addEventListener("load", function(){
-    console.log("After Load.");
-    console.log(httpRequest.responseText)
+    callback(httpRequest.responseText);
   });
 
-  httpRequest.open("GET", "http://www.omdbapi.com/?t=" + movie);
+  httpRequest.open("get", "http://www.omdbapi.com/?t=" + movieQuery);
   httpRequest.send();
+  return httpRequest;
+};
+
+function appendPoster(responseString) {
+  var movie = JSON.parse(responseString);
+
+  img = document.createElement("img");
+  img.src = movie.Poster;
+  img.alt = movie.Title;
+
+  var body = document.body;
+  body.appendChild(img);
+};
+
+function displayMovie(movieTitle){
+  getMovie(movieTitle, appendPoster);
 };
