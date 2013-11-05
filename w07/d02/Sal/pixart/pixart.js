8dc4cee7b32d09b2fc5d0fb07afe42b853c5780c
slardizzone <slardizzone@gmail.com>
2013-11-05 11:08:02 -0500

    complete pixart

--- w07/d02/Sal/pixart/pixart.js
+++ w07/d02/Sal/pixart/pixart.js
@@ -7,31 +7,36 @@ window.onload = function() {
   var inputField = document.getElementById('color-field');
   var brush = document.querySelector('div.brush');
   var button = document.getElementById('set-color');
+  var box = document.querySelector('square')
+  var color = '#1B4370';
 
   // TO ENCAPSULATE THE SET COLOR FUNCTION
-  // function setColor() {
-  //   var color = inputField.value; 
-  //   brush.style.background = color; 
-  // }
+  function setColor() {
+    color = inputField.value; 
+    brush.style.background = color; 
+  }
 
   // ADD EVENT LISTENER TO BUTTON
   button.addEventListener('click', function(){
-   var color = inputField.value;
-   brush.style.background = color; 
+      setColor();
   });
 
   // ADD EVENT LISTENER TO FORM TO SUBMIT UPON ENTER
   inputField.addEventListener("keypress", function(e) {
     var key = e.which || e.keycode
     if (key === 13) {
-      var color = inputField.value;
-      brush.style.background = color; 
+      setColor(); 
     }
   });
 
-  for(var i = 0; i < 20; i++) {
+
+  //CREATE FOR LOOP TO CREATE 20 SQUARE AND ADD LISTENERS
+  for(var i = 0; i < 15000; i++){
     var box = document.createElement('div');
     box.className = 'square';
+    box.addEventListener("mouseover", function(){
+     this.style.background = color;
+    });
     document.body.appendChild(box);
   };
 }
