2bcf6705786f290ea1f6f5e979b7f14d37506147
Russell Taff <rtaff2012@gmail.com>
2013-11-05 09:53:22 -0500

    event listener press enter

--- w07/d02/Russell/pixelart/pixart/pixart.js
+++ w07/d02/Russell/pixelart/pixart/pixart.js
@@ -1,15 +1,22 @@
-window.onload = function () {
+window.onload = function() {
   var inputField = document.getElementById("color-field");
   var button = document.getElementById("set-color");
   var brush= document.querySelector(".brush");
 
-  button.addEventListener('click', function(e){
+
+  function setColor(){
+      var color = inputField.value;
+      brush.style.background = color;
+  }
+  button.addEventListener('click', function(){
     var color = inputField.value 
     brush.style.background = color;
-  })
-  field.addEventListener('keypress', function (e) {
+  });
+
+  inputField.addEventListener('keypress', function(e){
     var key = e.which || e.keyCode;
-    if (key == 13) { inputField();
-    field.value = null;
+    if (key === 13){
+      setColor();
     }
+  });
 }
