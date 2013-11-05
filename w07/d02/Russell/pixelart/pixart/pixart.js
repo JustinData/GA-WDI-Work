ce79f0f4e0d3a5cbf358f363d3b0198e1ce98a15
Russell Taff <rtaff2012@gmail.com>
2013-11-05 10:14:17 -0500

    got each box to change colors on click

--- w07/d02/Russell/pixelart/pixart/pixart.js
+++ w07/d02/Russell/pixelart/pixart/pixart.js
@@ -2,6 +2,7 @@ window.onload = function() {
   var inputField = document.getElementById("color-field");
   var button = document.getElementById("set-color");
   var brush= document.querySelector(".brush");
+  var square = document.querySelector(".square");
 
 
   function setColor(){
@@ -18,5 +19,17 @@ window.onload = function() {
     if (key === 13){
       setColor();
     }
-  });
+      });
+    for (var i = 0; i<20; i++){
+      var box = document.createElement('div');
+      box.className = 'square';
+      document.body.appendChild(box);
+      box.addEventListener('click', function(){
+      var boxcolor = "green";
+      this.style.background = boxcolor;
+      })
+    }
+
 }
+
+
