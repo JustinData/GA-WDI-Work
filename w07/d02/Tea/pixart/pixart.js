08fc9dc7ad77f107ae318c540a1c241694924594
teabait <hello.teaho@gmail.com>
2013-11-05 09:42:30 -0500

    add 20 squares

--- w07/d02/Tea/pixart/pixart.js
+++ w07/d02/Tea/pixart/pixart.js
@@ -1,5 +1,6 @@
 window.onload = function(){
   var setColorButton = document.getElementById('set-color');
+  
   setColorButton.addEventListener('click', function(e){
     e.preventDefault();
     var colorField = document.getElementById("color-field");
@@ -8,13 +9,21 @@ window.onload = function(){
     changeColor(brushBox,colorField.value);
     colorField.value = null;
   });
+
+  moarSquares();
+
+
 }
 
 function changeColor(element, color){
    element.style.background = color;
 }
 
-function moarSquare(){
-  var div = window.document.createElement("div");
-  div.className = "square";
+function moarSquares(){
+  for(i = 0; i < 21; i ++) {
+    var div = window.document.createElement("div");
+    div.className = "square";
+    var squareBox = document.querySelector(".controls");
+    squareBox.appendChild(div);
+  }
 }
