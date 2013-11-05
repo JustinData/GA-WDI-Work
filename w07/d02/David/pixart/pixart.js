cccd1780b006d1c34f07bf60fc6a7502afa5073c
David Fox Powell <dtothefp@gmail.com>
2013-11-05 10:30:33 -0500

    Color box stage 6 complete

--- w07/d02/David/pixart/pixart.js
+++ w07/d02/David/pixart/pixart.js
@@ -9,11 +9,13 @@ Art.prototype.initialize = function() {
   this.submitButton = document.getElementById("set-color");
   this.colorBox = document.querySelector("div.brush");
   this.form = document.getElementById("color-form");
+  this.color = this.valueWindow.value;
   var self = this;
   this.form.addEventListener('submit', function(event) {
     event.preventDefault();
+    console.log("click function");
+    console.log(self.valueWindow.value);
     self.changeColor( self.valueWindow.value );
-    self.createDiv( self.valueWindow.value );
   });
   /******** Alternative method for submiting with return or click *********/
   // this.valueWindow.addEventListener('keypress' function(e){
@@ -26,18 +28,20 @@ Art.prototype.initialize = function() {
 
 Art.prototype.changeColor = function( colorStr ) {
  this.colorBox.style.background = colorStr;
+ this.color = colorStr;
 }
 
-Art.prototype.createDiv = function( colorStr ) {
-  console.log("create div function");
-  console.log("chosen color" + colorStr);
-  for (var i = 0; i < 20; i++) {
+Art.prototype.createDiv = function( ) {
+  // console.log("create div function");
+  // console.log("chosen color" + this.color);
+  var self = this;
+  for (var i = 0; i < 8000; i++) {
     var body = document.querySelector("body");
     var divSquare = document.createElement("div");
     divSquare.className = "square";
     //must use this inside the listener because the divSquare variable changes every time through the loop
-    divSquare.addEventListener('click', function(color) {
-      this.style.background = colorStr;
+    divSquare.addEventListener('mouseover', function(color) {
+      this.style.background = self.color;
     });
     body.appendChild(divSquare);
   }
@@ -46,4 +50,5 @@ Art.prototype.createDiv = function( colorStr ) {
 window.onload = function() {
   console.log("loaded!!");
   newGame.initialize();
+  newGame.createDiv();
 }
