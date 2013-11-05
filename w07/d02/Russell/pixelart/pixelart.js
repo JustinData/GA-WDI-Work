33051c08d72863c6f2ab16e20e83b0cc2dddb370
Russell Taff <rtaff2012@gmail.com>
2013-11-05 09:28:27 -0500

    morning code

---
+++ w07/d02/Russell/pixelart/pixelart.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+  var inputField = document.getElementById("color-field");
+  var button = document.getElementById("set-color");
+  var brush= document.querySelector(".brush");
+  
+}
