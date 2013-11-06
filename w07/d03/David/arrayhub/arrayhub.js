8ae1f7b6ea73f808c6ee3e9d8910d13b5ffa451a
David Fox Powell <dtothefp@gmail.com>
2013-11-06 09:48:38 -0500

    morning ex part 3 complete

--- w07/d03/David/arrayhub/arrayhub.js
+++ w07/d03/David/arrayhub/arrayhub.js
@@ -1,28 +1,39 @@
 window.onload = function() {
   console.log("LOADED!!!");
+  submitForm(document.querySelector("form"));
 }
 
-function getResponse(){
-var request = new XMLHttpRequest();
-request.open('GET','https://api.github.com/users/dtothefp'); 
-request.send();
-return request.responseText;
+function submitForm(formElement){
+  var formInput = formInput = document.querySelector("input");
+  formElement.addEventListener("submit", function(e){
+    e.preventDefault();
+    getUser(formInput.value, displayUser);
+    formInput.value = "";
+  }); 
 }
 
-var response = request.responseText;
-response = JSON.parse(response);
+function getUser(handle, callback) {
+  window.httpRequest = new XMLHttpRequest();
+  httpRequest.addEventListener("load", function(){
+    callback(JSON.parse(httpRequest.responseText));
+  });
+  httpRequest.open('GET','https://api.github.com/users/' + handle); 
+  httpRequest.send();
+}
 
-var userDiv = document.createElement("div");
-userDiv.className = "user";
-var userImg = document.createElement("img");
-userImg.src = response.avatar_url;
-var h2 = document.createElement("h2");
-var userLink = document.createElement("a");
-userLink.innerHTML = response.login;
-userLink.href = response.html_url
+function displayUser(handleObj){
+  var userDiv = document.createElement("div");
+  userDiv.className = "user";
+  var userImg = document.createElement("img");
+  userImg.src = handleObj.avatar_url;
+  var h2 = document.createElement("h2");
+  var userLink = document.createElement("a");
+  userLink.innerHTML = handleObj.login;
+  userLink.href = handleObj.html_url
 
-userDiv.appendChild(userImg);
-h2.appendChild(userLink);
-userDiv.appendChild(h2)
-document.body.appendChild(userDiv);
+  userDiv.appendChild(userImg);
+  h2.appendChild(userLink);
+  userDiv.appendChild(h2)
+  document.body.appendChild(userDiv);
+}
 
