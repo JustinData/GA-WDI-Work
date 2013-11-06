26f3fa4207de2f82dec46c430a83023807441d77
Julie lappano <julie.lappano@gmail.com>
2013-11-06 10:08:13 -0500

    add git div function

--- w07/d03/Julie/arrayhub.js
+++ w07/d03/Julie/arrayhub.js
@@ -1,42 +1,38 @@
-// #Arrayhub
 
-// Warmup to practice AJAX using XMLHttpRequest & review JavaScript
 
-// ###Commit 1
 
-// * Get your AJAX request working in the console like we did yesterday with the Open Movie Database. When you enter a username and make a GET request to an address like this: `https://api.github.com/users/peterylai`, you should be able to get response text that looks like this:
 
-// ```json
-// {
-//   "login": "peterylai",
-//   "id": 1940258,
-//   "avatar_url": "https://0.gravatar.com/avatar/4053f280a55ea718b238d3116b6a627a?d=https%3A%2F%2Fidenticons.github.com%2Fdb486e14f5d3617c0a08b61015e56d3d.png&r=x",
-//   "gravatar_id": "4053f280a55ea718b238d3116b6a627a",
-//   "url": "https://api.github.com/users/peterylai",
-//   "html_url": "https://github.com/peterylai",
-//   "followers_url": "https://api.github.com/users/peterylai/followers",
-//   "following_url": "https://api.github.com/users/peterylai/following{/other_user}",
-//   "gists_url": "https://api.github.com/users/peterylai/gists{/gist_id}",
-//   "starred_url": "https://api.github.com/users/peterylai/starred{/owner}{/repo}",
-//   "subscriptions_url": "https://api.github.com/users/peterylai/subscriptions",
-//   "organizations_url": "https://api.github.com/users/peterylai/orgs",
-//   "repos_url": "https://api.github.com/users/peterylai/repos",
-//   "events_url": "https://api.github.com/users/peterylai/events{/privacy}",
-//   "received_events_url": "https://api.github.com/users/peterylai/received_events",
-//   "type": "User",
-//   "site_admin": false,
-//   "name": "Peter Lai",
-//   "company": null,
-//   "blog": null,
-//   "location": null,
-//   "email": null,
-//   "hireable": false,
-//   "bio": null,
-//   "public_repos": 10,
-//   "followers": 18,
-//   "following": 3,
-//   "created_at": "2012-07-09T02:18:22Z",
-//   "updated_at": "2013-11-05T20:57:54Z",
-//   "public_gists": 1
-// }
-// ```
+function makeDiv(requestData){
+    var text = requestData.responseText;
+    var objResponse = JSON.parse(text);
+    var gitDiv = document.createElement("div");
+    gitDiv.setAttribute("class", "user");
+    document.body.appendChild(gitDiv);
+    var img = document.createElement("img");
+    img.src = objResponse.avatar_url;
+    gitDiv.appendChild(img);
+    var header = document.createElement("h2");
+    var link = document.createElement("a");
+    link.href = objResponse.url;
+    link.innerText = objResponse.login;
+    gitDiv.appendChild(header);
+    header.appendChild(link);
+}
+
+
+
+function getUser(handle){
+  window.httpRequest = new XMLHttpRequest();
+  httpRequest.addEventListener("load", function(){
+
+
+    makeDiv(httpRequest);
+
+
+  });
+  httpRequest.open("GET", "https://api.github.com/users/" + handle);
+
+  httpRequest.send();
+}
+
+
