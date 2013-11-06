54da3233f3630a78d49ee72172912c7552f20ae0
teabait <hello.teaho@gmail.com>
2013-11-06 09:44:59 -0500

    array hub without bonus

---
+++ w07/d03/Tea/arrayhub/arrayhub.js
@@ -0,0 +1,42 @@
+window.onload = function(){
+
+  var input = document.querySelector("input");
+  var button = document.querySelector("button");
+  button.addEventListener('click', function(e){
+    e.preventDefault();
+    var username = input.value;
+    getGit(username, createUserDiv);
+  });
+}
+
+function getGit(username, callback){
+  // set window. to make it accessible within console
+  window.httpRequest = new XMLHttpRequest();
+
+  // listens to Load event before doing this
+  httpRequest.addEventListener("load", function(){
+      callback(JSON.parse(httpRequest.responseText));
+  });
+
+  httpRequest.open("GET", "https://api.github.com/users/" + username);
+  httpRequest.send();
+}
+
+function createUserDiv(username){
+  // user div
+  var div = window.document.createElement("div");
+  div.className = "user";
+  document.body.appendChild(div);
+  // user image
+  var img = window.document.createElement("img");
+  img.src = username.avatar_url;
+  div.appendChild(img);
+  //user name & link to Github
+  var h2 = window.document.createElement("h2");
+  var a = window.document.createElement("a");
+  a.href = "https://github.com/" + username.login;
+  a.innerHTML = username.login;
+  h2.appendChild(a);
+  div.appendChild(h2);
+
+}
