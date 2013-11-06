b398c7e6158c0b85e34f8ac477e68c13825b51b0
JustinGA <justinkemmerer@gmail.com>
2013-11-06 09:18:20 -0500

    7 2

--- w07/d02/Justin/pixart/pixart.js
+++ w07/d02/Justin/pixart/pixart.js
@@ -8,10 +8,42 @@ window.onload = function() {
 	var brush = document.querySelector(".brush");
 	var button = document.getElementById("set-color");
 	var inputField = document.getElementById("color-field");
-	// input = ""
 
-	button.addEventListener("click", function(){
+	function setColor(){
 		var color = inputField.value;
 		brush.style.background = color;
+		inputField.value = "";
+	}
+
+	button.addEventListener("click", function(){
+		setColor();
 	})
+
+	inputField.addEventListener("keypress", function(event) {
+		var key = event.which || event.keycode;
+		if (key === 13){
+			setColor();
+		}
+	})
+
+	function createDivs() {
+		var newDiv = document.createElement("div");
+		newDiv.setAttribute("class", "square");
+		body.appendChild(newDiv);
+	}
+
+	for (var i = 0; i < 8000; i++){
+		var box = document.createElement('div');
+		box.className = "square";
+		box.addEventListener("mouseover", function(){
+			var color = inputField.value;
+			if (this.style.background === color){
+				this.style.background = "";
+			} else {
+				this.style.background = color;
+			}
+		})
+		document.body.appendChild(box);
+	}
 }
+
