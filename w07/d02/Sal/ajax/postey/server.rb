5d7df8d75da2301bef1c8184d2d8f934c00857a4
slardizzone <slardizzone@gmail.com>
2013-11-06 13:53:36 -0500

    more ajax exercise with postey

--- w07/d02/Sal/ajax/postey/server.rb
+++ w07/d02/Sal/ajax/postey/server.rb
@@ -1,11 +1,16 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'pry'
+require 'json'
 
-get '/' do 
+posts = [{title: "First Post", content: "So much stuff"}]
+
+get '/' do
+  @posts = posts
   erb :index
 end
 
 post '/' do 
-  binding.pry
+  posts << {title: params[:title], content: params[:content]}
+  JSON(posts)
 end
