5324a2675b452041d7551b49ce1ed5e1c7518b12
Julie lappano <julie.lappano@gmail.com>
2013-11-05 21:56:15 -0500

    input works

--- w07/d01/Julie/hangman/hangman-starter/js/game.js
+++ w07/d01/Julie/hangman/hangman-starter/js/game.js
@@ -11,15 +11,31 @@ var inputBox = document.getElementById("letter_field");
  Hangman.prototype.chooseSampleWord = function() {
   var sample = _.sample(this.wordBank);
 }
+ 
 
 Hangman.prototype.inputLetter = function() {
 var inputBox = document.getElementById("letter_field");
    inputBox.onkeyup= myfunction;
+var array = this.letterGuessedAlready
 
 function myfunction(){
-    console.log(inputBox.value);
+    var guessedLetters = document.getElementById("guessed_letters");
+    var guessedLetterValue = inputBox.value;
+
+    if(_.indexOf(array, guessedLetterValue) === -1){
+    guessedLetters.innerHTML += guessedLetterValue;
+    array.push(guessedLetterValue);
+    this.value = " ";
   }
+   else{
+    this.value = " ";
+   }
+  
+  }
+
 }
+
+
 
 window.onload = function() {
 
