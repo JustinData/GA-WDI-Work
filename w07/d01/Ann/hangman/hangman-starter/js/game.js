5798f7202478bd8c8257d9472e85c7056690a5f4
millzpaugh <ann.millspaugh@gmail.com>
2013-11-05 20:37:19 -0500

    word and info

--- w07/d01/Ann/hangman/hangman-starter/js/game.js
+++ w07/d01/Ann/hangman/hangman-starter/js/game.js
@@ -1,8 +1,11 @@
 window.onload = function(){
   console.log("I GOT DA BEATZZZZZ")
 
+  wordsArray = ['ruby', 'rails', 'javascript', 'array', 'hash', 'underscore', 'sinatra', 'model', 'controller', 'view', 'devise', 'authentication', 'capybara', 'jasmine', 'cache', 'sublime', 'terminal', 'system', 'twitter', 'facebook', 'function', 'google', 'amazon', 'development', 'data', 'design', 'inheritance', 'prototype', 'gist', 'github', 'agile', 'fizzbuzz', 'route', 'gem', 'deployment', 'database']; 
+
   guessedLettersArray = []; 
   letterField = document.getElementById("letter_field"); 
+  resetButton = document.getElementById("reset_button"); 
 
   letterField.addEventListener('keypress', function(e){
       var key = e.which || e.keyCode;
@@ -10,14 +13,37 @@ window.onload = function(){
         guessedLettersArray.push(letterField.value); 
       console.log(guessedLettersArray); 
     };  
-    updateGuessedLetters(); 
+    updateGuessedLetters();
+    remainingGuesses();  
     letterField.value = null; 
-  }); 
+  });
+  randomWord(); 
 }
+
+
+
+  function randomWord(){
+    resetButton.addEventListener('click', function(e){
+      var word = wordsArray[Math.floor(Math.random() * wordsArray.length)];
+      var wordToGuess = document.getElementById("word_string");
+      wordToGuess.innerText = word 
+    }); 
+  }; 
+
 
  function updateGuessedLetters(){
     var guessedLetters = document.getElementById("guessed_letters"); 
     guessedLetters.innerText = guessedLettersArray.join(" ");
     }; 
 
- 
+ function remainingGuesses(){
+    var guesses = (9 - (guessedLettersArray.length + 1)); 
+    var numberofguesses = document.getElementById("guesses_left"); 
+    numberofguesses.innerText = guesses; 
+ }; 
+
+
+
+
+
+
