326eb2bd68f73966853a47c3354d28929ae44414
David Fox Powell <dtothefp@gmail.com>
2013-11-06 13:38:26 -0500

    post lunch commit

--- w07/d03/David/arrayhub/arrayhub.js
+++ w07/d03/David/arrayhub/arrayhub.js
@@ -5,6 +5,7 @@ window.onload = function() {
 
 function submitForm(formElement){
   var formInput = formInput = document.querySelector("input");
+  // could also use getUser(form.elements["handle"].value) -- this targets the input in the form with name="handle"
   formElement.addEventListener("submit", function(e){
     e.preventDefault();
     getUser(formInput.value, displayUser);
@@ -28,16 +29,22 @@ function displayUser(handleObj){
   userImg.src = handleObj.avatar_url;
   var h2 = document.createElement("h2");
   var userLink = document.createElement("a");
-  userLink.innerHTML = handleObj.login;
+  userLink.textContent = handleObj.login;
   userLink.href = handleObj.html_url
+
+   userImg.addEventListener("dblclick", function(){
+    // removes the parent node of the image element, which is the div
+    // or can use var grandparent = this.parentNode.parentNode;
+    // grandparent.removeChild(this.parentNode);
+    // would use this if we didn't know what the parent is
+    document.body.removeChild(this.parentNode);
+  });
 
   userDiv.appendChild(userImg);
   h2.appendChild(userLink);
   userDiv.appendChild(h2)
   document.body.appendChild(userDiv);
 
-  userImg.addEventListener("dblclick", function(){
-    userDiv.remove();
-  });
+
 }
 
