0ba7b900108ee720afdc60ed959cf2f10b068644
Nichol Alexander <nichol.alexander@gmail.com>
2013-11-06 09:41:27 -0500

    c2 - using console

--- w07/d03/Nichol/arrayhub.js
+++ w07/d03/Nichol/arrayhub.js
@@ -7,12 +7,39 @@ function getGitter(username){
 
   httpRequest.addEventListener("load", function(){
     console.log(JSON.parse(httpRequest.responseText));
+    updatePage();
   });
 
   httpRequest.open("GET", "https://api.github.com/users/" + username);
   httpRequest.send();
 
 }
+
+var updatePage = function(){
+  response = JSON.parse(httpRequest.responseText);
+
+  newdiv = document.createElement("div");
+  newdiv.className = "user";
+
+  newimg = document.createElement("img");
+  newimg.src = response["avatar_url"];
+
+  newa = document.createElement("a");
+  newa.href = response["html_url"];
+  newa.innerHTML = response["login"];
+
+  newh = document.createElement("h2");
+
+  newh.appendChild(newa);
+
+  newdiv.appendChild(newimg);
+  newdiv.appendChild(newh);  
+
+  document.body.appendChild(newdiv);
+}
+
+
+
 
 // function getMovie(movie, callback){
 //   // set window. to make it accessible within console
