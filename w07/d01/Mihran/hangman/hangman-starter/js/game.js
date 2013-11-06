5141fa4695f5753c533399a6ca474d7a06e3b43f
Mihran Abrahamian <abrahamian.mihran@gmail.com>
2013-11-05 21:51:40 -0500

    totally works

--- w07/d01/Mihran/hangman/hangman-starter/js/game.js
+++ w07/d01/Mihran/hangman/hangman-starter/js/game.js
@@ -1,30 +1,25 @@
 var words = ['ruby', 'rails', 'javascript', 'array', 'hash', 'underscore', 'sinatra', 'model', 'controller', 'view', 'devise', 'authentication', 'capybara', 'jasmine', 'cache', 'sublime', 'terminal', 'system', 'twitter', 'facebook', 'function', 'google', 'amazon', 'development', 'data', 'design', 'inheritance', 'prototype', 'gist', 'github', 'agile', 'fizzbuzz', 'route', 'gem', 'deployment', 'database'];
-var currentWord = _.sample(words)
-var currentWordArray = _.toArray(currentWord)
-var currentWordLength = currentWordArray.length
-var guessedLettersArray = [];
-var wrongGuessesLeft = 9;
 
-
-window.onload = function(){
-
+newGame = function(){
+  currentWord = _.sample(words)
+  currentWordArray = _.toArray(currentWord)
+  currentWordLength = currentWordArray.length
+  guessedLettersArray = [];
+  wrongGuessesLeft = 9;
   underscores = document.getElementById("word_string");
   _(currentWordLength).times(function(){underscores.innerHTML += "_"});
-
   guessedLetters = document.getElementById("guessed_letters");
-
   guessesLeft = document.getElementById("guesses_left");
   guessesLeft.innerHTML = 9;
-
   letterField = document.getElementById("letter_field")
   letterField.addEventListener('keypress', function (e){
-
     var key = e.which || e.keyCode;
     if (key == 13) { 
+      //if it's not in the word, then push it to guessedLettersArray and
       if (guessedLettersArray.indexOf(letterField.value) === -1) {
       guessedLettersArray.push(letterField.value);
       console.log(guessedLettersArray);
-      updateGuessedLetters();
+      guessedLetters.innerHTML = guessedLettersArray.join(" ");
       };
 
       //if input letter is not in the word, and you still have wrongGuesses left, decrease wrongGuesses by 1,
@@ -37,40 +32,28 @@ window.onload = function(){
       if (currentWordArray.indexOf(letterField.value) > -1) {
         _.each(currentWordArray, function(e, i) {
           if ( e == letterField.value) {
-            underscores.innerHTML[i] = e;
+            textArr = underscores.innerHTML.split("")
+            textArr[i] = e
+            underscores.innerHTML = textArr.join("");
           }
         });
       }
-    
     letterField.value = null;
-
     }
   })
+}
 
+window.onload = function(){
+
+  newGame();
 
   giveUpButton = document.getElementById("give_up_button");
   giveUpButton.addEventListener("click", function(){
-    underscores.innerHTML = currentWord
+    underscores.innerHTML = currentWordArray
   })
-
-//refactor this and the whole program will become more object-oriented!!
   resetButton = document.getElementById("reset_button");
   resetButton.addEventListener("click", function(){
-    currentWord = _.sample(words)
-    currentWordArray = _.toArray(currentWord)
-    currentWordLength = currentWordArray.length
-    guessedLettersArray = [];
-    wrongGuessesLeft = 9;
     underscores.innerHTML = ""
-    _(currentWordLength).times(function(){underscores.innerHTML += "_"});
-
+    newGame();
   })
 }
-
-function updateGuessedLetters(){
-  guessedLetters.innerHTML = guessedLettersArray.join(" ");
-}
-
-
-
-
