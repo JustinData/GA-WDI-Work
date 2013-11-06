e92040421bb1921ffdd94c179a6b8203379af833
Julie lappano <julie.lappano@gmail.com>
2013-11-05 23:29:04 -0500

    guesses down works

--- w07/d01/Julie/hangman/hangman-starter/js/game.js
+++ w07/d01/Julie/hangman/hangman-starter/js/game.js
@@ -6,29 +6,53 @@ function Hangman(){
 
  }
 
-var inputBox = document.getElementById("letter_field");
+
+
+ Hangman.prototype.displayGuesses = function() {
+    var spanGuess = document.getElementById("guesses_left");
+    spanGuess.innerText = this.guessLeft;
+ }
+
 
  Hangman.prototype.chooseSampleWord = function() {
   var sample = _.sample(this.wordBank);
+  var word = document.getElementById("word_string");
+  var splitSample = sample.split("");
+  console.log(splitSample);
+  for (var i =  splitSample.length - 1; i >= 0; i--) {
+       word.innerText += "_";
+  }
+  return splitSample;
 }
  
 
+
+
+
 Hangman.prototype.inputLetter = function() {
+
 var inputBox = document.getElementById("letter_field");
+  var array = this.letterGuessedAlready;
+  var numGuesses = this.guessLeft;
+  var spanGuess = document.getElementById("guesses_left"); 
+
+
    inputBox.onkeyup= myfunction;
-var array = this.letterGuessedAlready
 
 function myfunction(){
     var guessedLetters = document.getElementById("guessed_letters");
     var guessedLetterValue = inputBox.value;
 
     if(_.indexOf(array, guessedLetterValue) === -1){
-    guessedLetters.innerHTML += guessedLetterValue;
-    array.push(guessedLetterValue);
-    this.value = " ";
+      guessedLetters.innerHTML += guessedLetterValue;
+      array.push(guessedLetterValue);
+      numGuesses --;
+      spanGuess.innerText = numGuesses;
+      this.value = " ";    
   }
    else{
     this.value = " ";
+
    }
   
   }
@@ -37,6 +61,14 @@ function myfunction(){
 
 
 
-window.onload = function() {
 
+
+
+
+
+window.onload = function() {
+  game = new Hangman();
+  game.chooseSampleWord();
+  game.inputLetter();
+  game.displayGuesses();
 }
