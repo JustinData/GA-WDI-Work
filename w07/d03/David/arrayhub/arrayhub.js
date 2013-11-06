0f184d3f39f1d8bee742bd9ab46ea4d311bf3c17
David Fox Powell <dtothefp@gmail.com>
2013-11-06 09:27:24 -0500

    morning ex part 2 complete

--- w07/d03/David/arrayhub/arrayhub.js
+++ w07/d03/David/arrayhub/arrayhub.js
@@ -8,3 +8,21 @@ request.open('GET','https://api.github.com/users/dtothefp');
 request.send();
 return request.responseText;
 }
+
+var response = request.responseText;
+response = JSON.parse(response);
+
+var userDiv = document.createElement("div");
+userDiv.className = "user";
+var userImg = document.createElement("img");
+userImg.src = response.avatar_url;
+var h2 = document.createElement("h2");
+var userLink = document.createElement("a");
+userLink.innerHTML = response.login;
+userLink.href = response.html_url
+
+userDiv.appendChild(userImg);
+h2.appendChild(userLink);
+userDiv.appendChild(h2)
+document.body.appendChild(userDiv);
+
