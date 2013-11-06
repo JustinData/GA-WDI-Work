6b50b155ec92253804f430b21a108fac26ba915a
Julie lappano <julie.lappano@gmail.com>
2013-11-06 09:25:28 -0500

    geting info in browser

---
+++ w07/d03/Julie/arrayhub.js
@@ -0,0 +1,42 @@
+// #Arrayhub
+
+// Warmup to practice AJAX using XMLHttpRequest & review JavaScript
+
+// ###Commit 1
+
+// * Get your AJAX request working in the console like we did yesterday with the Open Movie Database. When you enter a username and make a GET request to an address like this: `https://api.github.com/users/peterylai`, you should be able to get response text that looks like this:
+
+// ```json
+// {
+//   "login": "peterylai",
+//   "id": 1940258,
+//   "avatar_url": "https://0.gravatar.com/avatar/4053f280a55ea718b238d3116b6a627a?d=https%3A%2F%2Fidenticons.github.com%2Fdb486e14f5d3617c0a08b61015e56d3d.png&r=x",
+//   "gravatar_id": "4053f280a55ea718b238d3116b6a627a",
+//   "url": "https://api.github.com/users/peterylai",
+//   "html_url": "https://github.com/peterylai",
+//   "followers_url": "https://api.github.com/users/peterylai/followers",
+//   "following_url": "https://api.github.com/users/peterylai/following{/other_user}",
+//   "gists_url": "https://api.github.com/users/peterylai/gists{/gist_id}",
+//   "starred_url": "https://api.github.com/users/peterylai/starred{/owner}{/repo}",
+//   "subscriptions_url": "https://api.github.com/users/peterylai/subscriptions",
+//   "organizations_url": "https://api.github.com/users/peterylai/orgs",
+//   "repos_url": "https://api.github.com/users/peterylai/repos",
+//   "events_url": "https://api.github.com/users/peterylai/events{/privacy}",
+//   "received_events_url": "https://api.github.com/users/peterylai/received_events",
+//   "type": "User",
+//   "site_admin": false,
+//   "name": "Peter Lai",
+//   "company": null,
+//   "blog": null,
+//   "location": null,
+//   "email": null,
+//   "hireable": false,
+//   "bio": null,
+//   "public_repos": 10,
+//   "followers": 18,
+//   "following": 3,
+//   "created_at": "2012-07-09T02:18:22Z",
+//   "updated_at": "2013-11-05T20:57:54Z",
+//   "public_gists": 1
+// }
+// ```
