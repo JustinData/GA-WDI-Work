3e8c238d3452af7ecef83e8ffdb8fb8857e04f25
brittlewis12 <brittlewis12@gmail.com>
2013-11-06 09:06:04 -0500

    work from w7d2

---
+++ w07/d02/Britt/ajax/public/ajax.js
@@ -0,0 +1,23 @@
+window.onload = function(){
+  console.log("LOADED!");
+  attachSubmitListener(document.querySelector("form"));
+};
+
+function attachSubmitListener(formElem){
+  formElem.addEventListener("submit", function(event){
+    event.preventDefault();
+    console.log("FORM SUBMITTED!");
+    postData(this);
+  });
+};
+
+function postData(formElem){
+  var httpRequest = new XMLHttpRequest();
+
+  // httpRequest.addEventListener("load", callback);
+
+  httpRequest.open("post", "/");
+
+  var serializedData = new FormData(formElem);
+  httpRequest.send(serializedData);
+};
---
+++ w07/d02/Britt/ajax/server.rb
@@ -0,0 +1,12 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'pry'
+
+get '/?' do
+  erb :index
+end
+
+post '/?' do
+  # shmee blah stuff
+  binding.pry
+end
