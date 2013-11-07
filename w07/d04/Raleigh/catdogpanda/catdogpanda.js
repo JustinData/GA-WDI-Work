62ccc260d65299b15eeadd75406c8015b8032347
Raleigh D Froeber <RDFroeber@gmail.com>
2013-11-07 10:22:18 -0500

    Image freeze on click functionality added

--- w07/d04/Raleigh/catdogpanda/catdogpanda.js
+++ w07/d04/Raleigh/catdogpanda/catdogpanda.js
@@ -1,17 +1,17 @@
 $(document).ready(function(){
-   var cat_img = $('<img class="cat">');
-   cat_img.attr('src', "http://placekitten.com/500/500");
+   var cat_img = $("<img class='cat'>");
+   cat_img.attr("src", "http://placekitten.com/500/500");
    $("body").append(cat_img);
 
-   var dog_img = $('<img class="dog">');
-   dog_img.attr('src', "http://placedog.com/400/400");
+   var dog_img = $("<img class='dog'>");
+   dog_img.attr("src", "http://placedog.com/400/400");
    $("body").append(dog_img);
 
-   var panda_img = $('<img class="panda">');
-   panda_img.attr('src', "http://www.cutepandapictures.com/wp-content/uploads/2012/05/cutepandamombaby.jpg");
+   var panda_img = $("<img class='panda'>");
+   panda_img.attr("src", "http://www.cutepandapictures.com/wp-content/uploads/2012/05/cutepandamombaby.jpg");
    $("body").append(panda_img);
 
-   $("body").on('mousemove', function(e){
+   $("body").on('mousemove', function(event){
       var msg = "(" + event.pageX + ", " + event.pageY + ")";
       $("h2#coords").text(msg);
 
@@ -22,8 +22,12 @@ $(document).ready(function(){
       $(".panda").css({"margin-left": event.pageX/5, "margin-top": event.pageY/5});
    })
 
-   $('img').on('click', function(){
+   $("img").on('click', function(event){
+      event.stopPropagation();
       $(this).toggleClass("border");
-   })
+   });
 
+   $("body").on('click', function(){
+      $("body").off("mousemove");
+   });
 });
