5d706df9a6babf04d8750e29f518e237e10cbc2b
Mihran Abrahamian <abrahamian.mihran@gmail.com>
2013-11-07 09:29:36 -0500

    memory, cards flip up

--- w07/d03/Mihran/memory/memory-starter/memory.js
+++ w07/d03/Mihran/memory/memory-starter/memory.js
@@ -1,24 +1,28 @@
-// This is one way you could approach this. 
-// This is more of a functional approach than an object-oriented one, but it works well in this scenario.
-// If you don't like this approach feel free to throw this code out and write your own :)
-
-//Code In Here gets executed once code is ready. This is like window.onload.
-$(function() {
-
-});
-
-//array of letters in the game
-var letters = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E' ];
-
-//Id of the last card you clicked on.
-var lastCardId = ''
-
-// Code that runs to set up the game
-function startGame() {
-
+function Game(letters){
+  this.randomOrder = _.shuffle(letters);
 }
 
-//Code that runs when you click on a card
-function cardClick() {
-
+Game.prototype.initialize = function(){
+  var randomOrder = this.randomOrder;
+  var game = document.getElementById("game");
+  for (var i = 0; i<this.randomOrder.length; i++) {
+    var column = document.createElement("div");
+    column.className = "column";
+    game.appendChild(column);
+    column.id = i;
+    column.addEventListener("click", function(){
+      this.innerHTML = randomOrder[this.id];
+    })
+  }
 }
+
+window.onload = function(){
+  startButton = document.querySelector("button")
+  startButton.addEventListener("click", function(){
+    $(game).empty();
+    memory = new Game(['A','A','B','B','C','C','D','D', 'E', 'E']);
+    memory.initialize();
+  });
+}
+
+
