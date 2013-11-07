e3bb138fb1e9a8353268ac2ddec26ba67e9f3c76
My Name <myemail@example.com>
2013-11-07 10:02:59 -0500

    parallax working

--- w07/d04/Paris/catdogpanda/catdogpanda.js
+++ w07/d04/Paris/catdogpanda/catdogpanda.js
@@ -11,5 +11,19 @@ $(function() {
   $("body").append(panda);
 
 
+  $("body").mousemove(function(event) {
+  var pageCoords = "( " + event.pageX + ", " + event.pageY + " )";
+  var clientCoords = "( " + event.clientX + ", " + event.clientY + " )";
+  $( "span:first" ).text( "( event.pageX, event.pageY ) : " + pageCoords );
+  $( "span:last" ).text( "( event.clientX, event.clientY ) : " + clientCoords );
+  console.log(clientCoords);
+  $( "#coords" ).text( clientCoords );
+  $(".cat").css({"margin-left": event.pageX/4, "margin-top": event.pageY/4});
+  $(".dog").css({"margin-left": event.pageX/10, "margin-top": event.pageY/10});
+  $(".panda").css({"margin-left": event.pageX/20, "margin-top": event.pageY/20});
+});
+
+  
 
 });
+
