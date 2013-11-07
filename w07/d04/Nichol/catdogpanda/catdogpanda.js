e9ec1344e3662443f5b98300d584869548dee25e
Nichol Alexander <nichol.alexander@gmail.com>
2013-11-07 09:33:00 -0500

    c1 - first damn cat

---
+++ w07/d04/Nichol/catdogpanda/catdogpanda.js
@@ -0,0 +1,11 @@
+//Code In Here gets executed once code is ready. This is like window.onload.
+$(function() {
+  
+  var cat = $("<img class='cat' src='http://placekitten.com/200/200' />");
+  $("body").append(cat);
+
+
+  
+
+
+});
