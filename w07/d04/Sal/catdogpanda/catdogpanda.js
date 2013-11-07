76c36934914492e04b0be29c29cb8d38667f59e4
slardizzone <slardizzone@gmail.com>
2013-11-07 09:27:03 -0500

    1st part of catdogpanda exercise

---
+++ w07/d04/Sal/catdogpanda/catdogpanda.js
@@ -0,0 +1,5 @@
+$(function() {
+  $('body').append('<img class="cat" src="http://placekitten.com/200/200" />')
+  $('body').append('<img class="dog" src="http://placedog.com/200/200" />')
+  $('body').append('<img class="panda" src="http://www.chromama.net/wp-content/uploads/2011/09/sad-panda-2-200x200.jpg" />')
+});
