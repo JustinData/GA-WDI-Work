2f9c6bfad0f0a190454a85764beaa3db64f39c0c
daviskoh <koh.davis.0@gmail.com>
2013-11-07 18:49:50 -0500

    startGame function)

--- w07/d03/Davis/memory/memory-starter/memory.js
+++ w07/d03/Davis/memory/memory-starter/memory.js
@@ -1,9 +1,17 @@
-// This is one way you could approach this. 
-// This is more of a functional approach than an object-oriented one, but it works well in this scenario.
-// If you don't like this approach feel free to throw this code out and write your own :)
+// 10 divs: 5 top, 5 bottom upon click start button
+  // randomly assign each a letter
+// when click on div, render letter
+// on next click
+  // render current div
+  // when value of clicked div != rendered div
+    // unrender previous div
+  // else,
+    // render previous div
 
-//Code In Here gets executed once code is ready. This is like window.onload.
 $(function() {
+  $("button").click(function(){
+    startGame();
+  })
 
 });
 
@@ -13,12 +21,19 @@ var letters = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E' ];
 //Id of the last card you clicked on.
 var lastCardId = ''
 
-// Code that runs to set up the game
 function startGame() {
-
+  // create 10 divs & render
+  var shuffledLetters = _.shuffle(letters);
+  for(var i=0; i<shuffledLetters.length; i++) {
+    // setup divs w/ unique id
+    var div = ($("<div class='column' id=" + i + "></div>"));
+    div.css("color", "white");
+    $("div#game").append(div);
+  }
 }
 
 //Code that runs when you click on a card
 function cardClick() {
 
 }
+
