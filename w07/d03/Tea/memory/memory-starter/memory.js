cb03d2f93a6e2dbbadb687adeba776554feeb7ce
teabait <hello.teaho@gmail.com>
2013-11-06 22:49:02 -0500

    only displays last clicked

--- w07/d03/Tea/memory/memory-starter/memory.js
+++ w07/d03/Tea/memory/memory-starter/memory.js
@@ -12,22 +12,36 @@ var letters = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E' ];
 var shuffled = _.shuffle(letters);
 //Id of the last card you clicked on.
 var lastCardId = ''
-
+var clicks = 0;
 // Code that runs to set up the game
 function startGame() {
-    var cardBox = document.body.querySelector("#game");
+    var cardBox = $(document.body.querySelector("#game"));
     $('button').click(function(){
       for(var i=0; i < 10; i ++){
         var card = $("<div>").attr("id", i).attr("class", "column");
-        $(cardBox).append(card);
+        cardBox.append(card);
       };
     });
     // Code that runs when you click on a card
-    $(cardBox).on( "click", "div", function(){
-      var letter = shuffled[this.id]
+    cardBox.on( "click", "div", function(){
+      var letterId = this.id
+      var letter = shuffled[letterId]
       console.log(letter);
       // display letter to viewer
-      $(this).html(letter);
+      lastCard = $(this);
+      lastCard.html(letter);
+      var lastCard = $(document.getElementById(lastCardId));
+      lastCardId = letterId;
+      console.log(lastCardId);
+      clicks++;
+      console.log(lastCard);
+      console.log("clicks = " + clicks);
+      var divs = document.body.getElementsByClassName("column");
+      for(var i=0; i < divs.length; i ++){
+        if (divs[i].id != lastCardId) {
+          divs[i].innerHTML = "";
+        }
+      };
     });
   }
 
