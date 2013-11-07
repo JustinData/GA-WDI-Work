2134c07142ffaee26a08b2a4589577355a21062e
daviskoh <koh.davis.0@gmail.com>
2013-11-07 09:46:45 -0500

    h1 displays coordinates of mouse

---
+++ w07/d04/Davis/catdogpanda/catdogpanda.js
@@ -0,0 +1,16 @@
+$(function() {
+  var cat = $( "<img class='cat' src='http://eofdreams.com/data_images/dreams/cat/cat-06.jpg'/>" );
+  cat.appendTo($("body"));
+
+  var dog = $( "<img class='dog' src='http://puntito131.puntopressllc.netdna-cdn.com/wp-content/uploads/2013/07/Dog123.jpg'/>" );
+  dog.appendTo($("body"));
+
+  var panda = $( "<img class='panda' src='http://www.washingtonpost.com/rw/WashingtonPost/Content/Blogs/in-the-loop/Images/2012-09-17T130533Z_01_TOR901_RTRIDSP_3_USA-PANDA-WASHINGTON-218.jpg'/>" );
+  panda.appendTo($("body"));
+
+  $("body").mousemove(function(e) {
+    console.log(e.pageX, e.pageY);
+    $("h1").text(e.pageX + ',' + e.pageY);
+  });
+})
+
