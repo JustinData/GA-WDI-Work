a4ab71a9ce56ea203f2155e3780082109164e0a7
Nichol Alexander <nichol.alexander@gmail.com>
2013-11-07 09:38:21 -0500

    c1 cat,dog,panda

--- w07/d04/Nichol/catdogpanda/catdogpanda.js
+++ w07/d04/Nichol/catdogpanda/catdogpanda.js
@@ -4,8 +4,15 @@ $(function() {
   var cat = $("<img class='cat' src='http://placekitten.com/200/200' />");
   $("body").append(cat);
 
-
+  var dog = $("<img class='dog' src='http://placedog.com/200/200' />");
   
+  // var dog = $("<img>").addClass("dog")
+  //                     .attr("src", "http://placedog.com/200/200");
+  
+  $("body").append(dog);
+
+  var panda = $("<img class='panda' src='http://allipadwallpapers.com/wp-content/uploads/wallpapers/Animals/thumbs/thumbs_Panda.jpg' />");
+  $("body").append(panda);
 
 
 });
