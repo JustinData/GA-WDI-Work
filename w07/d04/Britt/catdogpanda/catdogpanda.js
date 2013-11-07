d417a2ac17dfbc1a35fe0f5264609398034b2707
brittlewis12 <brittlewis12@gmail.com>
2013-11-07 09:23:43 -0500

    first commit, catdogpanda: cat, dog and panda on page.

---
+++ w07/d04/Britt/catdogpanda/catdogpanda.js
@@ -0,0 +1,11 @@
+$( function(){
+  var cat = $("<img>").attr({"src": "http://placekitten.com/500/500", "class": "cat"});
+
+  var dog = $("<img>").attr({"src": "http://placedog.com/480/500", "class": "dog"});
+
+  var panda = $("<img>").attr({"src": "http://www.cutepandapictures.com/wp-content/uploads/2012/07/babypandahugsatree.jpg", "class": "panda"});
+
+  cat.appendTo( $("body") );
+  dog.appendTo( $("body") );
+  panda.appendTo( $("body") );
+} )
