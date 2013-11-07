6e2d6404786823cdc1ad9c3c7b09c58a2f39dcd1
My Name <myemail@example.com>
2013-11-07 09:34:08 -0500

    animals working. hooray.

---
+++ w07/d04/Paris/catdogpanda/catdogpanda.js
@@ -0,0 +1,15 @@
+$(function() {
+
+
+
+
+  var cat = $("<img class='cat' src= 'http://placekitten.com/300/400'/>");
+  $("body").append(cat);
+  var dog = $("<img class='dog' src= 'http://placedog.com/400/300'/>");
+  $("body").append(dog);
+  var panda = $("<img class='panda' src= 'http://www.thepetcollective.tv/wp-content/uploads/2013/05/china-panda-researchers-4.jpg'/>");
+  $("body").append(panda);
+
+
+
+});
