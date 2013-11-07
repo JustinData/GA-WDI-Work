4739cdd1f1f7864b3c5a0170da2d23907d63e716
teabait <hello.teaho@gmail.com>
2013-11-07 09:38:56 -0500

    gets mouse cooridnates and displays them

--- w07/d04/Tea/catdogpanda/catdogpanda.js
+++ w07/d04/Tea/catdogpanda/catdogpanda.js
@@ -1,8 +1,13 @@
 $(function(){
-  var body = $(document.body);
-  addCat(body, 500, 600);
-  addDog(body, 500, 600);
-  addPanda(body);
+    var body = $(document.body);
+    addCat(body, 450, 450);
+    addDog(body, 500, 600);
+    addPanda(body);
+
+    body.mousemove(function( event ) {
+      var coords = "( " + event.pageX + ", " + event.pageY + " )";
+      $( "h1" ).text( coords );
+    });
 });
 
 function addCat(parent, width, height){
@@ -11,8 +16,8 @@ function addCat(parent, width, height){
   parent.append(kitten);
 }
 
-function addDog(parent, width, height){
-  var puppyURL = "http://placedog.com/" + width +"/"+ height; 
+function addDog(parent){
+  var puppyURL = "http://distilleryimage10.s3.amazonaws.com/5878d0fa43bd11e3869422000a1deb63_6.jpg"
   var puppy = $("<img>").attr("src", puppyURL).attr("class", "dog");
   parent.append(puppy);
 }
@@ -22,4 +27,3 @@ function addPanda(parent){
   var panda = $("<img>").attr("src", pandaURL).attr("class", "panda");
   parent.append(panda);
 }
-
