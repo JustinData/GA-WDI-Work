c14ab77e33c2ec1e9e7327320060b17a66993f94
My Name <myemail@example.com>
2013-11-07 09:04:55 -0500

    commit

---
+++ w07/d03/Paris/memory/memory-starter/memory.js
@@ -0,0 +1,53 @@
+// // This is one way you could approach this. 
+// // This is more of a functional approach than an object-oriented one, but it works well in this scenario.
+// // If you don't like this approach feel free to throw this code out and write your own :)
+
+// //Code In Here gets executed once code is ready. This is like window.onload.
+// $(function() 
+// // {
+//   // document.getElementByTagName("Start Game").addEventListener('click', startGame());
+// // });
+
+// //array of letters in the game
+// var letters = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E' ];
+
+// //Id of the last card you clicked on.
+// var lastCardId = ''
+
+// // Code that runs to set up the game
+// function startGame() 
+// // {
+// //   // for (var i = 0; i < 11; i++){
+// //   // // $(“<div>”).appendTo( $("#game") );
+// //   // // card.attr("id",i);
+// //   // // card.attr("class","column");
+// //   // };
+// // }
+
+
+// //Code that runs when you click on a card
+// // function cardClick() {
+//   // .on(‘click’), function(){
+//   // $(this).show card with css number with letter[i].sample;
+//   // on(‘click’), function(){
+//   // if the second card $(“div”).filter( “:contains(firstcardletter)” 
+//     // $(this).css(“background-color”,”brown”); 
+//    // on(‘click’), function(){ 
+//     // $(that).css(“background-color”,”brown”);
+//     ) 
+//   // else
+//     // .on('click'), function()
+//   // set the css to x of this and that on click to new card. 
+// //   });
+
+// // }
+
+
+
+
+
+
+
+
+
+
