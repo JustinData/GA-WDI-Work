cae30d8d2393be19e67c1e5575414d03fb01844b
slardizzone <slardizzone@gmail.com>
2013-11-07 10:43:40 -0500

    last parts of catdogpanda exercise

--- w07/d04/Sal/catdogpanda/catdogpanda.js
+++ w07/d04/Sal/catdogpanda/catdogpanda.js
@@ -3,7 +3,19 @@ $(function() {
   $('body').append('<img class="dog" src="http://placedog.com/200/200" />')
   $('body').append('<img class="panda" src="http://www.chromama.net/wp-content/uploads/2011/09/sad-panda-2-200x200.jpg" />')
 
-  $('body').mousemove(function(event) {
+  $('body').on('mousemove', function(event) {
     $('h1').text(event.pageX + ", " + event.pageY);
+    $('img.cat').css({"margin-left": event.pageX/4, "margin-top": event.pageY/4});
+    $('img.dog').css({"margin-left": event.pageX/10, "margin-top": event.pageY/10});
+    $('img.panda').css({"margin-left": event.pageX/20, "margin-top": event.pageY/20});
   });
+
+  $("img").click(function(event) {
+    event.stopPropagation();
+    $( this ).toggleClass( "border");
+  });
+
+  $('body').click(function() {
+    $('body').off('mousemove');
+  })
 });
