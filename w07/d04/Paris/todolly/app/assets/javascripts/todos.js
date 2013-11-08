e6f416eea404a00b9229da28b06d952e45c3f781
My Name <myemail@example.com>
2013-11-07 19:12:53 -0500

    todolly

--- w07/d04/Paris/todolly/app/assets/javascripts/todos.js
+++ w07/d04/Paris/todolly/app/assets/javascripts/todos.js
@@ -8,9 +8,16 @@ function completeIt() {
   $("li input[type='checkbox']").on("click", function(){
     $(this).parent().toggleClass("completed");
 
-    console.log($(this).parent().data("todo-id"));
-    // $.ajax({
-    //     url: "/todos/" + 
-    // });
+
+    var todoId = $(this).parent().data("todo-id");
+    var completed = $(this).is(":checked");
+
+    // to persist the data...
+    $.ajax({
+        url: "/todos/" + todoId,
+        type: "POST",
+        data: {todo: {completed: completed}, _method: 'put'},
+        dataType: "json"
+    });
   });
 }
