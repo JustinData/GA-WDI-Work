708e8a33d4954a561bb29139b769b102026ad80d
My Name <myemail@example.com>
2013-11-07 20:13:35 -0500

    add incremented ids

--- w07/d03/Paris/memory/memory-starter/memory.js
+++ w07/d03/Paris/memory/memory-starter/memory.js
@@ -3,9 +3,14 @@ $(function() {
   appendCards();
 });
 
+var letters = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E'];
+
 function appendCards() {
+  counter = -1
   for (var i = 0; i < 10; i++) {
-  var card = $("<div id=i class='column'>").appendTo($('#game'));
+  var card = $("<div class='column'>").appendTo($('#game'));
+  var cardId = $(card).attr('id', counter + 1);
+  counter++;
   };
 }
 
