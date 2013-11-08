304c79079a5161e875de9211b0bbe25aa8510966
teabait <hello.teaho@gmail.com>
2013-11-08 09:11:20 -0500

    to do

--- w07/d04/Tea/todoly/app/assets/javascripts/todos.js
+++ w07/d04/Tea/todoly/app/assets/javascripts/todos.js
@@ -6,6 +6,15 @@ $(function(){
 function strikeThrough(){
   $('li input[type="checkbox"]').on("click", function(){
     $(this).parent().toggleClass("completed");
-    console.log($(this).parent().data("todo-id"));
+    
+    var todoId = ($(this).parent().data("todo-id"));
+    var completed = $(this).is(':checked');
+
+    $.ajax({
+      url: "/todos/" + todoId,
+      type: "POST", 
+      data: {todo: {completed: completed}, _method: 'put'},
+      dataType: "json"
+   });
   });
 }
--- w07/d04/Tea/todoly/app/controllers/todos_controller.rb
+++ w07/d04/Tea/todoly/app/controllers/todos_controller.rb
@@ -5,4 +5,20 @@ def index
   render :index
 end
 
+def update
+  @todo = Todo.find(params[:id])
+
+  if @todo.update(todo_params)
+    render json: @todo
+  else
+    render status: 400, nothing: true
+  end
+end
+
+private
+
+def todo_params
+  params.require(:todo).permit(:task, :completed)
+end
+
 end
