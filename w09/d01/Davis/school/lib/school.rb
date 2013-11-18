95729eb654de969b92fae175f458dd228d41205e
daviskoh <koh.davis.0@gmail.com>
2013-11-18 14:03:49 -0500

    rspec context

--- w09/d01/Davis/school/lib/school.rb
+++ w09/d01/Davis/school/lib/school.rb
@@ -3,5 +3,18 @@ class School
 
   def initialize(name)
     @name = name
+    @students = []
+  end
+
+  def students
+    @students
+  end
+
+  def enroll_student(name, gpa)
+    students << {name: name, gpa: gpa}
+  end
+
+  def find_student(name)
+    students.find { |student| student[:name] == name }
   end
 end
--- w09/d01/Davis/school/spec/school_spec.rb
+++ w09/d01/Davis/school/spec/school_spec.rb
@@ -6,11 +6,13 @@ require 'spec_helper'
 require_relative '../lib/school'
 
 describe School do
+  # let block defines 'variable' for use within testing
+    # let sets up value
+  # whenever test calls on block below, it is initializing everytime
+  let(:school) { School.new("Springfield Elementary") }
+
   # :: for class method
   describe '::new' do
-
-    # let block defines 'variable' for use within testing
-    let(:school) { School.new("Springfield Elementary") }
 
     it 'creates a new school' do
       # code example inside it block
@@ -20,6 +22,37 @@ describe School do
     it 'has a name' do
       expect(school.name).to eq("Springfield Elementary")
     end
+
+    it 'has no students' do
+      expect(school.students.count).to eq(0)
+    end
   end
 
+  describe '#enroll_student' do
+    it 'adds a student to the school' do
+      school.enroll_student("Bart Simpson", 2.5)
+      expect(school.students.count).to eq(1)
+    end
+  end
+
+  describe '#find_student' do
+    context "Bart is enrolled" do
+      before do
+        school.enroll_student("Bart Simpson", 2.5)
+      end
+
+      it 'finds the student' do
+        bart = school.find_student("Bart Simpson")
+        expect(bart).to eq({name: "Bart Simpson", gpa: 2.5})
+      end
+    end
+
+    it 'doesnt find non existing students' do
+      expect(school.find_student("Shmee Johnson")).to eq(nil)
+    end
+  end
+
+  describe '#average_gpa' do
+
+  end
 end
