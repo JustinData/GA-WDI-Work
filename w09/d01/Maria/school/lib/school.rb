a1734c1aa29a5fb148fa35c0bad0ad1bc8d6ed2e
Maria Mercedes Martinez <pistachiopony@gmail.com>
2013-11-18 14:03:47 -0500

    rspec

--- w09/d01/Maria/school/lib/school.rb
+++ w09/d01/Maria/school/lib/school.rb
@@ -1,6 +1,7 @@
 class School
   def initialize(name)
     @name = name
+    @students = []
   end
 
   def name
@@ -8,16 +9,28 @@ class School
   end
 
   def students
-    @students
+    @students    
   end
 
-  def enroll_students(name, gpa)
+  def enroll_student(name, gpa)
     students.push({name: name, gpa: gpa})
   end
-  
+
   def find_student(name)
-    student.find do |student|
+    students.find do |student|
       student[:name] == name
     end
+  end
+
+  def average_gpa
+    gpas = students.map do |student|
+      student[:gpa]
+    end
+
+    sum = gpas.reduce do |sum, gpa|
+      sum + gpa
+    end
+
+    return sum / gpas.count
   end
 end
--- w09/d01/Maria/school/spec/school_spec.rb
+++ w09/d01/Maria/school/spec/school_spec.rb
@@ -1,14 +1,15 @@
-#school has students
-#each student has a gpa
-# the school has an average gpa
+# School has students
+# Each student has a GPA
+# The school has an average GPA
 
-require'spec_helper'
+require 'spec_helper'
 require_relative '../lib/school'
 
-describe "School" do
+describe School do
+  let(:school) { School.new("Springfield Elementary") }
+
   describe '::new' do
-    let (:school){ School.new("Springfield Elementary")}
-    it "creates a new school" do
+    it 'creates a new school' do
       expect(school).to_not eq nil
     end
 
@@ -17,7 +18,7 @@ describe "School" do
     end
 
     it 'has no students' do
-      expect(school.students.count).to eq (1)
+      expect(school.students.count).to eq(0)
     end
   end
 
@@ -32,11 +33,30 @@ describe "School" do
     it 'finds the student' do
       school.enroll_student("Bart Simpson", 2.5)
       bart = school.find_student("Bart Simpson")
-      expect(bart.to eq({name: "Bart Simpson", gpa: 2.5}))
+      expect(bart).to eq({name: "Bart Simpson", gpa: 2.5})
     end
 
     it "doesn't find non existing students" do
-      expect(school.find_student("Shmee Johnson")). to eq(nil)
+      expect(school.find_student("Shmee Johnson")).to eq(nil)
+    end
+  end
+
+  describe '#average_gpa' do
+    it 'gives an average' do
+      school.enroll_student("Bart Simpson", 2.0)
+      school.enroll_student("Lisa Simpson", 4.0)
+
+      expect(school.average_gpa).to eq(3.0)
     end
   end
 end
+
+
+
+
+
+
+
+
+
+
