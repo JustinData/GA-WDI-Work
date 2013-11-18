cbdeec3270b1565c32091994c6a4b90d087e05ec
daviskoh <koh.davis.0@gmail.com>
2013-11-18 11:33:27 -0500

    2 tests

--- w09/d01/Davis/school/spec/school_spec.rb
+++ w09/d01/Davis/school/spec/school_spec.rb
@@ -5,10 +5,19 @@
 require 'spec_helper'
 require_relative '../lib/school'
 
-describe "School" do
-  describe '#some_method' do
-    it "has behavior"
+describe School do
+  # :: for class method
+  describe '::new' do
+    it 'creates a new school' do
+      # code example inside it block
+      school = School.new("Springfield Elementary")
+      expect(school).to_not eq nil
+    end
+
+    it 'has a name' do
+      school = School.new("Springfield Elementary")
+      expect(school.name).to eq("Springfield Elementary")
+    end
   end
 
-  
 end
