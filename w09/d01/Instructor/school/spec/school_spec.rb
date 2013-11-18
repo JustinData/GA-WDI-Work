42e00f10d8e0f755b820aa975f7c09a949bfe050
Jeffrey Konowitch <jeff.konowitch@generalassemb.ly>
2013-11-18 12:29:36 -0500

    before lunch

--- w09/d01/Instructor/school/spec/school_spec.rb
+++ w09/d01/Instructor/school/spec/school_spec.rb
@@ -6,15 +6,57 @@ require 'spec_helper'
 require_relative '../lib/school'
 
 describe School do
+  let(:school) { School.new("Springfield Elementary") }
+
   describe '::new' do
     it 'creates a new school' do
-      school = School.new("Something")
       expect(school).to_not eq nil
     end
 
     it 'has a name' do
-      school = School.new("Springfield Elementary")
       expect(school.name).to eq("Springfield Elementary")
     end
+
+    it 'has no students' do
+      expect(school.students.count).to eq(0)
+    end
+  end
+
+  describe '#enroll_student' do
+    it "adds a student to the school" do
+      school.enroll_student("Bart Simpson", 2.5)
+      expect(school.students.count).to eq(1)
+    end
+  end
+
+  describe '#find_student' do
+    it 'finds the student' do
+      school.enroll_student("Bart Simpson", 2.5)
+      bart = school.find_student("Bart Simpson")
+      expect(bart).to eq({name: "Bart Simpson", gpa: 2.5})
+    end
+
+    it "doesn't find non existing students" do
+      expect(school.find_student("Shmee Johnson")).to eq(nil)
+    end
+  end
+
+  describe '#average_gpa' do
+    it 'gives an average' do
+      school.enroll_student("Bart Simpson", 2.0)
+      school.enroll_student("Lisa Simpson", 4.0)
+
+      expect(school.average_gpa).to eq(3.0)
+    endh
   end
 end
+
+
+
+
+
+
+
+
+
+
