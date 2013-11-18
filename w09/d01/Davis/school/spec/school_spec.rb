eec71ef71da7fdfe563288c4080bdcd5fff71a30
daviskoh <koh.davis.0@gmail.com>
2013-11-18 10:57:56 -0500

    setup rspec morn ex

---
+++ w09/d01/Davis/school/spec/school_spec.rb
@@ -0,0 +1,14 @@
+# school has students
+# each student has a gpa
+# school has average gpa
+
+require 'spec_helper'
+require_relative '../lib/school'
+
+describe "School" do
+  describe '#some_method' do
+    it "has behavior"
+  end
+
+  
+end
