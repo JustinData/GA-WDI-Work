0711f3307310cd757016abcaf64c15cc9ed7da1d
Ernie Casilla <ecasilla@icloud.com>
2013-11-18 14:31:52 -0500

    name

--- w09/d01/Ernie/bank/lib/bank.rb
+++ w09/d01/Ernie/bank/lib/bank.rb
@@ -1,3 +1,10 @@
 class Bank
+  def initialize(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
   
 end
--- w09/d01/Ernie/bank/spec/bank_spec.rb
+++ w09/d01/Ernie/bank/spec/bank_spec.rb
@@ -1,6 +1,16 @@
-require'spec_helper'
+require 'spec_helper'
 require_relative '../lib/bank'
 
 describe Bank do
-  
+  let(:bank) { Bank.new("Chase") }
+
+  describe '::new' do
+    it 'creates a new bank' do
+      expect(bank).to_not eq nil
+    end
+
+    it 'has a name' do
+      expect(bank.name).to eq("Chase")
+    end
+  end
 end
