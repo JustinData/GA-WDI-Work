ffeed4bea8b61ccc49208e4998ad3361c08b6ec3
Raleigh D Froeber <RDFroeber@gmail.com>
2013-11-19 16:03:04 -0500

    Validity added for users and appointments

---
+++ w09/d02/Raleigh/roomly/app/models/appointment.rb
@@ -0,0 +1,3 @@
+class Appointment < ActiveRecord::Base
+   validates :user_id, :room_id, :start, :finish, presence: true
+end
---
+++ w09/d02/Raleigh/roomly/spec/models/appointment_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe Appointment do
+   let(:appointment) do
+      Appointment.new(user_id: 1, room_id: 2, start: Time.now, finish: 3.hours.from_now)
+   end
+
+   it 'is valid if it has all the attributes' do
+      expect(appointment).to be_valid
+   end
+
+   it 'is invalid without a user_id' do
+      appointment.user_id = nil
+      expect(appointment).to have(1).errors_on(:user_id)
+   end
+
+   it 'is invalid without a room_id' do
+      appointment.room_id = nil
+      expect(appointment).to have(1).errors_on(:room_id)
+   end
+
+   it 'is invalid without a start time' do
+      appointment.start = nil
+      expect(appointment).to have(1).errors_on(:start)
+   end
+
+   it 'is invalid without a finish time' do
+      appointment.finish = nil
+      expect(appointment).to have(1).errors_on(:finish)
+   end
+
+end
