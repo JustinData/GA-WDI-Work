761bc62984e5745b1e0c8b68e75dbd6465dd75d5
Mihran Abrahamian <abrahamian.mihran@gmail.com>
2013-11-19 14:35:10 -0500

    name presence

---
+++ w09/d02/Mihran/roomly/spec/models/user_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe User do
+  let(:user) {User.new}
+
+  describe "#name" do
+    context "no name" do
+      it "isn't valid" do
+        expect(user).not_to be_valid
+      end
+    end
+
+    context "has name" do
+      before do
+        user.name = "Jeff"
+      end
+
+      it "is valid" do
+        expect(user).to be_valid
+      end
+    end
+  end
+end
