4b39d9b6fcadebcc7fbed7ac6536b6e2513b4719
Jeffrey Konowitch <jeff.konowitch@generalassemb.ly>
2013-11-19 14:35:01 -0500

    name presence

--- w09/d02/Instructor/roomly/spec/models/user_spec.rb
+++ w09/d02/Instructor/roomly/spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.new }
+
+  describe "#name" do
+    context "no name" do
+      it "isn't valid" do
+        expect(user.valid?).to be_false
+      end
+    end
+
+    context "has name" do
+      before do
+        user.name = "Jeff"
+      end
+
+      it "is valid" do
+        expect(user.valid?).to be_true
+      end
+    end
+  end
 end
