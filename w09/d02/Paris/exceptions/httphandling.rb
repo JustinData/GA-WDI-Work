80eabe8e563fc419f675b24f5f79f927626f467b
My Name <parisliahyun@gmail.com>
2013-11-19 13:55:14 -0500

    error handling

--- w09/d02/Paris/exceptions/httphandling.rb
+++ w09/d02/Paris/exceptions/httphandling.rb
@@ -1,4 +1,22 @@
 require 'httparty'
 
-response = HTTParty.get('http://www.h4w5.com/')
+path = 'http://www.h4w5.com/'
+
+begin
+response = HTTParty.get(path)
 puts response.body
+
+rescue => e
+  puts e.message
+  puts "Failed address: " + path
+  print "Retry or attempt new address? [r/n] "
+  choice = gets.chomp
+
+  if choice == "r"
+    retry
+  elsif choice == "n"
+    print "New address! "
+    path = gets.chomp
+    retry
+  end
+end
