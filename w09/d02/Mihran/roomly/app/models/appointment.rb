d8dc6065f96b7136d5d2b0e7c9300fe69cb31fa6
Mihran Abrahamian <abrahamian.mihran@gmail.com>
2013-11-19 17:07:57 -0500

    custom validation, 'start before finish'

--- w09/d02/Mihran/roomly/app/models/appointment.rb
+++ w09/d02/Mihran/roomly/app/models/appointment.rb
@@ -1,2 +1,13 @@
 class Appointment < ActiveRecord::Base
+  validates :user_id, :room_id, :start, :finish, presence: true
+  validate :start_before_finish #if: Proc.new { |user| user.finish? }
+
+  def start_before_finish
+    # IE stop running this method - don't go any further!!!
+    return nil unless finish.present? && start.present?
+
+    if self.finish < self.start
+      errors.add(:start, "Can't start after finish!")
+    end
+  end
 end
