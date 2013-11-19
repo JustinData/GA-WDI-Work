ce0f6f677945941274c0d0660deeb643a2dade45
Julie lappano <julie.lappano@gmail.com>
2013-11-19 14:00:32 -0500

    exception handling example

--- w09/d02/Julie/exceptions/filehandling.rb
+++ w09/d02/Julie/exceptions/filehandling.rb
@@ -1,14 +1,20 @@
 require 'pry'
 
 def print_file( file_name )
-  file_path = file_name + ".txt"
-  f = File.new(file_path, "r")
+  begin 
+    file_path = file_name + ".txt"
+    f = File.new(file_path, "r")
 
   while line = f.readline
     puts line
   end
 
-  f.close
+    f.close
+  rescue
+    puts "---Fin---"
+  ensure
+    f.close
+  end
 end
 
 
--- w09/d02/Julie/exceptions/httphandling.rb
+++ w09/d02/Julie/exceptions/httphandling.rb
@@ -1,4 +1,15 @@
 require 'httparty'
 
-response = HTTParty.get('http://www.h4w5.com/')
-puts response.body
+path = 'http://www.h4w5.co'
+
+begin
+  response = HTTParty.get(path)
+  puts response.body
+
+rescue SocketError => e
+  puts e.message
+  puts "failed address: " + path  
+
+  retry
+
+end
