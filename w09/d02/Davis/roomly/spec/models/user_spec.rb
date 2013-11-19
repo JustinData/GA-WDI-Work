9357bb3a5d982b99d14b1d9ce7b46d0fc1f59df6
daviskoh <koh.davis.0@gmail.com>
2013-11-19 14:35:05 -0500

    name presence

--- w09/d02/Davis/roomly/spec/models/user_spec.rb
+++ w09/d02/Davis/roomly/spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.new }
+
+  describe '#name' do
+    context 'no name' do
+      it 'isnt valid' do
+        # testing intersection of AR validation & name method
+        expect(user).not_to be_valid
+        #or expect(user.valid?).to eq(false)
+          # valid? is AR behavior inherited from AR base
+      end
+    end
+
+    context 'has name' do
+      before do
+        user.name = "Jeff"
+      end
+
+      it 'is valid' do
+        expect(user).to be_valid
+      end
+    end
+  end
 end
