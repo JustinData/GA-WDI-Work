65d5d34489b74a9ae3fc61622fd86353ce19d4c5
Julie lappano <julie.lappano@gmail.com>
2013-11-19 16:56:02 -0500

    appointment spec

--- w09/d02/Julie/roomly/app/models/appointment.rb
+++ w09/d02/Julie/roomly/app/models/appointment.rb
@@ -1,2 +1,13 @@
 class Appointment < ActiveRecord::Base
+  validates :user_id, :room_id, :start, :finish, presence: true
+  validate :start_before_finish
+
+
+  def start_before_finish
+    return unless finish.present? && start.present?
+     if self.start > self.finish
+      errors.add(:start, "Can't finish before you start, human!")
+    end
+  end
+
 end
