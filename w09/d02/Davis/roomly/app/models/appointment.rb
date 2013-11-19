b993aaac9dc2419dabd15da1c9610e9ec5c7d457
daviskoh <koh.davis.0@gmail.com>
2013-11-19 15:47:22 -0500

    user_id presenece for appointment

--- w09/d02/Davis/roomly/app/models/appointment.rb
+++ w09/d02/Davis/roomly/app/models/appointment.rb
@@ -1,2 +1,3 @@
 class Appointment < ActiveRecord::Base
+  validates :user_id, presence: true
 end
--- w09/d02/Davis/roomly/spec/models/appointment_spec.rb
+++ w09/d02/Davis/roomly/spec/models/appointment_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe Appointment do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:appointment) do
+    Appointment.new(user_id: 1, room_id: 2, start: Time.now, finish: 3.hours.from_now)
+  end
+
+  it 'is valid if it has all the attributes' do
+    expect(appointment).to be_valid
+  end
+
+  it 'is invalid without a user_id' do
+    appointment.user_id = nil
+    expect(appointment).to have(1).errors_on(:user_id)
+  end
 end
