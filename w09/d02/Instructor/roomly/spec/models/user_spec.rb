9a10b489a312e6060bfce1cee50c90f083c938d7
Jeffrey Konowitch <jeff.konowitch@generalassemb.ly>
2013-11-19 15:22:12 -0500

    user tested

--- w09/d02/Instructor/roomly/spec/models/user_spec.rb
+++ w09/d02/Instructor/roomly/spec/models/user_spec.rb
@@ -6,7 +6,7 @@ describe User do
   describe "#name" do
     context "no name" do
       it "isn't valid" do
-        expect(user.valid?).to be_false
+        expect(user).to have(1).errors_on(:name)
       end
     end
 
@@ -16,7 +16,44 @@ describe User do
       end
 
       it "is valid" do
-        expect(user.valid?).to be_true
+        expect(user).to have(0).errors_on(:name)
+      end
+    end
+  end
+
+  describe "#email" do
+    context "no email" do
+      it "is not valid" do
+        expect(user).to have(1).errors_on(:email)
+      end
+    end
+
+    context "has email" do
+      before do
+        user.email = "jeff@ga.co"
+      end
+
+      it "is valid" do
+        expect(user).to have(0).errors_on(:email)
+      end
+
+      context "email exists" do
+        before do
+          user.name = "Jeff"
+          user.save!
+        end
+
+        let(:another_user) { User.new(name: "Some Person", email: "jeff@ga.co") }
+
+        it "is not valid" do
+          expect(another_user).to have(1).errors_on(:email)
+        end
+      end
+
+      context "email doesn't exist" do
+        it "is valid" do
+          expect(user).to have(0).errors_on(:email)
+        end
       end
     end
   end
