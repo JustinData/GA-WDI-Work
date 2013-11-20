122172b4967448db1560853c50ae7edf20c37bc6
My Name <parisliahyun@gmail.com>
2013-11-20 14:51:40 -0500

    today's stuff

--- w09/d02/Paris/roomly-solution/app/models/room.rb
+++ w09/d02/Paris/roomly-solution/app/models/room.rb
@@ -6,6 +6,9 @@ class Room < ActiveRecord::Base
   def available?(appointment)
     # all appointments for this room
     appointments
+    # the below is sql. could be:
+    # .where("(#{appointment.start}, #{appointment.finish}) OVERLAPS (start, finish)")
+    # looks for any appts with this room idea with these start finish times. .none? will return a boolean. Either there are no appts that overlap or there are appts that overlap.
       .where("(?, ?) OVERLAPS (start, finish)", appointment.start, appointment.finish)
       .none?
   end
