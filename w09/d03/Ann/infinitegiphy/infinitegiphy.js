f3ceb39013ad4c1cd10b8075a3f8c0d5932c7e29
millzpaugh <ann.millspaugh@gmail.com>
2013-11-21 10:32:01 -0500

    morning exercise

--- w09/d03/Ann/infinitegiphy/infinitegiphy.js
+++ w09/d03/Ann/infinitegiphy/infinitegiphy.js
@@ -1,20 +1,52 @@
+var giphs; 
+var count=0; 
 
-function getGIPHY() {
+function getGiphs() {
   $('form').on('submit', function(e){
     e.preventDefault(); 
 
-    var searchField = $('search').val(); 
-    var querySearch = searchField.replace(" ", "+"); 
+    var searchField = $('input[type="search"]').val(); 
+    // var querySearch = searchField.replace(" ", "+"); 
     var apiKey = "&api_key=dc6zaTOxFJmzC" 
-    // var giphLimit = "&limit=100"
+    var giphLimit = "&limit=100"
 
     $.ajax({
-      url: "http://api.giphy.com/v1/gifs/search?q=" + querySearch + apiKey,
-      method: "GET",
+      url: "http://api.giphy.com/v1/gifs/search?q=" + searchField + apiKey + giphLimit,
+      type: "GET",
       context: this,
+      dataType: "json",
       success: function(response){
-        console.log(response);
-      }
+        giphs = response; 
+        console.log(giphs);
+        count = 0; 
+      } 
     });
+  }); 
+}; 
+
+function attachGIPH() {
+  $('#add').on('click', function(e){
+    img = $("<img src=" + giphs.data[count].images.fixed_height.url + ">"); 
+    $('body').append(img); 
+    count++; 
+  }); 
+}; 
+
+function windowScroll(){
+  $(window).scroll(function(){
+    if($(window).scrollTop() + $(window).height() >= $(document).height()){
+      img = $("<img src=" + giphs.data[count].images.fixed_height.url + ">"); 
+      $('body').append(img); 
+      count++;
+    }
+  }); 
+} 
+
+$(function(){
+
+  getGiphs(); 
+  attachGIPH(); 
+  windowScroll(); 
+
 }); 
- 
+
