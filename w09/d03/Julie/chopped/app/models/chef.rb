dc3c34138d822b64e7fd93ff4e3cedd0c48e1573
Julie lappano <julie.lappano@gmail.com>
2013-11-21 09:18:13 -0500

    made some model files

--- w09/d03/Julie/chopped/app/models/chef.rb
+++ w09/d03/Julie/chopped/app/models/chef.rb
@@ -2,8 +2,8 @@ class Chef < ActiveRecord::Base
   validates :name, presence: true
   has_many :dishes
 
-  def make_dish(name, chef_id) 
-     Dish.create(name: name, chef_id: chef_id)
+  def make_dish(name) 
+     Dish.create(name: name, chef: self)
   end
 end
 
