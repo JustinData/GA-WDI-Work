4b97b713f22750dc6d496b6135b59ea80c0d246a
slardizzone <slardizzone@gmail.com>
2013-11-25 10:23:00 -0500

    chopped rspec exercise

--- w09/d03/Sal/chopped/app/models/chef.rb
+++ w09/d03/Sal/chopped/app/models/chef.rb
@@ -1,8 +1,8 @@
 class Chef < ActiveRecord::Base
-  validates :name, presence: true 
+  validates :name, :episode_id, presence: true 
   has_many :dishes
 
-  def make_dish(name, chef_id)
-    Dish.create(name: name, chef_id: chef_id)
+  def make_dish(name, round_id)
+    Dish.create(name: name, round_id: round_id, chef_id: self.id)
   end
 end
--- w09/d03/Sal/chopped/spec/models/chef_spec.rb
+++ w09/d03/Sal/chopped/spec/models/chef_spec.rb
@@ -8,7 +8,7 @@ describe Chef do
     context "with a name" do
       it "is valid" do
         chef.name = "Pierre"
-        expect(chef).to be_valid
+        expect(chef).to have(0).errors_on(:name)
       end    
     end
 
@@ -21,19 +21,51 @@ describe Chef do
 
   describe '#make_dish' do
     it "creates a dish" do
-      chef.id = 1
-      expect(chef.make_dish("Penne", chef.id)).to be_persisted
+      expect(chef.make_dish("Penne", 1)).to be_persisted
+    end
+
+    it "cannot make more than one dish per round" do
+      chef.make_dish("Penne", 1)
+      expect(chef.make_dish("Sketti", 1)).to be_false
     end
   end
 
   describe "get chef's dishes" do
-    before do
+    context "chef has made a dish" do
+      before do
+          chef.name = "joe"
+          chef.id = 2
+          chef.make_dish("Penne", 1)
+      end
+      it "returns dishes created by chef" do
+        expect(chef.dishes.count).to be >= 1
+      end
+    end
+
+    context "chef has not made a dish" do
+      before do 
         chef.name = "joe"
         chef.id = 2
-        chef.make_dish("Penne", chef.id)
+      end
+      it "does not return a dish" do
+        expect(chef.dishes.count).to be(0)
+      end
     end
-    it "returns dishes created by chef" do
-      expect(chef.dishes.count).to be >= 1
+  end
+
+  describe "#episode" do
+    context "with an episode" do
+      it "is valid" do
+        chef.name = "Pierre"
+        chef.episode_id = 1 
+        expect(chef).to be_valid
+      end
+    end
+
+    context "with no name" do
+      it "isn't valid" do
+        expect(chef).to have(1).errors_on(:episode_id)
+      end
     end
   end
 end
