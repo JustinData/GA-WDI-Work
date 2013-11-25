a64c50c39a7a3213f5492c7be75f14046b4ba014
daviskoh <koh.davis.0@gmail.com>
2013-11-25 10:32:13 -0500

    method that writes tests for you

--- w10/d01/Davis/cat_spec/spec/stuff_spec.rb
+++ w10/d01/Davis/cat_spec/spec/stuff_spec.rb
@@ -1,6 +1,23 @@
 require 'spec_helper'
 require_relative '../lib/stuff'
 
-scope = self
 
-self.expect(nil).to eq(nil)
+def write_test(desc_string, it_string, &block)
+  describe desc_string do 
+    it it_string do
+      yield
+    end
+  end
+end
+
+describe Stuff do
+  it 'has a stuff' do 
+    $stuff = self
+  end
+
+  write_test("description", "it thang") do
+    
+    $stuff.expect(nil).to $stuff.eq(nil)
+  end
+end
+
