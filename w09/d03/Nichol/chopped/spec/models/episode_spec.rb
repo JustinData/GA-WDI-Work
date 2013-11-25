d37fd7805ab5fcaaa9c61574b270eda08b288b0a
Nichol Alexander <nichol.alexander@gmail.com>
2013-11-25 15:26:41 -0500

    ready to pull

--- w09/d03/Nichol/chopped/spec/models/episode_spec.rb
+++ w09/d03/Nichol/chopped/spec/models/episode_spec.rb
@@ -26,48 +26,63 @@ describe Episode do
 
   it "has 4 chefs and is valid" do
     
-    
-    #4.times { episode.chefs.create }
-    
     chef1 = Chef.new
     chef1.name = "bob"
+    chef1.save
     chef1.episode = episode
+    chef1.save
+
     chef2 = Chef.new
     chef2.name = "sam"
     chef2.episode = episode
+    chef2.save
+
     chef3 = Chef.new
     chef3.name = "jim"
     chef3.episode = episode
+    chef3.save
+
     chef4 = Chef.new
     chef4.name = "mat"
     chef4.episode = episode
-
-    #binding.pry
+    chef4.save
+    
 
     expect(episode).to have(0).errors_on(:chefs)
   end
 
   it "has 5 chefs and and episode is invalid" do
-    # chef1 = Chef.new
-    # chef1.name = "bob"
-    # chef1.episode = episode
-    # chef2 = Chef.new
-    # chef2.name = "sam"
-    # chef2.episode = episode
-    # chef3 = Chef.new
-    # chef3.name = "jim"
-    # chef3.episode = episode
-    # chef4 = Chef.new
-    # chef4.name = "mat"
-    # chef4.episode = episode
     
-    # chef5 = Chef.new
-    # chef5.name = "larry"
-    # chef5.episode = episode
-    
-    5.times {episode.chefs.create}
+    chef1 = Chef.new
+    chef1.name = "bob"
+    chef1.save
+    chef1.episode = episode
+    chef1.save
 
-    binding.pry
+    chef2 = Chef.new
+    chef2.name = "sam"
+    chef2.save
+    chef2.episode = episode
+    chef2.save
+    
+    chef3 = Chef.new
+    chef3.name = "jim"
+    chef3.save
+    chef3.episode = episode
+    chef3.save
+
+    chef4 = Chef.new
+    chef4.name = "mat"
+    chef4.save
+    chef4.episode = episode
+    chef4.save
+
+    chef5 = Chef.new
+    chef5.name = "larry"
+    chef5.save
+    chef5.episode = episode
+    chef5.save
+    
 
     expect(episode).to have(1).errors_on(:chefs)
 
