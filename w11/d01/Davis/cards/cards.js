6d658a026fe8e9b817ddb2734b5b615f181f4e58
daviskoh <koh.davis.0@gmail.com>
2013-12-02 10:44:17 -0500

    card values stored as numbers, but read as string

--- w11/d01/Davis/cards/cards.js
+++ w11/d01/Davis/cards/cards.js
@@ -3,21 +3,34 @@ function Card(suit, value) {
   this.val = value;
 }
 Card.prototype.read = function() {
-  console.log(this.val + " of " + this.suit);
+  // if 11,12,13,14 change: "jack", "queen", "king", "ace"
+  newSuit = if (this.suit === 11) {
+    return "jack";
+  } else if (this.suit === 12) {
+    return "queen";
+  } else if (this.suit === 13) {
+    return "king";
+  } else if (this.suit === 14) {
+    return "ace";
+  } else {
+    return this.suit();
+  }
+
+  console.log(this.val + " of " + newSuit);
 }
 
-function Deck() {
-  var suits = ["spades", "hearts", "clubs", "diamonds"];
-  var values = [2,3,4,5,6,7,8,9,10,"jack", "queen", "king", "ace"]
+var suits = ["spades", "hearts", "clubs", "diamonds"];
+var values = [2,3,4,5,6,7,8,9,10,11,12,13,14];
 
+function Deck() {
   this.cards = [];
 
   for(var i=0; i<values.length; i++) {
-  for(var j=0; j<suits.length; j++) {
-    // console.log(values[i] + suits[j]);
-    this.cards.push( new Card(suits[j], values[i]) );
+    for(var j=0; j<suits.length; j++) {
+      // console.log(values[i] + suits[j]);
+      this.cards.push( new Card(suits[j], values[i]) );
+    }
   }
-}
 }
 Deck.prototype.shuffle = function() {
   this.cards = _.shuffle(this.cards);
@@ -28,6 +41,29 @@ Deck.prototype.draw = function(hand) {
 
 function Hand() {
   this.cards = [];
+}
+Hand.prototype.checkFlush = function() {
+  // iterate through each suit & check if hand is all 1
+  for(var i=0; i < suits.length; i++) {
+    var flush = _.every(this.cards, function(card) {
+      return card.suit === suits[i];
+    })
+
+    if (flush) { console.log("FLUSH"); }
+  }
+}
+Hand.prototype.checkStraight = function() {
+  // sort hand, iterate through each hand card, check if increment order
+  // check for non number cards
+  var sorted = this.cards.sort();
+  for(var i=0; i<sorted.length; i++) {
+
+  }
+}
+Hand.prototype.checkHand = function() {
+  this.checkFlush();
+  
+
 }
 Hand.prototype.read = function() {
   for(var i=0; i < this.cards.length; i++) {
@@ -45,3 +81,12 @@ Hand.prototype.addCard = function(card) {
 
 deck = new Deck;
 hand = new Hand;
+
+var flushCards = [];
+_.each(values, function(value) {
+  flushCards.push( new Card(suits[0], value) );
+})
+
+for(var i=0; i<5; i++) {
+  hand.cards.push( flushCards.pop() );
+}
