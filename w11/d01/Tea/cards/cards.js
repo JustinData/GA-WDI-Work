02746bb91cd664c74845493be15bece1a8e086fc
teabait <hello.teaho@gmail.com>
2013-12-02 10:40:01 -0500

    card deck with shuffle, draw, poker 5 card deal

---
+++ w11/d01/Tea/cards/cards.js
@@ -0,0 +1,53 @@
+window.onload = function(){}
+
+function Card(suit, value) {
+  this.suit = suit;
+  this.value = value;
+}
+
+Card.prototype.toString = function() {
+  console.log(this.suit + " " + this.value);
+}
+
+function Deck() {
+  this.cards = [];
+  var suits = ["♥","♦","♣","♠"];
+  var values = [2,3,4,5,6,7,8,9,10,"J","Q","K","A"];
+  // make card objects out of suits and values
+  for(var s = 0; s < suits.length; s++){
+    for(var i = 0; i < values.length; i++){
+      this.cards[this.cards.length] = new Card(suits[s], values[i]);
+    }
+  }
+}
+
+var deck = new Deck();
+var hand = new Hand();
+
+Deck.prototype.shuffle = function() {
+   this.cards = _.shuffle(this.cards);
+}
+
+Deck.prototype.draw = function(hand) {
+  hand.cards.push(this.cards[0]);
+  this.cards.splice(0, 1);
+}
+
+function Hand() {
+  this.cards = [];
+}
+
+Hand.prototype.announce = function() {
+  var i = this.cards.length;
+  for(var i = 0 ; i < 5 ; i++) {
+    console.log(this.cards[i]);
+  }
+}
+
+function Poker(){
+  deck.shuffle();
+  this.pokerHand = new Hand();
+  for(var i = 0 ; i < 5 ; i++) {
+    deck.draw(this.pokerHand);
+  }
+}
