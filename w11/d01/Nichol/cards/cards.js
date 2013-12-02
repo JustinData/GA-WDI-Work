8ec755a2515db507d6b1d15fbe3af17862ac9eb0
Nichol Alexander <nichol.alexander@gmail.com>
2013-12-02 09:44:53 -0500

    basic functionality of poker game

--- w11/d01/Nichol/cards/cards.js
+++ w11/d01/Nichol/cards/cards.js
@@ -13,17 +13,79 @@
 // deck.draw(5) removes 5 cards from deck and puts it in hand
 // hand.read tells us what cards are in our hand.
 
-function Question(q, uId, qId) {
-  this.details = q;
-  this.votes = 0;
-  this.answered = false;
-  this.paused = false;
-  this.userId = uId;
-  this.id = qId;
+function Card() {
+  this.suit = "";
+  this.rank = 0;
 }
 
-//Question object methods
+Card.prototype.read = function() {
 
-Question.prototype.upVote = function() {
-  this.votes = this.votes + 1;
-};
+}
+
+function Deck(){
+  this.cards = [];
+}
+
+Deck.prototype.initialize = function() {
+
+  for(i=1; i<=10; i++){
+    tempcard = new Card();
+    tempcard.rank = i;
+    tempcard.suit = "Diamond";
+    this.cards.push(tempcard);
+  }
+
+    tempcard = new Card();
+    tempcard.rank = "A";
+    tempcard.suit = "Diamond";
+    this.cards.push(tempcard);
+
+    tempcard = new Card();
+    tempcard.rank = "K";
+    tempcard.suit = "Diamond";
+    this.cards.push(tempcard);
+
+    tempcard = new Card();
+    tempcard.rank = "Q";
+    tempcard.suit = "Diamond";
+    this.cards.push(tempcard);
+
+    tempcard = new Card();
+    tempcard.rank = "J";
+    tempcard.suit = "Diamond";
+    this.cards.push(tempcard);
+
+}
+
+Deck.prototype.shuffle = function(){
+  this.cards = _.shuffle(this.cards);
+
+}
+
+Deck.prototype.draw = function(hand){
+  for(i=0; i<=4; i++){
+    hand.cards.push(this.cards.pop());
+  }
+}
+
+function Hand(){
+  this.cards = [];
+}
+
+Hand.prototype.read = function(){
+  _.each(this.cards, function(x){
+    console.log(x);
+  });
+}
+
+window.onload = function(){
+  console.log("hello and lets play poker");
+  
+  myDeck = new Deck();
+  myHand = new Hand();
+  myDeck.initialize();
+  myDeck.shuffle();
+  myDeck.draw(myHand);
+
+}
+
