54cf23efeeb8794baa2e1c24c69f33f238c0a5de
Maria Mercedes Martinez <pistachiopony@gmail.com>
2013-12-02 09:15:41 -0500

    card stuff

---
+++ w11/d01/Maria/cards/cards.js
@@ -0,0 +1,18 @@
+function card(){
+  //suit, value = attributes
+  //read/tostring = behavior
+
+}
+
+function deck(){
+  //2 methods shuffle and draw= behavior
+  //52 cards = attribute
+
+}
+
+function hand(){
+  //read= behavior
+  //cards= attribute
+
+
+}
