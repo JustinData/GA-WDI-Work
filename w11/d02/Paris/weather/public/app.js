eaba1f64effcb80742e11cbb32e1059ed23722aa
My Name <parisliahyun@gmail.com>
2013-12-04 09:56:31 -0500

    weather

--- w11/d02/Paris/weather/public/app.js
+++ w11/d02/Paris/weather/public/app.js
@@ -1,86 +1,88 @@
-// kitten stuff #############################
-
 var PlayingAroundRouter = Backbone.Router.extend({
   routes: {
     "clowns" : "clownsSayHi",
-    "kittens/:height/:width" : "addKitten"
+    "kittens/:height/:width" : "showMeTheKittens"
   },
 
-clownsSayHi: function() {
-  alert("Hello adults <%:b");
-},
+  clownsSayHi: function() {
+    alert("HELLO ADULTS <%:b");
+  },
 
-addKitten: function(height, width) {
-
-  $("<img>")
-    .attr("src", "http://placekitten.com/" + height + "/" + width)
-    .appendTo($("body"));
-}
-
+  showMeTheKittens: function(height, width) {
+    $("<img>")
+      .attr("src", "http://placekitten.com/" + height + "/" + width)
+      .appendTo($("body"));
+  }
 });
 
-
-// weather stuff ##########################
-var WeatherRouter = Backbone.Router.extend({
-routes: {    
-    "weather/:state/:city" : "getPlace"
-    
-},
-
-getPlace : function(state, city){
-  new WeatherView({state: state, city: city});
-}
-  });
-
 new PlayingAroundRouter();
+
+
+var WeatherRouter = Backbone.Router.extend({
+  routes: {
+    "weather/:state/:city": "weather"
+  },
+
+  weather: function(state, city) {
+    // something, somehow render some stuff
+    // PROBABLY (yes, definitely) render a view
+    new WeatherView({state: state, city: city});
+  }
+});
+
 new WeatherRouter();
-Backbone.history.start();
 
 var FormView = Backbone.View.extend({
   el: "form",
   events: {
-    "submit" : "weatherThing"
+    "submit": "wasSubmitted"
   },
-
-
-weatherThing : function(e){
-  e.preventDefault();
-  var city = this.$el.find("input[name='city']").val();
-  var state = this.$el.find("input[name='state']").val();
-  Backbone.history.navigate("weather/" + state + "/" + city, {trigger: true});
-  alert(city + " " + state);
-},
+  wasSubmitted: function(e) {
+    e.preventDefault();
+    var city = this.$el.find("input[name='city']").val();
+    var state = this.$el.find("input[name='state']").val();
+    Backbone.history.navigate("weather/" + state + "/" + city, {trigger: true});
+  }
 });
-
 
 var formView = new FormView();
 
 var WeatherView = Backbone.View.extend({
   tagName: "div",
 
-// turns the template defined in the html into a string.
   template: _.template($("script[type='text/html']").html()),
 
   initialize: function(opts) {
     this.state = opts.state;
     this.city = opts.city;
     this.$el.appendTo($("body"));
-    // the render fills the empty el html object with the stuff defined in the render function.
     this.render();
-},
+    this.fetchWeather();
+  },
 
-render : function() {
-  var compiledTemplate = this.template();
-  this.$el.html(compiledTemplate);
+  render: function() {
+    var compiledTemplate = this.template();
+    this.$el.html(compiledTemplate);
+  },
+
+  fetchWeather: function() {
+    $.ajax({
+      method: "GET",
+      url: "/weather?q=" + this.city + "," + this.state,
+      dataType: "json",
+      success: this.receiveWeather,
+      context: this
+    });
+  },
+
+  receiveWeather: function(weather) {
+    this.temperature = (weather.main.temp - 273) * 9/5 + 32;
+    this.render();
   }
+
 });
 
-// el is an html element and we tell it what to find via selectors.
-// Backbone will now take over the browser and start listening for on hash events. 
+//var view = new WeatherView({state: "California", city: "Pasadena"});
 
 
-
-
-
-
-
+Backbone.history.start();
