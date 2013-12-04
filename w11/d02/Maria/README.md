65dac1256646f5baeb9690ce708bd34800219cf4
Maria Mercedes Martinez <pistachiopony@gmail.com>
2013-12-03 20:05:37 -0500

    other stuff

--- w11/d02/Maria/README.md
+++ w11/d02/Maria/README.md
@@ -1,19 +1,6 @@
-#Students & Teachers
+# Stockbone
 
-Super quick warmup on JavaScript Objects
+Create a stock app using backbone that allows you to look up stock prices. You should be able to enter a stock symbol and render a view with the stock symbol and its last price.
 
-### Using the constructor method, define a Student
-- He/she has a name
-- He/she has a list of skills
-- He/she can learn a new skill
-- He/she can forget (remove the last oldest they learned)
-    
-### Using object literal notation, define a `feeney` object
-- He has a name
-- He has students
-- He can add students to his list of students
-- He can teach a skill to his students
-
-### Bonus
-- A student can "zone out". If they are zoned out and a teacher tries to to them something, they don't learn it.
-- Ensure that for a given student, their skills are unique (ie no duplicates)
+## Bonus
+* Change the color of the text for the stock price based on the `Change`. If it is down, the color should be red. If it is up, the color should be green.
--- w11/d02/Maria/weather/public/app.js
+++ w11/d02/Maria/weather/public/app.js
@@ -1,59 +1,58 @@
 var PlayingAroundRouter = Backbone.Router.extend({
-  routes: { //map a route to an action
+  routes: {
     "clowns" : "clownsSayHi",
     "kittens/:height/:width" : "showMeTheKittens"
   },
 
   clownsSayHi: function() {
-    alert("Hello Adults <%:o");
+    alert("HELLO ADULTS <%:b");
   },
-  showMeTheKittens: function(height, width){
+
+  showMeTheKittens: function(height, width) {
     $("<img>")
-    .attr("src", "http://placekitten.com/" + height + "/" + width)
-    .appendTo($("body"));
+      .attr("src", "http://placekitten.com/" + height + "/" + width)
+      .appendTo($("body"));
   }
 });
 
- new PlayingAroundRouter();
+new PlayingAroundRouter();
 
-Backbone.history.start();
-
-//#kittens
-//weather/Ma/Boston
 
 var WeatherRouter = Backbone.Router.extend({
   routes: {
-    "weather/:state/:city" : "weather"
+    "weather/:state/:city": "weather"
   },
-  weather: function(state, city){
-    new WeatherView({state: state, city: city})
+
+  weather: function(state, city) {
+    // something, somehow render some stuff
+    // PROBABLY (yes, definitely) render a view
+    new WeatherView({state: state, city: city});
   }
 });
 
 new WeatherRouter();
 
 var FormView = Backbone.View.extend({
-  el: "form", //this is a jquery css selector
-  events:{
-    "submit" : "wasSubmitted"
+  el: "form",
+  events: {
+    "submit": "wasSubmitted"
   },
-  wasSubmitted: function(e){
+  wasSubmitted: function(e) {
     e.preventDefault();
     var city = this.$el.find("input[name='city']").val();
     var state = this.$el.find("input[name='state']").val();
     Backbone.history.navigate("weather/" + state + "/" + city, {trigger: true});
   }
 });
-var formview = new FormView();
+
+var formView = new FormView();
 
 var WeatherView = Backbone.View.extend({
   tagName: "div",
 
-  template: _.template($("script[type='text/html']")),
+  template: _.template($("script[type='text/html']").html()),
 
-  initialize: function(opts){
-
-    //this.$el.html(this.template({state: opts.state, city: opts}));
+  initialize: function(opts) {
     this.state = opts.state;
     this.city = opts.city;
     this.$el.appendTo($("body"));
@@ -61,32 +60,29 @@ var WeatherView = Backbone.View.extend({
     this.fetchWeather();
   },
 
-  render: function(){
+  render: function() {
     var compiledTemplate = this.template();
     this.$el.html(compiledTemplate);
-  }
-  fetchWeather: function(){
+  },
+
+  fetchWeather: function() {
     $.ajax({
       method: "GET",
-      url: "/weather:q=" + this.city + "," + this.state,
+      url: "/weather?q=" + this.city + "," + this.state,
       dataType: "json",
       success: this.receiveWeather,
-      conext: this
+      context: this
     });
   },
 
-  receiveWeather: function(weather){
+  receiveWeather: function(weather) {
     this.temperature = (weather.main.temp - 273) * 9/5 + 32;
     this.render();
   }
-  
+
 });
-//var view = new WeatherView();
+
+//var view = new WeatherView({state: "California", city: "Pasadena"});
 
 
 Backbone.history.start();
-
-
-
-
-
