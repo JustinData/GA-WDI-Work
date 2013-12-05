7e10fa6be39c206b4c803dd9b63d14cce5b57d9d
Jeffrey Konowitch <jkonowitch@gmail.com>
2013-12-05 10:12:30 -0500

    with examples

--- w11/d04/Instructor/luhn.rb
+++ w11/d04/Instructor/luhn.rb
@@ -14,6 +14,7 @@ def valid?(number)
   luhn_sum % 10 == 0
 end
 
-number = 4408041234567893
-
-puts valid?(number)
+puts valid?(1234567890123456) #should be false
+puts valid?(4408041234567893) #should be true
+puts valid?(38520000023237) #should be true
+puts valid?(4222222222222) #should be true
