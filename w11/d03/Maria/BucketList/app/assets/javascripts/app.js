295caaf4be4084608d3596a83de6f964db387743
Maria Mercedes Martinez <pistachiopony@gmail.com>
2013-12-05 13:41:02 -0500

    stuff

--- w11/d03/Maria/BucketList/app/assets/javascripts/app.js
+++ w11/d03/Maria/BucketList/app/assets/javascripts/app.js
@@ -4,7 +4,7 @@ var Activity = Backbone.Model.extend({
   },
 
   toggleDone: function() {
-    this.set("done", !this.get("done"));
+    this.save({done: !this.get("done")});
   }
 });
 
@@ -16,13 +16,13 @@ var BucketList = Backbone.Collection.extend({
 
 var list = new BucketList();
 
-var skyDiving = new Activity({title: "Sky Diving"});
+// var skyDiving = new Activity({title: "Sky Diving"});
 
-list.add(skyDiving);
+// list.add(skyDiving);
 
-list.on("add", function() {
-  console.log(this.pluck("title"));
-});
+// list.on("add", function() {
+//   console.log(this.pluck("title"));
+// });
 
 var ActivityView = Backbone.View.extend({
   tagName: "li",
@@ -31,14 +31,21 @@ var ActivityView = Backbone.View.extend({
 
   events: {
     "click :checkbox": "toggleDoneRainbows"
+    "click span.remove" :"deleteActivity"
   },
 
   toggleDoneRainbows: function() {
     this.model.toggleDone();
   },
 
+  deleteActivity: function(){
+    this.model.destroy();
+  },
+
   initialize: function() {
     this.listenTo(this.model, "change", this.render);
+    this.listenTo(this.model, "destroy", this.remove);
+
     this.render();
   },
 
@@ -54,11 +61,20 @@ var ListView = Backbone.View.extend({
 
   initialize: function() {
     this.listenTo(this.collection, "add", this.addOne);
+    this.listenTo(this.collection, "reset", this.addAll);
   },
 
   addOne: function(activity) {
     var view = new ActivityView({model: activity});
     this.$el.append(view.el);
+  },
+
+  addAll: function() {
+    this.collection.each(this.addOne, this);
+  },
+  
+  deleteOne: function(){
+
   }
 });
 
@@ -74,11 +90,19 @@ var FormView = Backbone.View.extend({
     var title = this.$el.find("input[name='activity']").val();
     this.collection.create({title: title});
     this.el.reset();
+  },
+
+  deleteActivity: function(e) {
+    e.preventDefault();
+    this.collection.destroy({title: title});
+    this.el.reset();
   }
 
 });
 
 var form = new FormView({collection: list});
 var theList = new ListView({collection: list});
+
+list.fetch({reset: true});
 
 
