0138d52feb2f3195694c2dc90c36b5f850c30588
Mihran Abrahamian <abrahamian.mihran@gmail.com>
2013-12-06 16:09:23 -0500

    BucketList

--- w11/d03/Mihran/BucketList/app/assets/javascripts/app.js
+++ w11/d03/Mihran/BucketList/app/assets/javascripts/app.js
@@ -3,10 +3,10 @@ var Activity = Backbone.Model.extend({
     done: false
   },
 
-  toggleDone: function(){
-    this.set("done", !this.get("done"));
-    console.log(this.get("done"));
-  },
+  toggleDone: function() {
+    this.save({done: !this.get("done")});
+  }
+
 });
 
 var BucketList = Backbone.Collection.extend({
@@ -17,33 +17,42 @@ var BucketList = Backbone.Collection.extend({
 
 var list = new BucketList();
 
-var skyDiving = new Activity({title: "Sky Diving"});
-list.add(skyDiving);
+// var skyDiving = new Activity({title: "Sky Diving"});
 
-list.on("add", function() {
-  console.log(this.pluck("title"));
-});
+// list.add(skyDiving);
+
+// list.on("add", function() {
+//   console.log(this.pluck("title"));
+// });
 
 var ActivityView = Backbone.View.extend({
   tagName: "li",
+
   template: _.template($("script[type='text/html']").html()),
-  
+
   events: {
-    "click :checkbox": "toggleDoneRainbows"
+    "click :checkbox": "toggleDoneRainbows",
+    "click.remove": "deleteActivity"
   },
 
-  toggleDoneRainbows: function(){
+  toggleDoneRainbows: function() {
     this.model.toggleDone();
+  },
+
+  deleteActivity: function() {
+    this.model.destroy();
   },
 
   initialize: function() {
     this.listenTo(this.model, "change", this.render);
+    this.listenTo(this.model, "destroy", this.remove);
     this.render();
   },
 
   render: function() {
-    var compiledTemplate = this.template(this.model.toJSON());
-    this.$el.html(compiledTemplate);
+    //this.$el.text(this.model.get("title"));
+    var compiledTemp = this.template(this.model.toJSON());
+    this.$el.html(compiledTemp);
   }
 });
 
@@ -52,11 +61,16 @@ var ListView = Backbone.View.extend({
 
   initialize: function() {
     this.listenTo(this.collection, "add", this.addOne);
+    this.listenTo(this.collection, "reset", this.addAll);
   },
 
   addOne: function(activity) {
     var view = new ActivityView({model: activity});
     this.$el.append(view.el);
+  },
+
+  addAll: function() {
+    this.collection.each(this.addOne, this);
   }
 });
 
@@ -73,8 +87,9 @@ var FormView = Backbone.View.extend({
     this.collection.create({title: title});
     this.el.reset();
   }
-
 });
 
 var form = new FormView({collection: list});
 var theList = new ListView({collection: list});
+
+list.fetch({reset: true});
