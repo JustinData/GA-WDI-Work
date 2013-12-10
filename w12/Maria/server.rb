0599f3b2600c4abe2980ff95f5eca6d97bc4cee8
Maria Mercedes Martinez <pistachiopony@gmail.com>
2013-12-10 10:56:18 -0500

    deleted stuff

--- w12/Maria/server.rb
+++
@@ -1,9 +0,0 @@
-require 'sinatra'
-require 'sinatra/reloader'
-require 'httparty'
-require 'json'
-require 'uri'
-
-get '/' do
-  send_file File.join(settings.public_folder, 'index.html')
-end
--- w12/Maria/too_to_to/lib/too.rb
+++
@@ -1,8 +0,0 @@
-def too_to_to(string)
-  a = string.each_char {|c|
-    if c = c 
-      c.delete "c"
-    end
-    puts "#{a}"
-  }
-end
